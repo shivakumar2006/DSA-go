c732a7879df6902f4643ed466afc207573c1c3f8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-22 15:02:59 +0530

    commit

--- Arrays/linearSearch/linear.go
+++ Arrays/linearSearch/linear.go
@@ -5,11 +5,10 @@ import (
 )
 
 func main() {
-	array := []int{1, 4, 5, 6, 3, 7, 10, 69}
-	target := 7
+	arr := []int{2, 5, 3, 7, 69, 100, 45}
+	target := 69
 
-	result := linearSearch(array, target)
-
+	result := linearSearch(arr, target)
 	if result != -1 {
 		fmt.Printf("Element found at index %d\n", result)
 	} else {
@@ -17,11 +16,39 @@ func main() {
 	}
 }
 
-func linearSearch(array []int, target int) int {
-	for i, value := range array {
+func linearSearch(arr []int, target int) int {
+	for i, value := range arr {
 		if value == target {
 			return i
 		}
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	array := []int{1, 4, 5, 6, 3, 7, 10, 69}
+// 	target := 7
+
+// 	result := linearSearch(array, target)
+
+// 	if result != -1 {
+// 		fmt.Printf("Element found at index %d\n", result)
+// 	} else {
+// 		fmt.Println("Element not found")
+// 	}
+// }
+
+// func linearSearch(array []int, target int) int {
+// 	for i, value := range array {
+// 		if value == target {
+// 			return i
+// 		}
+// 	}
+// 	return -1
+// }
