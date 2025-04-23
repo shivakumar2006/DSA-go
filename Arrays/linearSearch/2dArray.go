808348423d26068287261c9f3dceeb438ee079db
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-23 22:08:05 +0530

    commit

--- Arrays/linearSearch/2dArray.go
+++ Arrays/linearSearch/2dArray.go
@@ -1,4 +1,4 @@
-//linearSearch in 2D Array...
+// //linearSearch in 2D Array...
 
 package main
 
@@ -13,9 +13,15 @@ func main() {
 		{48, 99, 72, 55},
 		{20, 4, 54},
 	}
-	target := 72
+
+	target := 69
 	result := search(arr, target)
-	fmt.Printf("Element is found at the index of [%d][%d]\n", result[0], result[1])
+
+	if result != nil {
+		fmt.Printf("Element is found at the index of [%d][%d]\n", result[0], result[1])
+	} else {
+		fmt.Println("Element is not found")
+	}
 }
 
 func search(arr [][]int, target int) []int {
@@ -28,3 +34,32 @@ func search(arr [][]int, target int) []int {
 	}
 	return []int{-1, -1}
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := [][]int{
+// 		{23, 4, 1},
+// 		{57, 69, 29},
+// 		{48, 99, 72, 55},
+// 		{20, 4, 54},
+// 	}
+// 	target := 72
+// 	result := search(arr, target)
+// 	fmt.Printf("Element is found at the index of [%d][%d]\n", result[0], result[1])
+// }
+
+// func search(arr [][]int, target int) []int {
+// 	for row := 0; row < len(arr); row++ {
+// 		for col := 0; col < len(arr[row]); col++ {
+// 			if arr[row][col] == target {
+// 				return []int{row, col}
+// 			}
+// 		}
+// 	}
+// 	return []int{-1, -1}
+// }
