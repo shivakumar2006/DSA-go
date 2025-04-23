0f7c65ccf5d8142d267bdc65756bf9e29a12370b
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-23 19:20:32 +0530

    commit

--- Arrays/BinarySearch/binary.go
+++ Arrays/BinarySearch/binary.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	arr := []int{2, 5, 6, 3, 69, 78, 42}
-	fmt.Println(binarySearch(arr, 69))
+	arr := []int{2, 5, 3, 6, 8, 10, 20}
+	fmt.Println(binarySearch(arr, 20))
 }
 
 func binarySearch(arr []int, x int) int {
@@ -22,5 +22,32 @@ func binarySearch(arr []int, x int) int {
 			l = mid + 1
 		}
 	}
-	return 1
+	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 6, 3, 69, 78, 42}
+// 	fmt.Println(binarySearch(arr, 69))
+// }
+
+// func binarySearch(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if arr[mid] == x {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return 1
+// }
--- Arrays/linearSearch/linear.go
+++ Arrays/linearSearch/linear.go
@@ -5,25 +5,89 @@ import (
 )
 
 func main() {
-	arr := []int{2, 5, 3, 7, 69, 100, 45}
-	target := 69
+	arr := []int{-2, 5, -3, 7, 69, -100, 45}
+	target := -100
 
 	result := linearSearch(arr, target)
 	if result != -1 {
-		fmt.Printf("Element found at index %d\n", result)
+		fmt.Printf("Element is found at the index of %d\n", result)
 	} else {
-		fmt.Println("Element not found")
+		fmt.Println("Element is not found")
 	}
 }
 
+// func linearSearch(arr []int , target int) int {
+// 	for i, value := range arr {
+// 		if value == target {
+// 			return i
+// 		}
+// 	}
+// 	return -1
+// }
+
 func linearSearch(arr []int, target int) int {
-	for i, value := range arr {
-		if value == target {
+	for i := 0; i <= len(arr); i++ {
+		if arr[i] == target {
 			return i
 		}
 	}
 	return -1
 }
+
+// BOOL linear search
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 3, 7, 69, 100, 45}
+// 	target := 69
+
+// 	result := linearSearch(arr, target)
+// 	if result != false {
+// 		fmt.Println("Element is found at the index \n", result)
+// 	} else {
+// 		fmt.Println("Element is not found")
+// 	}
+// }
+
+// func linearSearch(arr []int, target int) bool {
+// 	for i := 0; i <= len(arr); i++ {
+// 		if arr[i] == target {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 3, 7, 69, 100, 45}
+// 	target := 69
+
+// 	result := linearSearch(arr, target)
+// 	if result != -1 {
+// 		fmt.Printf("Element found at index %d\n", result)
+// 	} else {
+// 		fmt.Println("Element not found")
+// 	}
+// }
+
+// func linearSearch(arr []int, target int) int {
+// 	for i, value := range arr {
+// 		if value == target {
+// 			return i
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
