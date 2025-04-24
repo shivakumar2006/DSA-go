001e00f4f63f6471a571bbdecbd3d3666b44e08f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-24 13:32:56 +0530

    commit

--- Arrays/linearSearch/2dArray.go
+++ Arrays/linearSearch/2dArray.go
@@ -13,11 +13,11 @@ func main() {
 		{48, 99, 72, 55},
 		{20, 4, 54},
 	}
-	target := 55
-	res := search(arr, target)
+	target := 69
+	result := search(arr, target)
 
-	if res != nil {
-		fmt.Printf("Element is found at the index of [%d, %d]\n", res[0], res[1])
+	if result != nil {
+		fmt.Printf("Element is found at the index of [%d, %d]\n", result[0], result[1])
 	} else {
 		fmt.Println("Element is not found")
 	}
@@ -33,6 +33,40 @@ func search(arr [][]int, target int) []int {
 	}
 	return nil
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := [][]int{
+// 		{23, 4, 1},
+// 		{57, 69, 29},
+// 		{48, 99, 72, 55},
+// 		{20, 4, 54},
+// 	}
+// 	target := 55
+// 	res := search(arr, target)
+
+// 	if res != nil {
+// 		fmt.Printf("Element is found at the index of [%d, %d]\n", res[0], res[1])
+// 	} else {
+// 		fmt.Println("Element is not found")
+// 	}
+// }
+
+// func search(arr [][]int, target int) []int {
+// 	for row := 0; row < len(arr); row++ {
+// 		for col := 0; col < len(arr[row]); col++ {
+// 			if arr[row][col] == target {
+// 				return []int{row, col}
+// 			}
+// 		}
+// 	}
+// 	return nil
+// }
 
 // package main
 
--- Arrays/linearSearch/findMin.go
+++ Arrays/linearSearch/findMin.go
@@ -11,33 +11,60 @@ func main() {
 	fmt.Printf("The minimum number in the array is %d\n", findMin)
 }
 
-// func min(arr []int) int {
-// 	if len(arr) == 0 {
-// 		panic("Array is empty")
-// 	}
-
-// 	smallest := arr[0]
-// 	for _, value := range arr {
-// 		if value < smallest {
-// 			smallest = value
-// 		}
-// 	}
-// 	return smallest
-// }
-
 func min(arr []int) int {
 	if len(arr) == 0 {
 		panic("array is empty")
 	}
 
 	smallest := arr[0]
-	for i := 1; i < len(arr); i++ {
+	for i := 1; i <= len(arr); i++ {
 		if arr[i] < smallest {
 			smallest = arr[i]
 		}
 	}
 	return smallest
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{-2, 5, -3, 7, 69, -100, 45}
+
+// 	findMin := min(arr)
+// 	fmt.Printf("The minimum number in the array is %d\n", findMin)
+// }
+
+// // func min(arr []int) int {
+// // 	if len(arr) == 0 {
+// // 		panic("Array is empty")
+// // 	}
+
+// // 	smallest := arr[0]
+// // 	for _, value := range arr {
+// // 		if value < smallest {
+// // 			smallest = value
+// // 		}
+// // 	}
+// // 	return smallest
+// // }
+
+// func min(arr []int) int {
+// 	if len(arr) == 0 {
+// 		panic("array is empty")
+// 	}
+
+// 	smallest := arr[0]
+// 	for i := 1; i < len(arr); i++ {
+// 		if arr[i] < smallest {
+// 			smallest = arr[i]
+// 		}
+// 	}
+// 	return smallest
+// }
 
 // package main
 
