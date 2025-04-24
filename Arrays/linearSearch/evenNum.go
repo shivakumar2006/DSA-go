3284e07cf13be74b9218a46a06a35e7f76dc9816
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-24 14:28:03 +0530

    commit

--- Arrays/linearSearch/evenNum.go
+++ Arrays/linearSearch/evenNum.go
@@ -1,4 +1,4 @@
-// find number of numbers that have even numbers of digits...
+// // find number of numbers that have even numbers of digits...
 
 package main
 
@@ -9,8 +9,8 @@ import (
 func main() {
 	arr := []int{18, 124, 9, 1764, 98, 1}
 
-	result := evenNumbers(arr)
-	fmt.Printf("count of numners with even numbers of digit : %d\n", result)
+	res := evenNumbers(arr)
+	fmt.Printf("Count of the numbers with even numbers of digit : %d\n", res)
 }
 
 func evenNumbers(arr []int) int {
@@ -30,8 +30,44 @@ func evenDigits(num int) bool {
 
 	digits := 0
 	for num != 0 {
-		num = num / 10
+		num = num / 10 // just remove 1 digit on every iteration and add that 1 evertime until any number is not left for remoing
 		digits++
 	}
 	return digits%2 == 0
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{18, 124, 9, 1764, 98, 1}
+
+// 	result := evenNumbers(arr)
+// 	fmt.Printf("count of numners with even numbers of digit : %d\n", result)
+// }
+
+// func evenNumbers(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if evenDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func evenDigits(num int) bool {
+// 	if num < 0 {
+// 		num = -num
+// 	}
+
+// 	digits := 0
+// 	for num != 0 {
+// 		num = num / 10
+// 		digits++
+// 	}
+// 	return digits%2 == 0
+// }
---
+++ Arrays/linearSearch/oddNum.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{18, 124, 9, 1764, 98, 1}
+
+	res := oddNum(arr)
+	fmt.Printf("Count of the number of odd digits : %d\n", res)
+}
+
+func oddNum(arr []int) int {
+	count := 0
+	for i := 0; i < len(arr); i++ {
+		if oddDigits(arr[i]) {
+			count++
+		}
+	}
+	return count
+}
+
+func oddDigits(num int) bool {
+	if num < 0 {
+		num = -num
+	}
+
+	digits := 0
+	for num != 0 {
+		num = num / 10
+		digits++
+	}
+	return digits%2 != 0
+}
