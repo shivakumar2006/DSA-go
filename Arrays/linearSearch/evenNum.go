2e5cca11cfbeabc216619579042c7ac572860126
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-25 22:06:20 +0530

    commit

--- Arrays/linearSearch/evenNum.go
+++ Arrays/linearSearch/evenNum.go
@@ -10,7 +10,7 @@ func main() {
 	arr := []int{18, 124, 9, 1764, 98, 1}
 
 	res := evenNumbers(arr)
-	fmt.Printf("Count of the numbers with even numbers of digit : %d\n", res)
+	fmt.Printf("Count of the numbers with even number of digits : %d\n", res)
 }
 
 func evenNumbers(arr []int) int {
@@ -30,11 +30,47 @@ func evenDigits(num int) bool {
 
 	digits := 0
 	for num != 0 {
-		num = num / 10 // just remove 1 digit on every iteration and add that 1 evertime until any number is not left for remoing
+		num = num / 10
 		digits++
 	}
 	return digits%2 == 0
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{18, 124, 9, 1764, 98, 1}
+
+// 	res := evenNumbers(arr)
+// 	fmt.Printf("Count of the numbers with even numbers of digit : %d\n", res)
+// }
+
+// func evenNumbers(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if evenDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func evenDigits(num int) bool {
+// 	if num < 0 {
+// 		num = -num
+// 	}
+
+// 	digits := 0
+// 	for num != 0 {
+// 		num = num / 10 // just remove 1 digit on every iteration and add that 1 evertime until any number is not left for remoing
+// 		digits++
+// 	}
+// 	return digits%2 == 0
+// }
 
 // package main
 
--- Arrays/linearSearch/findMax.go
+++ Arrays/linearSearch/findMax.go
@@ -13,7 +13,7 @@ func main() {
 
 func max(arr []int) int {
 	if len(arr) == 0 {
-		panic("Array is empty")
+		panic("array is empty")
 	}
 
 	largest := arr[0]
@@ -24,6 +24,33 @@ func max(arr []int) int {
 	}
 	return largest
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{-2, 5, -3, 7, 69, -100, 45}
+
+// 	findMax := max(arr)
+// 	fmt.Printf("The maximum number is %d\n", findMax)
+// }
+
+// func max(arr []int) int {
+// 	if len(arr) == 0 {
+// 		panic("Array is empty")
+// 	}
+
+// 	largest := arr[0]
+// 	for i := 1; i < len(arr); i++ {
+// 		if arr[i] > largest {
+// 			largest = arr[i]
+// 		}
+// 	}
+// 	return largest
+// }
 
 // func max(arr []int) int {
 // 	if len(arr) == 0 {
