bbf75d260b115cf6aa2e35ac5d5e74a333c0fa47
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-25 21:52:22 +0530

    commit

--- Arrays/linearSearch/string.go
+++ Arrays/linearSearch/string.go
@@ -6,17 +6,17 @@ import (
 
 func main() {
 	str := "shiva"
-	targetChar := "v"
+	target := "v"
 
-	resultStr := search(str, targetChar)
-	if resultStr != -1 {
-		fmt.Printf("Element is found at the index of %d\n", resultStr)
+	res := findChar(str, target)
+	if res != -1 {
+		fmt.Printf("Element is found at the index of %d\n", res)
 	} else {
 		fmt.Println("Element is not found")
 	}
 }
 
-func search(str string, target string) int {
+func findChar(str string, target string) int {
 	for i := 0; i <= len(str); i++ {
 		if string(str[i]) == target {
 			return i
@@ -24,3 +24,30 @@ func search(str string, target string) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	str := "shiva"
+// 	targetChar := "v"
+
+// 	resultStr := search(str, targetChar)
+// 	if resultStr != -1 {
+// 		fmt.Printf("Element is found at the index of %d\n", resultStr)
+// 	} else {
+// 		fmt.Println("Element is not found")
+// 	}
+// }
+
+// func search(str string, target string) int {
+// 	for i := 0; i <= len(str); i++ {
+// 		if string(str[i]) == target {
+// 			return i
+// 		}
+// 	}
+// 	return -1
+// }
