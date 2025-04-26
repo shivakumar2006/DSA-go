2845305075e9b83dda849ada890f8808df155aab
shivakumar2006 <official.shivakumar06@gmail.com>
2025-04-26 18:26:05 +0530

    commit

--- Arrays/BinarySearch/binary.go
+++ Arrays/BinarySearch/binary.go
@@ -6,12 +6,12 @@ import (
 
 func main() {
 	arr := []int{2, 5, 3, 6, 8, 10, 20}
-	fmt.Println(binarySearch(arr, 20))
+	fmt.Println("the index number if binary search is : ", binarySearch(arr, 6))
 }
 
 func binarySearch(arr []int, x int) int {
 	l := 0
-	r := len(arr) - 1
+	r := len(arr)
 	for l <= r {
 		mid := (l + r) / 2
 		if arr[mid] == x {
@@ -24,6 +24,33 @@ func binarySearch(arr []int, x int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 3, 6, 8, 10, 20}
+// 	fmt.Println(binarySearch(arr, 20))
+// }
+
+// func binarySearch(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if arr[mid] == x {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
