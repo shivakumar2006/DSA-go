1ec2045c9d67c9bb3ae028908361f4bbe85678bb
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-06 22:50:09 +0530

    commit

--- Arrays/BinarySearch/findCeiling.go
+++ Arrays/BinarySearch/findCeiling.go
@@ -10,7 +10,7 @@ func main() {
 	arr := []int{2, 4, 5, 6, 9, 14, 16, 18}
 	tar := 15
 	res := findCeiling(arr, tar)
-	fmt.Printf("The Ceiling of the target number is found at the index of : %d\n", res)
+	fmt.Printf("The ceiling of the target number if found at the index of : %d\n", res)
 }
 
 func findCeiling(arr []int, x int) int {
@@ -31,3 +31,35 @@ func findCeiling(arr []int, x int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 4, 5, 6, 9, 14, 16, 18}
+// 	tar := 15
+// 	res := findCeiling(arr, tar)
+// 	fmt.Printf("The Ceiling of the target number is found at the index of : %d\n", res)
+// }
+
+// func findCeiling(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if arr[mid] == x {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if l < len(arr) {
+// 		return l
+// 	}
+// 	return -1
+// }
--- Arrays/BinarySearch/findFloor.go
+++ Arrays/BinarySearch/findFloor.go
@@ -6,9 +6,9 @@ import (
 
 func main() {
 	arr := []int{2, 4, 5, 6, 9, 11, 14, 16, 20}
-	tar := 2
+	tar := 10
 	res := findFloor(arr, tar)
-	fmt.Printf("The floor of the taret number is found at the index of : %d\n", res)
+	fmt.Printf("The floor of the target number us found at the index of : %d\n", res)
 }
 
 func findFloor(arr []int, x int) int {
@@ -29,3 +29,35 @@ func findFloor(arr []int, x int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 4, 5, 6, 9, 11, 14, 16, 20}
+// 	tar := 2
+// 	res := findFloor(arr, tar)
+// 	fmt.Printf("The floor of the taret number is found at the index of : %d\n", res)
+// }
+
+// func findFloor(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if arr[mid] == x {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if r >= 0 {
+// 		return r
+// 	}
+// 	return -1
+// }
