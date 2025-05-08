7d2b3736d38da61b47c7ab925ba00af103ea4476
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-08 17:01:03 +0530

    commit

--- Arrays/BinarySearch/findCeiling.go
+++ Arrays/BinarySearch/findCeiling.go
@@ -9,16 +9,20 @@ import (
 func main() {
 	arr := []int{2, 4, 5, 6, 9, 14, 16, 18}
 	tar := 15
-	res := findCeiling(arr, tar)
-	fmt.Printf("The ceiling of the target number if found at the index of : %d\n", res)
+	res := binarySearch(arr, tar)
+	if res != -1 {
+		fmt.Printf("The ceiling of the target number is found at the index of : %d\n", res)
+	} else {
+		fmt.Println("The ceiling of the target number is not found...")
+	}
 }
 
-func findCeiling(arr []int, x int) int {
+func binarySearch(arr []int, x int) int {
 	l := 0
 	r := len(arr) - 1
 	for l <= r {
 		mid := (l + r) / 2
-		if arr[mid] == x {
+		if x == arr[mid] {
 			return mid
 		} else if x < arr[mid] {
 			r = mid - 1
@@ -31,6 +35,38 @@ func findCeiling(arr []int, x int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 4, 5, 6, 9, 14, 16, 18}
+// 	tar := 15
+// 	res := findCeiling(arr, tar)
+// 	fmt.Printf("The ceiling of the target number if found at the index of : %d\n", res)
+// }
+
+// func findCeiling(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if arr[mid] == x {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if l < len(arr) {
+// 		return l
+// 	}
+// 	return -1
+// }
 
 // package main
 
