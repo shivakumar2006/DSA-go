371c4553ed16b06c427000e309fa759914758944
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-08 21:26:15 +0530

    commit

--- Arrays/BinarySearch/strFloor.go
+++ Arrays/BinarySearch/strFloor.go
@@ -1,12 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	str := []string{"c", "e", "h"}
-	tar := "g"
+	tar := "f"
 	res := floor(str, tar)
-	fmt.Printf("The floor of the target character is found at the index of : %d\n", res)
+	if res != -1 {
+		fmt.Printf("The floor of the target number is found at the index of %d\n", res)
+	} else {
+		fmt.Println("The floor of the target number is not found at any index...")
+	}
 }
 
 func floor(str []string, x string) int {
@@ -14,7 +20,7 @@ func floor(str []string, x string) int {
 	r := len(str) - 1
 	for l <= r {
 		mid := (l + r) / 2
-		if x == str[mid] {
+		if str[mid] == x {
 			return mid
 		} else if x < str[mid] {
 			r = mid - 1
@@ -27,3 +33,33 @@ func floor(str []string, x string) int {
 	}
 	return -1
 }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	str := []string{"c", "e", "h"}
+// 	tar := "g"
+// 	res := floor(str, tar)
+// 	fmt.Printf("The floor of the target character is found at the index of : %d\n", res)
+// }
+
+// func floor(str []string, x string) int {
+// 	l := 0
+// 	r := len(str) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == str[mid] {
+// 			return mid
+// 		} else if x < str[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if r >= 0 {
+// 		return r
+// 	}
+// 	return -1
+// }
