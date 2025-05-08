a681b6a3b59fc383e90e428496ca44e3591d081d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-08 23:09:08 +0530

    commit

--- Arrays/BinarySearch/firstAndLast.go
+++ Arrays/BinarySearch/firstAndLast.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
+	arr := []int{5, 7, 7, 7, 8, 8, 8, 8, 9, 9, 10}
 	tar := 8
 	res := firstAndLast(arr, tar)
 	fmt.Printf("The first and last position of the target is : [%d, %d]\n", res[0], res[1])
@@ -22,12 +22,12 @@ func firstAndLast(arr []int, tar int) []int {
 	if first == -1 && last == -1 {
 		return []int{-1, -1}
 	}
+
 	return []int{first, last}
 }
 
 func findFirst(arr []int, x int) int {
-	l := 0
-	r := len(arr) - 1
+	l, r := 0, len(arr)-1
 	res := -1
 	for l <= r {
 		mid := (l + r) / 2
@@ -44,8 +44,7 @@ func findFirst(arr []int, x int) int {
 }
 
 func findLast(arr []int, x int) int {
-	l := 0
-	r := len(arr) - 1
+	l, r := 0, len(arr)-1
 	res := -1
 	for l <= r {
 		mid := (l + r) / 2
@@ -60,6 +59,65 @@ func findLast(arr []int, x int) int {
 	}
 	return res
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
+// 	tar := 8
+// 	res := firstAndLast(arr, tar)
+// 	fmt.Printf("The first and last position of the target is : [%d, %d]\n", res[0], res[1])
+// }
+
+// func firstAndLast(arr []int, tar int) []int {
+// 	first := findFirst(arr, tar)
+// 	last := findLast(arr, tar)
+
+// 	if first == -1 && last == -1 {
+// 		return []int{-1, -1}
+// 	}
+// 	return []int{first, last}
+// }
+
+// func findFirst(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			r = mid - 1
+// 		} else if x > arr[mid] {
+// 			l = mid + 1
+// 		} else {
+// 			r = mid - 1
+// 		}
+// 	}
+// 	return res
+// }
+
+// func findLast(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			l = mid + 1
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return res
+// }
 
 // package main
 
