4660b8122c4410d3498c293581f943b340f0bae0
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-08 22:21:17 +0530

    commit

---
+++ Arrays/BinarySearch/firstAndLast.go
@@ -0,0 +1,57 @@
+// // find the first and last position of the target in the sorted array...
+
+// now we more optimized this code like if the both index is not found then it return [-1, -1]
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
+// 	tar := 7
+// 	first := findFirst(arr, tar)
+// 	last := findLast(arr, tar)
+// 	if first != -1 && last != -1 {
+// 		fmt.Printf("The first and last position of the target index is : [%d, %d]\n", first, last)
+// 	} else {
+// 		fmt.Println("The target both position is not found...")
+// 	}
+// }
+
+// func findFirst(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			r = mid - 1
+// 		} else if x > arr[mid] {
+// 			l = mid + 1
+// 		} else {
+// 			r = mid - 1
+// 		}
+// 	}
+// 	return res
+// }
+
+// func findLast(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			l = mid + 1
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return res
+// }
