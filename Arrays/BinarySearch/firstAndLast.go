93261e40119a36f6195746d70cb1b4530cc4e273
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-08 22:57:33 +0530

    comit

--- Arrays/BinarySearch/firstAndLast.go
+++ Arrays/BinarySearch/firstAndLast.go
@@ -2,6 +2,65 @@
 
 // now we more optimized this code like if the both index is not found then it return [-1, -1]
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
+	tar := 8
+	res := firstAndLast(arr, tar)
+	fmt.Printf("The first and last position of the target is : [%d, %d]\n", res[0], res[1])
+}
+
+func firstAndLast(arr []int, tar int) []int {
+	first := findFirst(arr, tar)
+	last := findLast(arr, tar)
+
+	if first == -1 && last == -1 {
+		return []int{-1, -1}
+	}
+	return []int{first, last}
+}
+
+func findFirst(arr []int, x int) int {
+	l := 0
+	r := len(arr) - 1
+	res := -1
+	for l <= r {
+		mid := (l + r) / 2
+		if x == arr[mid] {
+			res = mid
+			r = mid - 1
+		} else if x > arr[mid] {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return res
+}
+
+func findLast(arr []int, x int) int {
+	l := 0
+	r := len(arr) - 1
+	res := -1
+	for l <= r {
+		mid := (l + r) / 2
+		if x == arr[mid] {
+			res = mid
+			l = mid + 1
+		} else if x < arr[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return res
+}
+
 // package main
 
 // import (
