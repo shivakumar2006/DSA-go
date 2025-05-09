79262dbc090f26cda3e568ca162bccba7cfd1da9
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-09 22:28:41 +0530

    commit

--- Arrays/BinarySearch/firstAndLast.go
+++ Arrays/BinarySearch/firstAndLast.go
@@ -12,7 +12,7 @@ func main() {
 	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
 	tar := 7
 	res := firstAndLast(arr, tar)
-	fmt.Printf("The first and last posotion of the target index is : [%d, %d]\n", res[0], res[1])
+	fmt.Printf("The fist and last position of the target index is : [%d, %d]\n", res[0], res[1])
 }
 
 func firstAndLast(arr []int, tar int) []int {
@@ -59,6 +59,64 @@ func findLast(arr []int, x int) int {
 	}
 	return res
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
+// 	tar := 7
+// 	res := firstAndLast(arr, tar)
+// 	fmt.Printf("The first and last posotion of the target index is : [%d, %d]\n", res[0], res[1])
+// }
+
+// func firstAndLast(arr []int, tar int) []int {
+// 	first := findFirst(arr, tar)
+// 	last := findLast(arr, tar)
+
+// 	if first == -1 && last == -1 {
+// 		return []int{-1, -1}
+// 	}
+
+// 	return []int{first, last}
+// }
+
+// func findFirst(arr []int, x int) int {
+// 	l, r := 0, len(arr)-1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			r = mid - 1
+// 		} else if x > arr[mid] {
+// 			l = mid + 1
+// 		} else {
+// 			r = mid - 1
+// 		}
+// 	}
+// 	return res
+// }
+
+// func findLast(arr []int, x int) int {
+// 	l, r := 0, len(arr)-1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			l = mid + 1
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return res
+// }
 
 // package main
 
