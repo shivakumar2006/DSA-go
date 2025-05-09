c4be6dfa364a821735efc4fcbf7de1baf78a3784
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-10 00:09:49 +0530

    commit

--- Arrays/BinarySearch/infinite.go
+++ Arrays/BinarySearch/infinite.go
@@ -21,31 +21,82 @@ func main() {
 }
 
 func search(tar int) int {
-	l := 0
-	r := 1
+	start := 0
+	end := 1
 
-	for get(r) < tar {
-		l = r
-		r *= 2
+	for tar > arr[end] {
+		temp := end + 1
+		end = end + (end-start+1)*2
+		start = temp
+
+		// Add boundary check to avoid panic
+		if end >= len(arr) {
+			end = len(arr) - 1
+			break
+		}
 	}
+	return binarySearch(arr, tar, start, end)
+}
 
-	for l <= r {
-		mid := l + (r-l)/2
-		val := get(mid)
-		if val == tar {
-			return mid
-		} else if val < tar {
-			l = mid + 1
+func binarySearch(arr []int, tar int, start int, end int) int {
+	for start <= end {
+		mid := start + (end-start)/2
+
+		if tar < arr[mid] {
+			end = mid - 1
+		} else if tar > arr[mid] {
+			start = mid + 1
 		} else {
-			r = mid - 1
+			return mid
 		}
 	}
 	return -1
 }
 
-func get(res int) int {
-	if res >= len(arr) {
-		return int(^uint(0) >> 1)
-	}
-	return arr[res]
-}
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// var arr = []int{2, 4, 5, 7, 9, 11, 13, 16, 20, 25, 30, 35, 40}
+
+// func main() {
+// 	tar := 25
+// 	res := search(tar)
+// 	if res != -1 {
+// 		fmt.Printf("Target %d found at the index : %d\n", tar, res)
+// 	} else {
+// 		fmt.Println("Target not found...")
+// 	}
+// }
+
+// func search(tar int) int {
+// 	l := 0
+// 	r := 1
+
+// 	for get(r) < tar {
+// 		l = r
+// 		r *= 2
+// 	}
+
+// 	for l <= r {
+// 		mid := l + (r-l)/2
+// 		val := get(mid)
+// 		if val == tar {
+// 			return mid
+// 		} else if val < tar {
+// 			l = mid + 1
+// 		} else {
+// 			r = mid - 1
+// 		}
+// 	}
+// 	return -1
+// }
+
+// func get(res int) int {
+// 	if res >= len(arr) {
+// 		return int(^uint(0) >> 1) or return math.MaxInt
+// 	}
+// 	return arr[res]
+// }
