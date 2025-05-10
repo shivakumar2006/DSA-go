d6bce23be3600031707d2c337299fcbbb0528d08
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-10 18:03:30 +0530

    commit

--- Arrays/linearSearch/oddNum.go
+++ Arrays/linearSearch/oddNum.go
@@ -5,10 +5,9 @@ import (
 )
 
 func main() {
-	arr := []int{18, 124, 9, 1764, 98, 1}
-
+	arr := []int{18, 124, 9, 1764, 98, 1, 69}
 	res := oddNum(arr)
-	fmt.Printf("Coount of the number of digits is : %d\n", res)
+	fmt.Printf("Count of the number of digit is : %d\n", res)
 }
 
 func oddNum(arr []int) int {
@@ -33,6 +32,42 @@ func oddDigits(num int) bool {
 	}
 	return digits%2 != 0
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{18, 124, 9, 1764, 98, 1}
+
+// 	res := oddNum(arr)
+// 	fmt.Printf("Coount of the number of digits is : %d\n", res)
+// }
+
+// func oddNum(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if oddDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func oddDigits(num int) bool {
+// 	if num < 0 {
+// 		num = -num
+// 	}
+
+// 	digits := 0
+// 	for num != 0 {
+// 		num = num / 10
+// 		digits++
+// 	}
+// 	return digits%2 != 0
+// }
 
 // package main
 
