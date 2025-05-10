5c4012f9215536a92415bb3ac4e60d6df104dcc2
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-10 17:55:18 +0530

    commit

--- Arrays/linearSearch/evenNum.go
+++ Arrays/linearSearch/evenNum.go
@@ -7,10 +7,9 @@ import (
 )
 
 func main() {
-	arr := []int{18, 124, 9, 1764, 98, 1}
-
+	arr := []int{18, 124, 9, 1764, 98, 1, 69}
 	res := evenNumbers(arr)
-	fmt.Printf("Count of the numbers with even number of digits : %d\n", res)
+	fmt.Printf("Count of the numbers with even numbers of digits : %d\n", res)
 }
 
 func evenNumbers(arr []int) int {
@@ -35,6 +34,42 @@ func evenDigits(num int) bool {
 	}
 	return digits%2 == 0
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{18, 124, 9, 1764, 98, 1}
+
+// 	res := evenNumbers(arr)
+// 	fmt.Printf("Count of the numbers with even number of digits : %d\n", res)
+// }
+
+// func evenNumbers(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if evenDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func evenDigits(num int) bool {
+// 	if num < 0 {
+// 		num = -num
+// 	}
+
+// 	digits := 0
+// 	for num != 0 {
+// 		num = num / 10
+// 		digits++
+// 	}
+// 	return digits%2 == 0
+// }
 
 // package main
 
