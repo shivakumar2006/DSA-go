56f38a47d8954c805031cf75adf1e1e56a29f23e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-11 17:34:44 +0530

    commit

--- Arrays/BinarySearch/peak.go
+++ Arrays/BinarySearch/peak.go
@@ -7,25 +7,54 @@ import (
 )
 
 func main() {
-	arr := []int{0, 1, 0}
-	res := mountainArray(arr)
+	arr := []int{1, 2, 4, 5, 3, 2, 1}
+	res := peak(arr)
 	fmt.Printf("Peak index is : %d\n", res)
 }
 
-func mountainArray(arr []int) int {
+func peak(arr []int) int {
 	start := 0
 	end := len(arr) - 1
 
 	for start < end {
-		mid := start + (end-start)/2
+		mid := start + (end-start)/2 // (start + end) / 2
 		if arr[mid] > arr[mid+1] {
+			// means we are in descreasing array
 			end = mid
 		} else {
+			// means we are in increasing array
 			start = mid + 1
 		}
 	}
 	return start
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{0, 1, 0}
+// 	res := mountainArray(arr)
+// 	fmt.Printf("Peak index is : %d\n", res)
+// }
+
+// func mountainArray(arr []int) int {
+// 	start := 0
+// 	end := len(arr) - 1
+
+// 	for start < end {
+// 		mid := start + (end-start)/2
+// 		if arr[mid] > arr[mid+1] {
+// 			end = mid
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return start
+// }
 
 // package main
 
