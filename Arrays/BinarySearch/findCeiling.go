99bb6c15d010a735b18e91cb371f32020a29e041
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-12 22:26:42 +0530

    commit

--- Arrays/BinarySearch/findCeiling.go
+++ Arrays/BinarySearch/findCeiling.go
@@ -8,32 +8,102 @@ import (
 
 func main() {
 	arr := []int{2, 4, 5, 6, 9, 14}
-	tar := 7
+	tar := 12
 	res := findCeiling(arr, tar)
 	if res != -1 {
-		fmt.Printf("The ceiling of the target number is found at the index of : %d\n", res)
+		fmt.Printf("The ceiling of the target number is found at the index : %d\n", res)
 	} else {
-		fmt.Println("The ceiling of the tsrget number is not found...")
+		fmt.Println("Target is not found")
 	}
 }
 
 func findCeiling(arr []int, x int) int {
-	l, r := 0, len(arr)-1
-	for l <= r {
-		mid := (l + r) / 2
+	start, end := 0, len(arr)-1
+	for start <= end {
+		mid := start + (end-start)/2
 		if x == arr[mid] {
 			return mid
 		} else if x < arr[mid] {
-			r = mid - 1
+			end = mid - 1
 		} else {
-			l = mid + 1
+			start = mid + 1
 		}
 	}
-	if l < len(arr) {
-		return l
+	if start < len(arr) {
+		return start
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 4, 5, 6, 9, 14}
+// 	tar := 8
+// 	res := findCeiling(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("The ceiling of the target number is found at the index : %d\n", res)
+// 	} else {
+// 		fmt.Println("target number is not found")
+// 	}
+// }
+
+// func findCeiling(arr []int, x int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if x == arr[mid] {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	if start < len(arr) {
+// 		return start
+// 	}
+// 	return -1
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 4, 5, 6, 9, 14}
+// 	tar := 7
+// 	res := findCeiling(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("The ceiling of the target number is found at the index of : %d\n", res)
+// 	} else {
+// 		fmt.Println("The ceiling of the tsrget number is not found...")
+// 	}
+// }
+
+// func findCeiling(arr []int, x int) int {
+// 	l, r := 0, len(arr)-1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if l < len(arr) {
+// 		return l
+// 	}
+// 	return -1
+// }
 
 // package main
 
