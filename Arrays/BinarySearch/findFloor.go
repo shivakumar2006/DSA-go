fd8a1557de2d2d03b1d6f569d0485ded0c44acf1
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-12 22:36:37 +0530

    commit

--- Arrays/BinarySearch/findFloor.go
+++ Arrays/BinarySearch/findFloor.go
@@ -6,33 +6,68 @@ import (
 
 func main() {
 	arr := []int{2, 4, 5, 6, 9, 11, 14, 16, 20}
-	tar := 10
+	tar := 18
 	res := findFloor(arr, tar)
 	if res != -1 {
-		fmt.Printf("The floor of the target number is found at the index of : %d\n", res)
+		fmt.Printf("The floor of the target number is found at the index : %d\n", res)
 	} else {
-		fmt.Println("THe floor of the target numner is not found...")
+		fmt.Println("The floor of the target is not found")
 	}
 }
 
 func findFloor(arr []int, x int) int {
-	l := 0
-	r := len(arr) - 1
-	for l <= r {
-		mid := (l + r) / 2
-		if arr[mid] == x {
+	start, end := 0, len(arr)-1
+	for start <= end {
+		mid := start + (end-start)/2
+		if x == arr[mid] {
 			return mid
 		} else if x < arr[mid] {
-			r = mid - 1
+			end = mid - 1
 		} else {
-			l = mid + 1
+			start = mid + 1
 		}
 	}
-	if r >= 0 {
-		return r
+	if end >= 0 {
+		return end
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 4, 5, 6, 9, 11, 14, 16, 20}
+// 	tar := 10
+// 	res := findFloor(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("The floor of the target number is found at the index of : %d\n", res)
+// 	} else {
+// 		fmt.Println("THe floor of the target numner is not found...")
+// 	}
+// }
+
+// func findFloor(arr []int, x int) int {
+// 	l := 0
+// 	r := len(arr) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if arr[mid] == x {
+// 			return mid
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if r >= 0 {
+// 		return r
+// 	}
+// 	return -1
+// }
 
 // package main
 
