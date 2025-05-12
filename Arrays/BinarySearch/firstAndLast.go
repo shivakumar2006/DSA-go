e7240fda98d535194ce661c9d8e64c450293ab3f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-12 22:52:29 +0530

    commit

--- Arrays/BinarySearch/firstAndLast.go
+++ Arrays/BinarySearch/firstAndLast.go
@@ -9,10 +9,14 @@ import (
 )
 
 func main() {
-	arr := []int{5, 7, 7, 8, 8, 8, 8, 10}
-	tar := 8
+	arr := []int{5, 7, 7, 7, 7, 8, 8, 10}
+	tar := 7
 	res := firstAndLast(arr, tar)
-	fmt.Printf("The first and last position of the target index is : [%d, %d]\n", res[0], res[1])
+	if res != nil {
+		fmt.Printf("THe first and last position of the target number of index : [%d, %d]\n", res[0], res[1])
+	} else {
+		fmt.Println("Target not found")
+	}
 }
 
 func firstAndLast(arr []int, tar int) []int {
@@ -27,38 +31,96 @@ func firstAndLast(arr []int, tar int) []int {
 }
 
 func findFirst(arr []int, x int) int {
-	l, r := 0, len(arr)-1
+	start, end := 0, len(arr)-1
 	res := -1
-	for l <= r {
-		mid := (l + r) / 2
+	for start <= end {
+		mid := start + (end-start)/2
 		if x == arr[mid] {
 			res = mid
-			r = mid - 1
-		} else if x > arr[mid] {
-			l = mid + 1
+			end = mid - 1
+		} else if x < arr[mid] {
+			end = mid - 1
 		} else {
-			r = mid - 1
+			start = mid + 1
 		}
 	}
 	return res
 }
 
 func findLast(arr []int, x int) int {
-	l, r := 0, len(arr)-1
+	start, end := 0, len(arr)-1
 	res := -1
-	for l <= r {
-		mid := (l + r) / 2
+	for start <= end {
+		mid := start + (end-start)/2
 		if x == arr[mid] {
 			res = mid
-			l = mid + 1
+			start = mid + 1
 		} else if x < arr[mid] {
-			r = mid - 1
+			end = mid - 1
 		} else {
-			l = mid + 1
+			start = mid + 1
 		}
 	}
 	return res
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{5, 7, 7, 8, 8, 8, 8, 10}
+// 	tar := 8
+// 	res := firstAndLast(arr, tar)
+// 	fmt.Printf("The first and last position of the target index is : [%d, %d]\n", res[0], res[1])
+// }
+
+// func firstAndLast(arr []int, tar int) []int {
+// 	first := findFirst(arr, tar)
+// 	last := findLast(arr, tar)
+
+// 	if first == -1 && last == -1 {
+// 		return []int{-1, -1}
+// 	}
+
+// 	return []int{first, last}
+// }
+
+// func findFirst(arr []int, x int) int {
+// 	l, r := 0, len(arr)-1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			r = mid - 1
+// 		} else if x > arr[mid] {
+// 			l = mid + 1
+// 		} else {
+// 			r = mid - 1
+// 		}
+// 	}
+// 	return res
+// }
+
+// func findLast(arr []int, x int) int {
+// 	l, r := 0, len(arr)-1
+// 	res := -1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == arr[mid] {
+// 			res = mid
+// 			l = mid + 1
+// 		} else if x < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return res
+// }
 
 // package main
 
