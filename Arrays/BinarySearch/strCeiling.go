ce2df4675ed98aff4122a546fee0b4708aa5a067
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-12 22:32:16 +0530

    commit

--- Arrays/BinarySearch/strCeiling.go
+++ Arrays/BinarySearch/strCeiling.go
@@ -8,24 +8,59 @@ func main() {
 	str := []string{"c", "f", "g"}
 	tar := "d"
 	res := ceiling(str, tar)
-	fmt.Printf("The Index of the ceiling number is found : %d\n", res)
+	if res != -1 {
+		fmt.Printf("THe index of the ceiling number is found : %d\n", res)
+	} else {
+		fmt.Println("Target index is not found")
+	}
 }
 
 func ceiling(str []string, x string) int {
-	l := 0
-	r := len(str) - 1
-	for l <= r {
-		mid := (l + r) / 2
+	start, end := 0, len(str)-1
+	for start <= end {
+		mid := start + (end-start)/2
 		if x == str[mid] {
 			return mid
 		} else if x < str[mid] {
-			r = mid - 1
+			end = mid - 1
 		} else {
-			l = mid + 1
+			start = mid + 1
 		}
 	}
-	if l < len(str) {
-		return l
+	if start < len(str) {
+		return start
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	str := []string{"c", "f", "g"}
+// 	tar := "d"
+// 	res := ceiling(str, tar)
+// 	fmt.Printf("The Index of the ceiling number is found : %d\n", res)
+// }
+
+// func ceiling(str []string, x string) int {
+// 	l := 0
+// 	r := len(str) - 1
+// 	for l <= r {
+// 		mid := (l + r) / 2
+// 		if x == str[mid] {
+// 			return mid
+// 		} else if x < str[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	if l < len(str) {
+// 		return l
+// 	}
+// 	return -1
+// }
