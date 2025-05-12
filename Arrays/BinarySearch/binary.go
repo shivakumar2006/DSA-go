3fffc6199b6cec3c2106222a7c07b9cacaa3ded3
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-12 22:13:36 +0530

    commit

--- Arrays/BinarySearch/binary.go
+++ Arrays/BinarySearch/binary.go
@@ -6,29 +6,61 @@ import (
 
 func main() {
 	arr := []int{2, 5, 3, 6, 8, 10, 20}
-	tar := 5
+	tar := 20
 	res := binarySearch(arr, tar)
 	if res != -1 {
-		fmt.Printf("The index number of binarySearch is : %d\n", res)
+		fmt.Printf("The target number %d found at the index : %d\n", tar, res)
 	} else {
-		fmt.Println("THe index of the number is not found")
+		fmt.Println("Target not found")
 	}
 }
 
-func binarySearch(arr []int, tar int) int {
-	l, r := 0, len(arr)-1
-	for l <= r {
-		mid := l + (r-l)/2
-		if tar == arr[mid] {
+func binarySearch(arr []int, x int) int {
+	start, end := 0, len(arr)-1
+	for start <= end {
+		mid := start + (end-start)/2
+		if x == arr[mid] {
 			return mid
-		} else if tar < arr[mid] {
-			r = mid - 1
+		} else if x < arr[mid] {
+			end = mid - 1
 		} else {
-			l = mid + 1
+			start = mid + 1
 		}
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 3, 6, 8, 10, 20}
+// 	tar := 5
+// 	res := binarySearch(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("The index number of binarySearch is : %d\n", res)
+// 	} else {
+// 		fmt.Println("THe index of the number is not found")
+// 	}
+// }
+
+// func binarySearch(arr []int, tar int) int {
+// 	l, r := 0, len(arr)-1
+// 	for l <= r {
+// 		mid := l + (r-l)/2
+// 		if tar == arr[mid] {
+// 			return mid
+// 		} else if tar < arr[mid] {
+// 			r = mid - 1
+// 		} else {
+// 			l = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
