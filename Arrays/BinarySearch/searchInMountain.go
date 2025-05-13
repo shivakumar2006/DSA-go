e6803476e4dac133c7614c8939cf5649573996fd
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-13 14:01:06 +0530

    commit

--- Arrays/BinarySearch/searchInMountain.go
+++ Arrays/BinarySearch/searchInMountain.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	arr := []int{1, 2, 3, 5, 6, 4, 2, 1}
 	tar := 5
-	res := searchInMountain(arr, tar)
+	res := searchInMountainArray(arr, tar)
 	if res != -1 {
-		fmt.Printf("The index of the target is : %d\n", res)
+		fmt.Printf("The target index is found : %d\n", res)
 	} else {
 		fmt.Println("Target not found")
 	}
@@ -30,42 +30,108 @@ func findPeak(arr []int) int {
 	return start
 }
 
-func searchInMountain(arr []int, tar int) int {
+func searchInMountainArray(arr []int, tar int) int {
 	peak := findPeak(arr)
 
-	// search in ascending part
-	index := binarySearch(arr, tar, 0, peak, true)
-	if index != -1 {
-		return index
+	// searching in increasing part of the array...
+	find := binarySearch(arr, tar, 0, peak, true)
+	if find != -1 {
+		return find
 	}
-
-	// searcn in descending oreder
+	// serching in decreasing order...
 	return binarySearch(arr, tar, peak+1, len(arr)-1, false)
 }
 
-// order-agnostic binarySarch
 func binarySearch(arr []int, tar int, start int, end int, asc bool) int {
-	for start <= end {
+	for start < end {
 		mid := start + (end-start)/2
-		if arr[mid] == tar {
+		if tar == arr[mid] {
 			return mid
 		}
-		if asc {
+
+		if asc { // if we searching on increasing part of the array then we use this (on[1, 2, 3, 4, 5, 6])
 			if tar < arr[mid] {
 				end = mid - 1
 			} else {
 				start = mid + 1
 			}
-		} else {
+		} else { // if we searching on decreasing part of the array then we use else function (on[4, 2, 1])
 			if tar > arr[mid] {
-				end = mid - 1
-			} else {
 				start = mid + 1
+			} else {
+				end = mid - 1
 			}
 		}
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{1, 2, 3, 5, 6, 4, 2, 1}
+// 	tar := 5
+// 	res := searchInMountain(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("The index of the target is : %d\n", res)
+// 	} else {
+// 		fmt.Println("Target not found")
+// 	}
+// }
+
+// func findPeak(arr []int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start < end {
+// 		mid := start + (end-start)/2
+// 		if arr[mid] > arr[mid+1] {
+// 			end = mid
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return start
+// }
+
+// func searchInMountain(arr []int, tar int) int {
+// 	peak := findPeak(arr)
+
+// 	// search in ascending part
+// 	index := binarySearch(arr, tar, 0, peak, true)
+// 	if index != -1 {
+// 		return index
+// 	}
+
+// 	// searcn in descending oreder
+// 	return binarySearch(arr, tar, peak+1, len(arr)-1, false)
+// }
+
+// // order-agnostic binarySarch
+// func binarySearch(arr []int, tar int, start int, end int, asc bool) int {
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if arr[mid] == tar {
+// 			return mid
+// 		}
+// 		if asc {
+// 			if tar < arr[mid] {
+// 				end = mid - 1
+// 			} else {
+// 				start = mid + 1
+// 			}
+// 		} else {
+// 			if tar > arr[mid] {
+// 				end = mid - 1
+// 			} else {
+// 				start = mid + 1
+// 			}
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
