93dd5676387ce7eb362af1a991ba72b75a589844
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-14 06:52:18 +0530

    commit

---
+++ Arrays/BinarySearch/rotatedSorted.go
@@ -0,0 +1,31 @@
+// search in rotated sorted array
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{3, 4, 5, 6, 7, 0, 1, 2}
+	fmt.Println("The pivot is found at the index : ", findPivot(arr))
+}
+
+func findPivot(arr []int) int {
+	start, end := 0, len(arr)-1
+	for start <= end {
+		mid := start + (end-start)/2
+
+		//check if mid is pivot
+		if mid < end && arr[mid] > arr[mid+1] {
+			return mid
+		} else if mid > start && arr[mid] < arr[mid-1] { // if mid - 1 is pivot then
+			return mid - 1
+		} else if arr[mid] <= arr[start] {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	return -1
+}
