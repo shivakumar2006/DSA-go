fd41d89d5d31af47346c0020de86f735585f3bd6
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-14 18:26:56 +0530

    commit

--- Arrays/BinarySearch/rotatedSorted.go
+++ Arrays/BinarySearch/rotatedSorted.go
@@ -8,27 +8,42 @@ import (
 
 func main() {
 	arr := []int{3, 4, 5, 6, 7, 0, 1, 2}
-	fmt.Println("THe pivot is found at the index : ", findPivot(arr))
+	fmt.Printf("The pivot is found at the index : ", findPivot(arr))
 }
 
 func findPivot(arr []int) int {
-	start, end := 0, len(arr)-1
-	for start <= end {
-		mid := start + (end-start)/2
-		if mid < end && arr[mid] > arr[mid+1] {
-			return mid
-		}
-		if mid > start && arr[mid] < arr[mid-1] {
-			return mid - 1
-		}
-		if arr[mid] <= arr[start] {
-			end = mid - 1
-		} else {
-			start = mid + 1
-		}
-	}
-	return -1
+
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{3, 4, 5, 6, 7, 0, 1, 2}
+// 	fmt.Println("THe pivot is found at the index : ", findPivot(arr))
+// }
+
+// func findPivot(arr []int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if mid < end && arr[mid] > arr[mid+1] {
+// 			return mid
+// 		}
+// 		if mid > start && arr[mid] < arr[mid-1] {
+// 			return mid - 1
+// 		}
+// 		if arr[mid] <= arr[start] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
