810f12efee4317ce2a42d33106f364692396aa26
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-16 23:36:19 +0530

    commit

--- Arrays/BinarySearch/duplicateRotatedSorted.go
+++ Arrays/BinarySearch/duplicateRotatedSorted.go
@@ -20,7 +20,7 @@ func main() {
 func search(arr []int, tar int) int {
 	pivot := findPivot(arr)
 
-	if pivot == -1 {
+	if pivot != -1 {
 		return binarySearch(arr, tar, 0, len(arr)-1)
 	}
 	if tar >= arr[0] {
@@ -73,6 +73,80 @@ func binarySearch(arr []int, tar int, start int, end int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 2, 2, 2, 2, 9}
+// 	tar := 9
+// 	res := search(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("THe target element indexsound : %d\n", res)
+// 	} else {
+// 		fmt.Println("Target  not found")
+// 	}
+// }
+
+// func search(arr []int, tar int) int {
+// 	pivot := findPivot(arr)
+
+// 	if pivot == -1 {
+// 		return binarySearch(arr, tar, 0, len(arr)-1)
+// 	}
+// 	if tar >= arr[0] {
+// 		return binarySearch(arr, tar, 0, pivot-1)
+// 	}
+// 	return binarySearch(arr, tar, pivot+1, len(arr)-1)
+// }
+
+// func findPivot(arr []int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if mid < end && arr[mid] > arr[mid+1] {
+// 			return mid
+// 		}
+// 		if mid > start && arr[mid] < arr[mid-1] {
+// 			return mid - 1
+// 		}
+
+// 		// now we handle duplicate
+// 		if arr[start] == arr[mid] && arr[mid] == arr[end] {
+// 			if start < end && arr[start] > arr[start+1] {
+// 				return start
+// 			}
+// 			start++
+
+// 			if end > start && arr[end] < arr[end-1] {
+// 				return end - 1
+// 			}
+// 			end--
+// 		} else if arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end]) {
+// 			start = mid + 1
+// 		} else {
+// 			end = mid - 1
+// 		}
+// 	}
+// 	return -1
+// }
+
+// func binarySearch(arr []int, tar int, start int, end int) int {
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			return mid
+// 		} else if tar < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
