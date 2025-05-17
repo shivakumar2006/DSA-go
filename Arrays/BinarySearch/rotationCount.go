712613d82b32153417452a4161315a497bf4e324
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-17 23:05:35 +0530

    comit

--- Arrays/BinarySearch/rotationCount.go
+++ Arrays/BinarySearch/rotationCount.go
@@ -11,10 +11,7 @@ func main() {
 	fmt.Println(rotation(arr))
 }
 
-func rotation(arr []int) int {
-	pivot := findPivot(arr)
-	return pivot + 1
-}
+func rotation()
 
 func findPivot(arr []int) int {
 	start, end := 0, len(arr)-1
@@ -34,6 +31,41 @@ func findPivot(arr []int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{4, 5, 6, 7, 0, 1, 2}
+// 	fmt.Println(rotation(arr))
+// }
+
+// func rotation(arr []int) int {
+// 	pivot := findPivot(arr)
+// 	return pivot + 1
+// }
+
+// func findPivot(arr []int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if mid < end && arr[mid] > arr[mid+1] {
+// 			return mid
+// 		}
+// 		if mid > start && arr[mid] < arr[mid-1] {
+// 			return mid - 1
+// 		}
+// 		if arr[mid] <= arr[start] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
