45d64165474e418c57eada3202513d47dee8760f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-17 15:30:04 +0530

    commit

--- Arrays/BinarySearch/searchInMountain.go
+++ Arrays/BinarySearch/searchInMountain.go
@@ -8,13 +8,23 @@ import (
 
 func main() {
 	arr := []int{1, 2, 3, 5, 6, 4, 0}
-	tar := 0
-	res := searchInMountainArray(arr, tar)
+	tar := 4
+	res := search(arr, tar)
 	if res != -1 {
 		fmt.Printf("The target index is found : %d\n", res)
 	} else {
-		fmt.Println("Targe not found")
+		fmt.Println("Target nof found")
 	}
+}
+
+func search(arr []int, tar int) int {
+	peak := findPeak(arr)
+
+	findIndex := binarySearch(arr, tar, 0, peak, true)
+	if findIndex != -1 {
+		return findIndex
+	}
+	return binarySearch(arr, tar, peak+1, len(arr)-1, false)
 }
 
 func findPeak(arr []int) int {
@@ -28,16 +38,6 @@ func findPeak(arr []int) int {
 		}
 	}
 	return start
-}
-
-func searchInMountainArray(arr []int, tar int) int {
-	peak := findPeak(arr)
-
-	findIndex := binarySearch(arr, tar, 0, peak, true)
-	if findIndex != -1 {
-		return findIndex
-	}
-	return binarySearch(arr, tar, peak+1, len(arr)-1, false)
 }
 
 func binarySearch(arr []int, tar int, start int, end int, asc bool) int {
@@ -62,6 +62,69 @@ func binarySearch(arr []int, tar int, start int, end int, asc bool) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{1, 2, 3, 5, 6, 4, 0}
+// 	tar := 0
+// 	res := searchInMountainArray(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("The target index is found : %d\n", res)
+// 	} else {
+// 		fmt.Println("Targe not found")
+// 	}
+// }
+
+// func findPeak(arr []int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start < end {
+// 		mid := start + (end-start)/2
+// 		if arr[mid] > arr[mid+1] {
+// 			end = mid
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return start
+// }
+
+// func searchInMountainArray(arr []int, tar int) int {
+// 	peak := findPeak(arr)
+
+// 	findIndex := binarySearch(arr, tar, 0, peak, true)
+// 	if findIndex != -1 {
+// 		return findIndex
+// 	}
+// 	return binarySearch(arr, tar, peak+1, len(arr)-1, false)
+// }
+
+// func binarySearch(arr []int, tar int, start int, end int, asc bool) int {
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			return mid
+// 		}
+// 		if asc {
+// 			if tar < arr[mid] {
+// 				end = mid - 1
+// 			} else {
+// 				start = mid + 1
+// 			}
+// 		} else {
+// 			if tar < arr[mid] {
+// 				start = mid + 1
+// 			} else {
+// 				end = mid - 1
+// 			}
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
