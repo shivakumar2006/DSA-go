bd15fe4a5dd0c20eb678b33cac19c6077e18ce2f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-19 23:05:49 +0530

    commit

--- Arrays/BinarySearch/bsIn2DArray/RowColMatrix.go
+++ Arrays/BinarySearch/bsIn2DArray/RowColMatrix.go
@@ -13,10 +13,10 @@ func main() {
 		{28, 29, 37, 49},
 		{33, 34, 38, 50},
 	}
-	tar := 37
+	tar := 38
 	res := search(arr, tar)
 	if res[0] != -1 {
-		fmt.Printf("The target found : [%d, %d]\n", res[0], res[1])
+		fmt.Printf("The target is found : [%d, %d]\n", res[0], res[1])
 	} else {
 		fmt.Println("Target not found")
 	}
@@ -24,7 +24,7 @@ func main() {
 
 func search(arr [][]int, tar int) []int {
 	row := 0
-	col := len(arr[0]) - 1
+	col := len(arr) - 1
 	for row < len(arr) && col >= 0 {
 		if arr[row][col] == tar {
 			return []int{row, col}
@@ -37,3 +37,41 @@ func search(arr [][]int, tar int) []int {
 	}
 	return []int{-1, -1}
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := [][]int{
+// 		{10, 20, 30, 40},
+// 		{15, 25, 35, 45},
+// 		{28, 29, 37, 49},
+// 		{33, 34, 38, 50},
+// 	}
+// 	tar := 37
+// 	res := search(arr, tar)
+// 	if res[0] != -1 {
+// 		fmt.Printf("The target found : [%d, %d]\n", res[0], res[1])
+// 	} else {
+// 		fmt.Println("Target not found")
+// 	}
+// }
+
+// func search(arr [][]int, tar int) []int {
+// 	row := 0
+// 	col := len(arr[0]) - 1
+// 	for row < len(arr) && col >= 0 {
+// 		if arr[row][col] == tar {
+// 			return []int{row, col}
+// 		}
+// 		if arr[row][col] < tar {
+// 			row++
+// 		} else {
+// 			col--
+// 		}
+// 	}
+// 	return []int{-1, -1}
+// }
