8d6dcd09aa823e69f5506128330f8b105bf83395
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-23 09:19:58 +0530

    commit

---
+++ Arrays/BinarySearch/leftRight.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{0, 0, 1, 0, 1, 1}
+	res := leftRight(arr)
+	fmt.Println(res)
+}
+
+func leftRight(arr []int) []int {
+	start, end := 0, len(arr)-1
+	result := make([]int, len(arr))
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			result[start] = 0
+			start++
+		} else {
+			result[end] = 1
+			end--
+		}
+	}
+	return result
+}
