05f0fcdfd4f2d69513d121f874da051d89e79620
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-29 21:56:03 +0530

    commit

--- Arrays/Sorting/SelectionSort.go
+++ Arrays/Sorting/SelectionSort.go
@@ -55,3 +55,32 @@ func sort(arr []int) []int {
 // 	}
 // 	return arr
 // }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{5, 1, 4, 2, 6}
+// 	res := sort(arr)
+// 	fmt.Println(res)
+// }
+
+// func sort(arr []int) []int {
+// 	for i := 0; i < len(arr); i++ {
+// 		minIndex := i
+// 		for j := i + 1; j < len(arr); j++ {
+// 			if arr[j] < arr[minIndex] {
+// 				minIndex = j
+// 			}
+// 		}
+// 		if minIndex != i {
+// 			temp := arr[i]
+// 			arr[i] = arr[minIndex]
+// 			arr[minIndex] = temp
+// 		}
+// 	}
+// 	return arr
+// }
