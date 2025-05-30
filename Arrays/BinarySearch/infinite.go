61895560d60b1bc160fda0946eee7c00898e959f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-05-30 20:50:21 +0530

    commit

--- Arrays/BinarySearch/infinite.go
+++ Arrays/BinarySearch/infinite.go
@@ -13,7 +13,7 @@ func main() {
 	tar := 30
 	res := search(arr, tar)
 	if res != -1 {
-		fmt.Printf("Target %d found at the index: %d\n", tar, res)
+		fmt.Printf("Taret %d found at the index : %d\n", tar, res)
 	} else {
 		fmt.Println("Target not found")
 	}
@@ -35,7 +35,7 @@ func search(arr []int, tar int) int {
 func binarySearch(arr []int, tar int, start int, end int) int {
 	for start <= end {
 		mid := start + (end-start)/2
-		if tar == arr[mid] {
+		if arr[mid] == tar {
 			return mid
 		} else if tar < arr[mid] {
 			end = mid - 1
@@ -45,6 +45,50 @@ func binarySearch(arr []int, tar int, start int, end int) int {
 	}
 	return -1
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 6, 8, 9, 10, 13, 16, 20, 30, 35, 40}
+// 	tar := 30
+// 	res := search(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("Target %d found at the index: %d\n", tar, res)
+// 	} else {
+// 		fmt.Println("Target not found")
+// 	}
+// }
+
+// func search(arr []int, tar int) int {
+// 	start, end := 0, 1
+// 	for tar > arr[end] {
+// 		newStart := end + 1
+// 		end = end + (end-start+1)*2
+// 		if end >= len(arr) {
+// 			end = len(arr) - 1
+// 		}
+// 		start = newStart
+// 	}
+// 	return binarySearch(arr, tar, start, end)
+// }
+
+// func binarySearch(arr []int, tar int, start int, end int) int {
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			return mid
+// 		} else if tar < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
