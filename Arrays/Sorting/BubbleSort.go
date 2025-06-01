822ce45e04505548a87a6f3ca718775a9db0cf27
shivakumar2006 <official.shivakumar06@gmail.com>
2025-06-01 16:01:48 +0530

    commit

--- Arrays/Sorting/BubbleSort.go
+++ Arrays/Sorting/BubbleSort.go
@@ -117,3 +117,33 @@ func sort(arr []int) []int {
 // 	}
 // 	return arr
 // }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{5, 1, 4, 2, 6}
+// 	res := sort(arr)
+// 	fmt.Println(res)
+// }
+
+// func sort(arr []int) []int {
+// 	for i := 0; i < len(arr); i++ {
+// 		swapped := false
+// 		for j := 1; j < len(arr); j++ {
+// 			if arr[j] < arr[j-1] {
+// 				temp := arr[j]
+// 				arr[j] = arr[j-1]
+// 				arr[j-1] = temp
+// 				swapped = true
+// 			}
+// 		}
+// 		if !swapped {
+// 			break
+// 		}
+// 	}
+// 	return arr
+// }
