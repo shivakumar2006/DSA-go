8ace3df3c8a5c6a49cbeaf6f5284c085867e194a
shivakumar2006 <official.shivakumar06@gmail.com>
2025-07-21 16:42:28 +0530

    commit

--- Arrays/linearSearch/findMin.go
+++ Arrays/linearSearch/findMin.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	arr := []int{-1, -3, -5, 69, -100, 10}
+	arr := []int{-1, -1, -5, 69, -100, 10}
 	findMin := min(arr)
 	fmt.Printf("The min number in the array is : %d\n", findMin)
 }
@@ -23,6 +21,32 @@ func min(arr []int) int {
 	}
 	return small
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{-1, -3, -5, 69, -100, 10}
+// 	findMin := min(arr)
+// 	fmt.Printf("The min number in the array is : %d\n", findMin)
+// }
+
+// func min(arr []int) int {
+// 	if len(arr) == 0 {
+// 		panic("array is empty")
+// 	}
+
+// 	small := arr[0]
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] < small {
+// 			small = arr[i]
+// 		}
+// 	}
+// 	return small
+// }
 
 // func min(arr []int) int {
 // 	if len(arr) == 0 {
