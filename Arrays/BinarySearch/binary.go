bf6396a22a7409c4fbf0be9bfc240c9081ce803f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-07-21 17:58:55 +0530

    commit

--- Arrays/BinarySearch/binary.go
+++ Arrays/BinarySearch/binary.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	arr := []int{2, 5, 3, 6, 6, 8, 10, 20}
-	tar := 20
+	tar := 10
 	res := search(arr, tar)
 	if res != -1 {
-		fmt.Printf("THe target number %d found at the index : %d\n", tar, res)
+		fmt.Printf("The target numner %d found at the index : %d\n", tar, res)
 	} else {
 		fmt.Println("Target not found")
 	}
@@ -29,6 +29,53 @@ func search(arr []int, tar int) int {
 	}
 	return -1
 }
+
+// func search(arr []int, tar int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			return mid
+// 		} else if tar < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{2, 5, 3, 6, 6, 8, 10, 20}
+// 	tar := 20
+// 	res := search(arr, tar)
+// 	if res != -1 {
+// 		fmt.Printf("THe target number %d found at the index : %d\n", tar, res)
+// 	} else {
+// 		fmt.Println("Target not found")
+// 	}
+// }
+
+// func search(arr []int, tar int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			return mid
+// 		} else if tar < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
 
 // package main
 
--- Arrays/BinarySearch/leftRight.go
+++ Arrays/BinarySearch/leftRight.go
@@ -25,6 +25,63 @@ func split(arr []int) []int {
 	return res
 }
 
+// func split(arr []int) []int {
+// 	start, end := 0, len(arr)-1
+// 	res := make([]int, len(arr))
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] == 0 {
+// 			res[start] = 0
+// 			start++
+// 		} else {
+// 			res[end] = 1
+// 			end--
+// 		}
+// 	}
+// 	return res
+// }
+
+// func split(arr []int) []int {
+// 	start, end := 0, len(arr)-1
+// 	res := make([]int, len(arr))
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] == 0 {
+// 			res[start] = 0
+// 			start++
+// 		} else {
+// 			res[end] = 1
+// 			end--
+// 		}
+// 	}
+// 	return res
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	arr := []int{0, 0, 1, 0, 1, 0}
+// 	res := split(arr)
+// 	fmt.Println(res)
+// }
+
+// func split(arr []int) []int {
+// 	start, end := 0, len(arr)-1
+// 	res := make([]int, len(arr))
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] == 0 {
+// 			res[start] = 0
+// 			start++
+// 		} else {
+// 			res[end] = 1
+// 			end--
+// 		}
+// 	}
+// 	return res
+// }
+
 // package main
 
 // import (
