7ec78b3fc77520b556e823f73069e150824165b5
shivakumar2006 <official.shivakumar06@gmail.com>
2025-07-21 16:57:00 +0530

    commit

--- Arrays/linearSearch/oddNum.go
+++ Arrays/linearSearch/oddNum.go
@@ -20,18 +20,64 @@ func oddNum(arr []int) int {
 	return count
 }
 
-func oddDigits(num int) bool {
-	if num < 0 {
-		num = -num
+func oddDigits(nums int) bool {
+	if nums < 0 {
+		nums = -nums
 	}
 
 	digits := 0
-	for num != 0 {
-		num = num / 10
+	for nums != 0 {
+		nums = nums / 10
 		digits++
 	}
 	return digits%2 != 0
 }
+
+// func oddNum(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if oddDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func oddDigits(nums int) bool {
+// 	if nums < 0 {
+// 		nums = -nums
+// 	}
+
+// 	digits := 0
+// 	for nums != 0 {
+// 		nums = nums / 10
+// 		digits++
+// 	}
+// 	return digits%2 != 0
+// }
+
+// func oddNum(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if oddDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func oddDigits(num int) bool {
+// 	if num < 0 {
+// 		num = -num
+// 	}
+
+// 	digits := 0
+// 	for num != 0 {
+// 		num = num / 10
+// 		digits++
+// 	}
+// 	return digits%2 != 0
+// }
 
 // func oddNum(arr []int) int {
 // 	count := 0
