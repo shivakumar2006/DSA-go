caa1c7de789f6e28cf0572970c98c00c0ee4665f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-07-24 18:55:59 +0530

    commit

--- Arrays/BinarySearch/duplicateRotatedSorted.go
+++ Arrays/BinarySearch/duplicateRotatedSorted.go
@@ -19,6 +19,17 @@ func main() {
 
 func search(arr []int, tar int) int {
 	pivot := findPivot(arr)
+
+	if pivot == -1 {
+		return binarySearch(arr, tar, 0, len(arr)-1)
+	}
+	if tar == arr[pivot] {
+		return pivot
+	}
+	if tar <= arr[0] {
+		return binarySearch(arr, tar, 0, pivot)
+	}
+	return binarySearch(arr, tar, pivot+1, len(arr)-1)
 }
 
 func findPivot(arr []int) int {
@@ -26,9 +37,42 @@ func findPivot(arr []int) int {
 	for start <= end {
 		mid := start + (end-start)/2
 		if mid < end && arr[mid] > arr[mid+1] {
+			return mid
+		} else if mid > start && arr[mid] < arr[mid-1] {
+			return mid - 1
+		}
 
+		// handle duplicate
+		if arr[start] == arr[mid] && arr[mid] == arr[end] {
+			if start < end && arr[start] < arr[start+1] {
+				return start + 1
+			}
+			start++
+			if start < end && arr[end] < arr[end-1] {
+				return end - 1
+			}
+			end--
+		} else if arr[start] < arr[end] || arr[start] == arr[mid] && arr[start] < arr[mid] {
+			end = mid - 1
+		} else {
+			start = mid + 1
 		}
 	}
+	return -1
+}
+
+func binarySearch(arr []int, tar int, start int, end int) int {
+	for start <= end {
+		mid := start + (end-start)/2
+		if tar == arr[mid] {
+			return mid
+		} else if tar < arr[mid] {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	return -1
 }
 
 // func search(arr []int, tar int) int {
