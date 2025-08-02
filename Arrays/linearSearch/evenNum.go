9c5f0dc6fc141f4f0d4b1ac81f84c1b0da1c1dff
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-02 17:28:19 +0530

    commit

--- Arrays/linearSearch/evenNum.go
+++ Arrays/linearSearch/evenNum.go
@@ -56,6 +56,29 @@ func evenDigits(num int) bool {
 // 	return digits%2 == 0
 // }
 
+// func evenNum(arr []int) int {
+// 	count := 0
+// 	for i := 0; i < len(arr); i++ {
+// 		if evenDigits(arr[i]) {
+// 			count++
+// 		}
+// 	}
+// 	return count
+// }
+
+// func evenDigits(num int) bool {
+// 	if num < 0 {
+// 		num = -num
+// 	}
+
+// 	digits := 0
+// 	for num != 0 {
+// 		num = num / 10
+// 		digits++
+// 	}
+// 	return digits%2 == 0
+// }
+
 // package main
 
 // import "fmt"
