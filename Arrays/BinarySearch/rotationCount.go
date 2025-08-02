549002722e76eea140c1e46e8fd3cb3ee723026d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-02 19:09:10 +0530

    commit

--- Arrays/BinarySearch/rotationCount.go
+++ Arrays/BinarySearch/rotationCount.go
@@ -33,6 +33,28 @@ func findPivot(arr []int) int {
 	return -1
 }
 
+// func rotation(arr []int) int {
+// 	pivot := findPivot(arr)
+// 	return pivot + 1
+// }
+
+// func findPivot(arr []int) int {
+// 	start, end := 0, len(arr)-1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if mid < end && arr[mid] > arr[mid+1] {
+// 			return mid
+// 		} else if mid > start && arr[mid] < arr[mid-1] {
+// 			return mid - 1
+// 		} else if arr[mid] <= arr[start] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return -1
+// }
+
 // package main
 
 // import (
