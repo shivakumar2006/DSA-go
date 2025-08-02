196acd669795374cbe4995a124dcef70a10c9ead
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-02 17:58:37 +0530

    commit

--- Arrays/BinarySearch/findFloor.go
+++ Arrays/BinarySearch/findFloor.go
@@ -15,6 +15,24 @@ func main() {
 	}
 }
 
+func find(arr []int, tar int) int {
+	start, end := 0, len(arr)-1
+	for start <= end {
+		mid := start + (end-start)/2
+		if tar == arr[mid] {
+			return mid
+		} else if tar < arr[mid] {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	if end >= 0 {
+		return end
+	}
+	return -1
+}
+
 // func find(arr []int, tar int) int {
 // 	start, end := 0, len(arr)-1
 // 	for start <= end {
