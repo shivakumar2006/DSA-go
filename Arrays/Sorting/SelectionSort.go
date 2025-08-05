32e0a8f37d52f22f46c58e40f2310e63ea35bfab
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-05 18:48:29 +0530

    kafka

--- Arrays/Sorting/SelectionSort.go
+++ Arrays/Sorting/SelectionSort.go
@@ -10,6 +10,23 @@ func main() {
 	fmt.Println(res)
 }
 
+func SelectionSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		minIndex := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+		if minIndex != i {
+			temp := arr[i]
+			arr[i] = arr[minIndex]
+			arr[minIndex] = temp
+		}
+	}
+	return arr
+}
+
 // func SelectionSort(arr []int) []int {
 // 	for i := 0; i < len(arr); i++ {
 // 		minIndex := i
