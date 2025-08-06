dae2fd9268ece7aad07589b13fc635a130955ee6
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-06 20:28:10 +0530

    commit

--- Arrays/Sorting/cyclic.go
+++ Arrays/Sorting/cyclic.go
@@ -13,7 +13,7 @@ func main() {
 func sort(arr []int) []int {
 	i := 0
 	for i < len(arr) {
-		correct := arr[i] - 1 // Our correct index on the arr[i]-1 index
+		correct := arr[i] - 1
 		if arr[i] != arr[correct] {
 			swap(arr, i, correct)
 		} else {
@@ -23,9 +23,26 @@ func sort(arr []int) []int {
 	return arr
 }
 
-func swap(arr []int, i int, correct int) {
-	arr[i], arr[correct] = arr[correct], arr[i]
+func swap(arr []int, i int, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
 }
+
+// func sort(arr []int) []int {
+// 	i := 0
+// 	for i < len(arr) {
+// 		correct := arr[i] - 1 // Our correct index on the arr[i]-1 index
+// 		if arr[i] != arr[correct] {
+// 			swap(arr, i, correct)
+// 		} else {
+// 			i++
+// 		}
+// 	}
+// 	return arr
+// }
+
+// func swap(arr []int, i int, correct int) {
+// 	arr[i], arr[correct] = arr[correct], arr[i]
+// }
 
 // This is wrong......
 
