232e9c98853e3f45b9393f0e74befc66cfe022e4
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-06 15:31:48 +0530

    commit

---
+++ Arrays/Sorting/insertionSort.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{5, 1, 4, 3, 2, 6}
+	res := sort(arr)
+	fmt.Println(res)
+}
+
+func sort(arr []int) []int {
+	for i := 0; i < len(arr)-1; i++ {
+		for j := i + 1; j > 0; j-- {
+			if arr[j] < arr[j-1] {
+				swap(arr, j, j-1)
+			} else {
+				break
+			}
+		}
+	}
+	return arr
+}
+
+func swap(arr []int, j int, i int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
