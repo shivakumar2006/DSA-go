323c9a3a067303173a6b688b4cd805189aba9cc9
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-07 14:57:56 +0530

    commit

---
+++ Arrays/Sorting/cyclicSort/firstMissingPos.go
@@ -0,0 +1,36 @@
+// First missing positive,
+// Amazon
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{3, 4, -1, 1}
+	fmt.Println(sort(arr))
+}
+
+func sort(arr []int) int {
+	i := 0
+	for i < len(arr) {
+		correct := arr[i] - 1
+		if arr[i] > 0 && arr[i] <= len(arr) && arr[i] != arr[correct] {
+			swap(arr, i, correct)
+		} else {
+			i++
+		}
+	}
+
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != i+1 {
+			return i + 1
+		}
+	}
+	return len(arr)
+}
+
+func swap(arr []int, i int, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
