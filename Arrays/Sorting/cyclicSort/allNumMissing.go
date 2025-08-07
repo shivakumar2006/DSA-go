153b24d890c05510425ea2b5f33dba48aa8148cf
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-07 13:05:49 +0530

    commit

--- Arrays/Sorting/cyclicSort/allNumMissing.go
+++ Arrays/Sorting/cyclicSort/allNumMissing.go
@@ -13,6 +13,29 @@ func main() {
 	fmt.Println(res)
 }
 
+func sort(arr []int) []int {
+	i := 0
+	for i < len(arr) {
+		correct := arr[i] - 1
+		if arr[i] > 0 && correct < len(arr) && arr[i] != arr[correct] {
+			swap(arr, i, correct)
+		} else {
+			i++
+		}
+	}
+	var missing []int
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != i+1 {
+			missing = append(missing, i+1)
+		}
+	}
+	return missing
+}
+
+func swap(arr []int, i int, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
+
 // func sort(arr []int) []int {
 // 	i := 0
 // 	for i < len(arr) {
