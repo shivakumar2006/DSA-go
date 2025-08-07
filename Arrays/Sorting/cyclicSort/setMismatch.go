730cffd8d24cad8ee7d8061ba2f99250f54af692
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-07 14:37:28 +0530

    commit

--- Arrays/Sorting/cyclicSort/setMismatch.go
+++ Arrays/Sorting/cyclicSort/setMismatch.go
@@ -23,8 +23,7 @@ func sort(arr []int) []int {
 	var result []int
 	for i := 0; i < len(arr); i++ {
 		if arr[i] != i+1 {
-			result = append(result, arr[i], i+1) // arr[i] is for duplicate and i+1 is for missing
-			break
+			result = append(result, arr[i], i+1)
 		}
 	}
 	return result
@@ -33,3 +32,27 @@ func sort(arr []int) []int {
 func swap(arr []int, i int, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
+
+// func sort(arr []int) []int {
+// 	i := 0
+// 	for i < len(arr) {
+// 		correct := arr[i] - 1
+// 		if arr[i] != arr[correct] {
+// 			swap(arr, i, correct)
+// 		} else {
+// 			i++
+// 		}
+// 	}
+// 	var result []int
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] != i+1 {
+// 			result = append(result, arr[i], i+1) // arr[i] is for duplicate and i+1 is for missing
+// 			break
+// 		}
+// 	}
+// 	return result
+// }
+
+// func swap(arr []int, i int, j int) {
+// 	arr[i], arr[j] = arr[j], arr[i]
+// }
