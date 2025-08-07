47e04a8842490a364eb339ec2f25bd04632b17c7
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-07 13:21:22 +0530

    commit

--- Arrays/Sorting/cyclicSort/allDupliNum.go
+++ Arrays/Sorting/cyclicSort/allDupliNum.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	arr := []int{1, 2, 3, 4, 4, 5, 6, 6}
+	res := sort(arr)
+	fmt.Println(res)
+}
+
+// func sort(arr []int) []int {
+// 	i := 0
+// 	for i < len(arr) {
+// 		correct := arr[i] - 1
+// 		if arr[i] != arr[correct] {
+// 			swap(arr, i, correct)
+// 		} else {
+// 			i++
+// 		}
+// 	}
+// 	var duplicate []int
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] != i+1 {
+// 			if !contains(duplicate, arr[i]) {
+// 				duplicate = append(duplicate, arr[i])
+// 			}
+// 		}
+// 	}
+// 	return duplicate
+// }
+
+// func swap(arr []int, i int, j int) {
+// 	arr[i], arr[j] = arr[j], arr[i]
+// }
+
+// func contains(arr []int, value int) bool {
+// 	for _, v := range arr {
+// 		if v == value {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
--- Arrays/Sorting/cyclicSort/duplicateNum.go
+++ Arrays/Sorting/cyclicSort/duplicateNum.go
@@ -25,12 +25,32 @@ func sort(arr []int) int {
 			i++
 		}
 	}
-	return -1
+	return len(arr)
 }
 
 func swap(arr []int, i int, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
+
+// func sort(arr []int) int {
+// 	i := 0
+// 	for i < len(arr) {
+// 		correct := arr[i] - 1
+// 		if arr[i] != arr[correct] {
+// 			swap(arr, i, correct)
+// 		} else {
+// 			if i != correct {
+// 				return arr[i]
+// 			}
+// 			i++
+// 		}
+// 	}
+// 	return -1
+// }
+
+// func swap(arr []int, i int, j int) {
+// 	arr[i], arr[j] = arr[j], arr[i]
+// }
 
 // func sort(arr []int) int {
 // 	i := 0
