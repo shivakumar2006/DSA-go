933110e14de59a7d880fbeec1e4b8e29bf26c97a
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-07 15:42:03 +0530

    commit

--- Arrays/Sorting/cyclicSort/firstMissingPos.go
+++ Arrays/Sorting/cyclicSort/firstMissingPos.go
@@ -16,7 +16,7 @@ func sort(arr []int) int {
 	i := 0
 	for i < len(arr) {
 		correct := arr[i] - 1
-		if arr[i] > 0 && arr[i] <= len(arr) && arr[i] != arr[correct] {
+		if arr[i] > 0 && arr[i] < len(arr) && arr[i] != arr[correct] {
 			swap(arr, i, correct)
 		} else {
 			i++
@@ -33,6 +33,24 @@ func sort(arr []int) int {
 func swap(arr []int, i int, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
+
+// func sort(arr []int) int {
+// 	i := 0
+// 	for i < len(arr) {
+// 		correct := arr[i] - 1
+// 		if arr[i] > 0 && arr[i] <= len(arr) && arr[i] != arr[correct] {
+// 			swap(arr, i, correct)
+// 		} else {
+// 			i++
+// 		}
+// 	}
+// 	for i := 0; i < len(arr); i++ {
+// 		if arr[i] != i+1 {
+// 			return i + 1
+// 		}
+// 	}
+// 	return len(arr)
+// }
 
 // func sort(arr []int) int {
 // 	i := 0
