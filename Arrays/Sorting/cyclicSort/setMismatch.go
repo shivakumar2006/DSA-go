a2a38827ca86a7612df86fea0bdbf38f4ba37810
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-07 14:27:49 +0530

    commit

--- Arrays/Sorting/cyclicSort/setMismatch.go
+++ Arrays/Sorting/cyclicSort/setMismatch.go
@@ -1,7 +1,35 @@
-package main 
+package main
 
 import (
 	"fmt"
 )
 
-func 
+func main() {
+	arr := []int{2, 1, 4, 2, 5, 6}
+	res := sort(arr)
+	fmt.Println(res)
+}
+
+func sort(arr []int) []int {
+	i := 0
+	for i < len(arr) {
+		correct := arr[i] - 1
+		if arr[i] != arr[correct] {
+			swap(arr, i, correct)
+		} else {
+			i++
+		}
+	}
+	var result []int
+	for i := 0; i < len(arr); i++ {
+		if arr[i] != i+1 {
+			result = append(result, arr[i], i+1)
+			break
+		}
+	}
+	return result
+}
+
+func swap(arr []int, i int, j int) {
+	arr[i], arr[j] = arr[j], arr[i]
+}
