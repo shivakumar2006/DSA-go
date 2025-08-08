774c7a6e50cf639ae86f3e90107f54a960835b9b
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-08 18:06:09 +0530

    commit

--- Patterns/pyramid.go
+++ Patterns/pyramid.go
@@ -9,17 +9,46 @@ func main() {
 }
 
 func pyramid(n int) {
-	for row := n; row >= 1; row-- {
-		for space := 0; space < n-row; space++ {
+	for row := 1; row <= n; row++ {
+		for space := 1; space <= n-row; space++ {
 			fmt.Print(" ")
 		}
+		// totalCol := row
+
+		// if row > n {
+		// 	totalCol = 2*n - row
+		// }
 
 		for col := 1; col <= row; col++ {
 			fmt.Print("* ")
 		}
 		fmt.Println()
 	}
+
+	for i := n - 1; i >= 1; i-- {
+		for space := 0; space < n-i; space++ {
+			fmt.Print(" ")
+		}
+
+		for col := 1; col <= i; col++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
 }
+
+// func pyramid(n int) {
+// 	for row := n; row >= 1; row-- {
+// 		for space := 0; space < n-row; space++ {
+// 			fmt.Print(" ")
+// 		}
+
+// 		for col := 1; col <= row; col++ {
+// 			fmt.Print("* ")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
 
 // inverted pyramid
 // func pyramid(n int) {
