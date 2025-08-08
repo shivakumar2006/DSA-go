93e92d3d5427e4f65c40a64a56eae5ed1b47bb88
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-08 18:51:52 +0530

    commit

--- Patterns/pyramid.go
+++ Patterns/pyramid.go
@@ -9,33 +9,52 @@ func main() {
 }
 
 func pyramid(n int) {
-	for row := 1; row <= n; row++ {
-		for space := 1; space <= n-row; space++ {
-			fmt.Print(" ")
+	for row := 0; row <= 2*n; row++ {
+		totalCol := row
+		if row > n {
+			totalCol = 2*n - row
 		}
-		// totalCol := row
 
-		// if row > n {
-		// 	totalCol = 2*n - row
-		// }
-
-		for col := 1; col <= row; col++ {
-			fmt.Print("* ")
-		}
-		fmt.Println()
-	}
-
-	for i := n - 1; i >= 1; i-- {
-		for space := 0; space < n-i; space++ {
+		noOfSpaces := n - totalCol
+		for s := 0; s < noOfSpaces; s++ {
 			fmt.Print(" ")
 		}
 
-		for col := 1; col <= i; col++ {
+		for col := 0; col < totalCol; col++ {
 			fmt.Print("* ")
 		}
 		fmt.Println()
 	}
 }
+
+// func pyramid(n int) {
+// 	for row := 1; row <= n; row++ {
+// 		for space := 1; space <= n-row; space++ {
+// 			fmt.Print(" ")
+// 		}
+// 		// totalCol := row
+
+// 		// if row > n {
+// 		// 	totalCol = 2*n - row
+// 		// }
+
+// 		for col := 1; col <= row; col++ {
+// 			fmt.Print("* ")
+// 		}
+// 		fmt.Println()
+// 	}
+
+// 	for i := n - 1; i >= 1; i-- {
+// 		for space := 0; space < n-i; space++ {
+// 			fmt.Print(" ")
+// 		}
+
+// 		for col := 1; col <= i; col++ {
+// 			fmt.Print("* ")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
 
 // func pyramid(n int) {
 // 	for row := n; row >= 1; row-- {
