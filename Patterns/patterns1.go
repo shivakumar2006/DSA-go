de19a83dde0c295944f006094ed4c336acfc4f7d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-08 17:00:06 +0530

    commit

--- Patterns/patterns1.go
+++ Patterns/patterns1.go
@@ -2,24 +2,26 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	pattern(5)
 }
 
-// pattern 6....
-
-// func pattern(n int) {
-// 	for row := 1; row <= 2*n-1; row++ {
-// 		totalCol := row
-// 		if row > n {
-// 			totalCol = 2*n - row
-// 		}
-// 		for col := 1; col <= totalCol; col++ {
-// 			fmt.Print(col, " ")
-// 		}
-// 		fmt.Println()
-// 	}
-// }
+func pattern(n int) {
+	for row := 1; row <= 2*n-1; row++ {
+		totalCol := row
+		if row > n {
+			totalCol = 2*n - row
+		}
+		for col := 1; col <= totalCol; col++ {
+			fmt.Print(col, " ")
+		}
+		fmt.Println()
+	}
+}
 
 // func pattern(n int) {
 // 	for row := 1; row <= 2*n-1; row++ {
