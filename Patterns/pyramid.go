2bd6dce41d19f32330437d886ab203fd049ea493
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-11 22:40:14 +0530

    commit

--- Patterns/pyramid.go
+++ Patterns/pyramid.go
@@ -1,31 +1,40 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	pyramid(5)
 }
 
-func pyramid(n int) {
-	for row := 0; row <= 2*n; row++ {
-		totalCol := row
-		if row > n {
-			totalCol = 2*n - row
-		}
+// func pyramid(n int) {
+// 	for row := n; row >= 1; row-- {
+// 		for space := 1; space <= n-row; space++ {
+// 			fmt.Print(" ")
+// 		}
 
-		noOfSpaces := n - totalCol
-		for s := 0; s < noOfSpaces; s++ {
-			fmt.Print(" ")
-		}
+// 		for col := 1; col <= row; col++ {
+// 			fmt.Print("* ")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
 
-		for col := 0; col < totalCol; col++ {
-			fmt.Print("* ")
-		}
-		fmt.Println()
-	}
-}
+// func pyramid(n int) {
+// 	for row := 0; row <= 2*n; row++ {
+// 		totalCol := row
+// 		if row > n {
+// 			totalCol = 2*n - row
+// 		}
+
+// 		noOfSpaces := n - totalCol
+// 		for s := 0; s < noOfSpaces; s++ {
+// 			fmt.Print(" ")
+// 		}
+
+// 		for col := 0; col < totalCol; col++ {
+// 			fmt.Print("* ")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
 
 // func pyramid(n int) {
 // 	for row := 1; row <= n; row++ {
