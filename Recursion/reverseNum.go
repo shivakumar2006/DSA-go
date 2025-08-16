775bc2f7b8bbf54433885a5e51fa81ae6c5e8cfa
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-16 17:56:41 +0530

    commit

--- Recursion/reverseNum.go
+++ Recursion/reverseNum.go
@@ -1,5 +1,27 @@
 // Reverse a number
 
+// way-2
+// this approach is bit slower
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(rev(1234))
+}
+
+func rev(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	digits := int(math.Log10(float64(n))) // number of digits - 1
+	return (n%10)*int(math.Pow(10, float64(digits))) + rev(n/10)
+}
+
 // way-1
 // package main
 
