e63707a45a095ba8d50ec2f5c351fafa5558708d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-16 18:49:12 +0530

    commit

---
+++ Recursion/CountZeroes.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(count(310210))
+}
+
+func count(n int) int {
+	return helper(n, 0)
+}
+
+func helper(n int, c int) int {
+	if n == 0 {
+		return c
+	}
+
+	rem := n % 10
+	if rem == 0 {
+		return helper(n/10, c+1)
+	}
+	return helper(n/10, c)
+}
