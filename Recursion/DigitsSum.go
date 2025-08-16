5469242a137c2010edb74edc4deca979fd53c2f2
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-16 17:03:58 +0530

    commit

---
+++ Recursion/DigitsSum.go
@@ -0,0 +1,35 @@
+// Sum of all the digits
+
+package main
+
+import "fmt"
+
+func main() {
+	ans := sum(1422)
+	fmt.Println(ans)
+}
+
+func sum(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	return (n % 10) + sum(n/10)
+}
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ans := sum(1342)
+// 	fmt.Println(ans)
+// }
+
+// func sum(n int) int {
+// 	if n == 0 {
+// 		return 0
+// 	}
+
+// 	return (n % 10) + sum(n/10)
+// }
