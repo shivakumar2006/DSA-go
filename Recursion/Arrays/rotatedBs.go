c303893c45f5aad9209217a762d1842a1e93ad06
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-17 18:02:24 +0530

    commit

--- Recursion/Arrays/rotatedBs.go
+++ Recursion/Arrays/rotatedBs.go
@@ -6,7 +6,7 @@ import "fmt"
 
 func main() {
 	arr := []int{5, 6, 7, 8, 9, 1, 2, 3}
-	fmt.Println(search(arr, 8, 0, len(arr)-1))
+	fmt.Println(search(arr, 3, 0, len(arr)-1))
 }
 
 func search(arr []int, tar int, start int, end int) int {
@@ -29,9 +29,34 @@ func search(arr []int, tar int, start int, end int) int {
 
 	if arr[start] >= arr[mid] && tar <= arr[end] {
 		return search(arr, tar, mid+1, end)
+	} else {
+		return search(arr, tar, start, mid-1)
 	}
-	return search(arr, tar, start, mid-1)
 }
+
+// func search(arr []int, tar int, start int, end int) int {
+// 	if start > end {
+// 		return -1
+// 	}
+
+// 	mid := start + (end-start)/2
+// 	if arr[mid] == tar {
+// 		return mid
+// 	}
+
+// 	if arr[start] <= arr[mid] {
+// 		if tar >= arr[start] && tar <= arr[mid] {
+// 			return search(arr, tar, start, mid-1)
+// 		} else {
+// 			return search(arr, tar, mid+1, end)
+// 		}
+// 	}
+
+// 	if arr[start] >= arr[mid] && tar <= arr[end] {
+// 		return search(arr, tar, mid+1, end)
+// 	}
+// 	return search(arr, tar, start, mid-1)
+// }
 
 // func search(arr []int, tar int, start int, end int) int {
 // 	if start > end {
