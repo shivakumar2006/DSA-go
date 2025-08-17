df667d394e827682f49a88c5ad2aa579cdac0c6d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-18 04:24:55 +0530

    commit

---
+++ Recursion/Patterns/bubbleSort.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{4, 3, 2, 1}
+	bubble(arr, len(arr)-1, 0)
+	fmt.Println(arr)
+}
+
+func bubble(arr []int, r int, c int) {
+	if r == 0 {
+		return
+	}
+
+	if c < r {
+		if arr[c] > arr[c+1] {
+			temp := arr[c]
+			arr[c] = arr[c+1]
+			arr[c+1] = temp
+		}
+		bubble(arr, r, c+1)
+	} else {
+		bubble(arr, r-1, 0)
+	}
+}
