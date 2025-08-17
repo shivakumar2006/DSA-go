2041266d41561e08692a5a6d280811a8ad313527
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-18 04:47:25 +0530

    commit

---
+++ Recursion/Patterns/selectionSort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{4, 3, 2, 1}
+	selection(arr, len(arr), 0, 0)
+	fmt.Println(arr)
+}
+
+func selection(arr []int, r, c int, max int) {
+	if r == 0 {
+		return
+	}
+
+	if c < r {
+		if arr[c] > arr[max] {
+			selection(arr, r, c+1, c)
+		} else {
+			selection(arr, r, c+1, max)
+		}
+	} else {
+		temp := arr[max]
+		arr[max] = arr[r-1]
+		arr[r-1] = temp
+		selection(arr, r-1, 0, 0)
+	}
+}
