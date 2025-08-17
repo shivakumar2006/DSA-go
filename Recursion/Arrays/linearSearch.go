a43f618b627a9af0dbe3c2e8d9735073cf213a67
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-17 16:25:54 +0530

    commit

--- Recursion/Arrays/linearSearch.go
+++ Recursion/Arrays/linearSearch.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1, 2, 3, 6, 69, 99}
+	arr := []int{1, 2, 3, 6, 69, 69, 99}
 	fmt.Println("Is the target is find : ", find(arr, 69, 0))
 	fmt.Println("the index of the target element is : ", findIndex(arr, 69, 0))
 	fmt.Println("the target index is from the last index is : ", findLastIndex(arr, 69, len(arr)-1))
@@ -15,11 +15,11 @@ func find(arr []int, tar int, index int) bool {
 		return false
 	}
 
-	return arr[index] == tar || find(arr, tar, index+1)
+	return arr[index] < arr[index+1] || find(arr, tar, index+1)
 }
 
 func findIndex(arr []int, tar int, index int) int {
-	if index == len(arr) {
+	if index == len(arr)-1 {
 		return -1
 	}
 
@@ -48,9 +48,53 @@ func findAllIndex(arr []int, tar int, index int) []int {
 	}
 
 	result := findAllIndex(arr, tar, index+1)
-
 	if arr[index] == tar {
-		result = append([]int{index}, result...) // prepend current index
+		result = append([]int{index}, result...)
 	}
 	return result
 }
+
+// func find(arr []int, tar int, index int) bool {
+// 	if index == len(arr)-1 {
+// 		return false
+// 	}
+
+// 	return arr[index] == tar || find(arr, tar, index+1)
+// }
+
+// func findIndex(arr []int, tar int, index int) int {
+// 	if index == len(arr) {
+// 		return -1
+// 	}
+
+// 	if arr[index] == tar {
+// 		return index
+// 	} else {
+// 		return findIndex(arr, tar, index+1)
+// 	}
+// }
+
+// func findLastIndex(arr []int, tar int, index int) int {
+// 	if index == -1 {
+// 		return -1
+// 	}
+
+// 	if arr[index] == tar {
+// 		return index
+// 	} else {
+// 		return findLastIndex(arr, tar, index-1)
+// 	}
+// }
+
+// func findAllIndex(arr []int, tar int, index int) []int {
+// 	if index == len(arr)-1 {
+// 		return []int{}
+// 	}
+
+// 	result := findAllIndex(arr, tar, index+1)
+
+// 	if arr[index] == tar {
+// 		result = append([]int{index}, result...) // prepend current index
+// 	}
+// 	return result
+// }
