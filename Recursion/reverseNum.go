cd1c5e880760330aca4da939a3cfc2591269a17b
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-17 15:05:58 +0530

    commit

--- Recursion/reverseNum.go
+++ Recursion/reverseNum.go
@@ -1,26 +1,67 @@
 // Reverse a number
 
-// way-2
-// this approach is bit slower
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
+
+var sum int = 0
 
 func main() {
-	fmt.Println(rev(1234))
+	rev((1234))
+	fmt.Println(sum)
 }
 
-func rev(n int) int {
+func rev(n int) {
 	if n == 0 {
-		return 0
+		return
 	}
 
-	digits := int(math.Log10(float64(n))) // number of digits - 1
-	return (n%10)*int(math.Pow(10, float64(digits))) + rev(n/10)
+	remainder := n % 10
+	sum = sum*10 + remainder
+	rev(n / 10)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// func main() {
+// 	fmt.Println(rev(1234))
+// }
+
+// func rev(n int) int {
+// 	if n == 0 {
+// 		return 0
+// 	}
+
+// 	digits := int(math.Log10(float64(n)))
+// 	return (n%10)*int(math.Pow(10, float64(digits))) + rev(n/10)
+// }
+
+// way-2
+// this approach is bit slower
+// package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// func main() {
+// 	fmt.Println(rev(1234))
+// }
+
+// func rev(n int) int {
+// 	if n == 0 {
+// 		return 0
+// 	}
+
+// 	digits := int(math.Log10(float64(n))) // number of digits - 1
+// 	return (n%10)*int(math.Pow(10, float64(digits))) + rev(n/10)
+// }
 
 // way-1
 // package main
