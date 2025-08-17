f5a9d6401f9a75d2b2cc5232f049a616fd8b6539
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-17 15:15:11 +0530

    commit

--- Recursion/CountZeroes.go
+++ Recursion/CountZeroes.go
@@ -10,17 +10,57 @@ func count(n int) int {
 	return helper(n, 0)
 }
 
-func helper(n int, c int) int {
+func helper(n int, count int) int {
 	if n == 0 {
-		return c
+		return count
 	}
 
 	rem := n % 10
 	if rem == 0 {
-		return helper(n/10, c+1)
+		return helper(n/10, count+1)
 	}
-	return helper(n/10, c)
+	return helper(n/10, count)
 }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println(count(310010))
+// }
+
+// func count(n int) int {
+// 	return helper(n, 0)
+// }
+
+// func helper(n int, count int) int {
+// 	if n == 0 {
+// 		return count
+// 	}
+
+// 	rem := n % 10
+// 	if rem == 0 {
+// 		return helper(n/10, count+1)
+// 	}
+// 	return helper(n/10, count)
+// }
+
+// func count(n int) int {
+// 	return helper(n, 0)
+// }
+
+// func helper(n int, c int) int {
+// 	if n == 0 {
+// 		return c
+// 	}
+
+// 	rem := n % 10
+// 	if rem == 0 {
+// 		return helper(n/10, c+1)
+// 	}
+// 	return helper(n/10, c)
+// }
 
 // package main
 
