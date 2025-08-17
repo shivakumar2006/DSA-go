1d8735de63f9d58442834f09d23a87f1066fe353
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-17 14:51:07 +0530

    commit

--- Recursion/factorial.go
+++ Recursion/factorial.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	ans := fact(5)
+	ans := fact(6)
 	fmt.Println(ans)
 }
 
@@ -16,6 +16,23 @@ func fact(n int) int {
 
 	return n * fact(n-1)
 }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	ans := fact(5)
+// 	fmt.Println(ans)
+// }
+
+// func fact(n int) int {
+// 	if n == 0 {
+// 		return 1
+// 	}
+
+// 	return n * fact(n-1)
+// }
 
 // package main
 
