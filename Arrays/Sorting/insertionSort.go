38866a5ddad94da2a48b85badc282613ebb810d8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-18 03:32:19 +0530

    commit

--- Arrays/Sorting/insertionSort.go
+++ Arrays/Sorting/insertionSort.go
@@ -11,8 +11,8 @@ func main() {
 }
 
 func sort(arr []int) []int {
-	for i := 0; i < len(arr)-1; i++ {
-		for j := i + 1; j > 0; j-- {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
 			if arr[j] < arr[j-1] {
 				swap(arr, j, j-1)
 			} else {
@@ -26,6 +26,23 @@ func sort(arr []int) []int {
 func swap(arr []int, i int, j int) {
 	arr[i], arr[j] = arr[j], arr[i]
 }
+
+// func sort(arr []int) []int {
+// 	for i := 0; i < len(arr)-1; i++ {
+// 		for j := i + 1; j > 0; j-- {
+// 			if arr[j] < arr[j-1] {
+// 				swap(arr, j, j-1)
+// 			} else {
+// 				break
+// 			}
+// 		}
+// 	}
+// 	return arr
+// }
+
+// func swap(arr []int, i int, j int) {
+// 	arr[i], arr[j] = arr[j], arr[i]
+// }
 
 // func sort(arr []int) []int {
 // 	for i := 0; i < len(arr)-1; i++ {
