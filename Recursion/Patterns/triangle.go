3c37e305d3aefe1a034cfdb486b95e5a0f58e413
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-18 04:19:04 +0530

    commit

--- Recursion/Patterns/triangle.go
+++ Recursion/Patterns/triangle.go
@@ -6,19 +6,47 @@ func main() {
 	triangle(5, 0)
 }
 
-func triangle(row int, col int) {
-	if row == 0 {
+func triangle(r, c int) {
+	if r == 0 {
 		return
 	}
 
-	if col < row {
-		triangle(row, col+1)
+	if c < r {
 		fmt.Print("* ")
+		triangle(r, c+1)
 	} else {
-		triangle(row-1, 0)
 		fmt.Println()
+		triangle(r-1, 0)
 	}
 }
+
+// func triangle(r int, c int) {
+// 	if r == 0 {
+// 		return
+// 	}
+
+// 	if c < r {
+// 		triangle(r, c+1)
+// 		fmt.Print("* ")
+// 	} else {
+// 		triangle(r-1, 0)
+// 		fmt.Println()
+// 	}
+// }
+
+// func triangle(row int, col int) {
+// 	if row == 0 {
+// 		return
+// 	}
+
+// 	if col < row {
+// 		triangle(row, col+1)
+// 		fmt.Print("* ")
+// 	} else {
+// 		triangle(row-1, 0)
+// 		fmt.Println()
+// 	}
+// }
 
 // func triangle(row int, col int) {
 // 	if row == 0 {
