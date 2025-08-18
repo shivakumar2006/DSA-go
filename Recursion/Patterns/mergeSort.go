2746c47787b08635dd68ad8f0770d0bcc3234606
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-19 04:07:17 +0530

    commit

--- Recursion/Patterns/mergeSort.go
+++ Recursion/Patterns/mergeSort.go
@@ -34,9 +34,41 @@ func merge(left []int, right []int) []int {
 		}
 	}
 
-	//append remaining elements
 	result = append(result, left[i:]...)
 	result = append(result, right[j:]...)
 
 	return result
 }
+
+// func mergeSort(arr []int) []int {
+// 	if len(arr) <= 1 {
+// 		return arr
+// 	}
+
+// 	mid := len(arr) / 2
+// 	left := mergeSort(arr[:mid])
+// 	right := mergeSort(arr[mid:])
+
+// 	return merge(left, right)
+// }
+
+// func merge(left []int, right []int) []int {
+// 	result := []int{}
+// 	i, j := 0, 0
+
+// 	for i < len(left) && j < len(right) {
+// 		if left[i] < right[j] {
+// 			result = append(result, left[i])
+// 			i++
+// 		} else {
+// 			result = append(result, right[j])
+// 			j++
+// 		}
+// 	}
+
+// 	//append remaining elements
+// 	result = append(result, left[i:]...)
+// 	result = append(result, right[j:]...)
+
+// 	return result
+// }
