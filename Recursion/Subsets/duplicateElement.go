c1c56a709a48c6c8c8ca6574c85041b0968cf593
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-22 02:46:03 +0530

    commit

--- Recursion/Subsets/duplicateElement.go
+++ Recursion/Subsets/duplicateElement.go
@@ -11,20 +11,19 @@ func main() {
 }
 
 func subsets(arr []int) [][]int {
-	sort.Ints(arr) // handle duplicates
+	sort.Ints(arr)
 	result := [][]int{{}}
 	start, end := 0, 0
 
 	for i := 0; i < len(arr); i++ {
 		start = 0
-		// if current element is same as previous, only then starts from end+1
 		if i > 0 && arr[i] == arr[i-1] {
 			start = end + 1
 		}
+
 		end = len(result) - 1
 		n := len(result)
 		for j := start; j < n; j++ {
-			// coppy existing subsets
 			newSubset := append([]int{}, result[j]...)
 			newSubset = append(newSubset, arr[i])
 			result = append(result, newSubset)
@@ -32,3 +31,26 @@ func subsets(arr []int) [][]int {
 	}
 	return result
 }
+
+// func subsets(arr []int) [][]int {
+// 	sort.Ints(arr) // handle duplicates
+// 	result := [][]int{{}}
+// 	start, end := 0, 0
+
+// 	for i := 0; i < len(arr); i++ {
+// 		start = 0
+// 		// if current element is same as previous, only then starts from end+1
+// 		if i > 0 && arr[i] == arr[i-1] {
+// 			start = end + 1
+// 		}
+// 		end = len(result) - 1
+// 		n := len(result)
+// 		for j := start; j < n; j++ {
+// 			// coppy existing subsets
+// 			newSubset := append([]int{}, result[j]...)
+// 			newSubset = append(newSubset, arr[i])
+// 			result = append(result, newSubset)
+// 		}
+// 	}
+// 	return result
+// }
