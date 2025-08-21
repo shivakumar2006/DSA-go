5db3ffdf62cce8164207bca8b3c67315a0eb4a30
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-22 03:04:41 +0530

    commit

--- Recursion/Subsets/duplicateElement.go
+++ Recursion/Subsets/duplicateElement.go
@@ -41,6 +41,27 @@ func subsets(arr []int) [][]int {
 // 		if i > 0 && arr[i] == arr[i-1] {
 // 			start = end + 1
 // 		}
+// 		end = len(result) - 1
+// 		n := len(result)
+// 		for j := start; j < n; j++ {
+// 			newSubset := append([]int{}, result[j]...)
+// 			newSubset = append(newSubset, arr[i])
+// 			result = append(result, newSubset)
+// 		}
+// 	}
+// 	return result
+// }
+
+// func subsets(arr []int) [][]int {
+// 	sort.Ints(arr)
+// 	result := [][]int{{}}
+// 	start, end := 0, 0
+
+// 	for i := 0; i < len(arr); i++ {
+// 		start = 0
+// 		if i > 0 && arr[i] == arr[i-1] {
+// 			start = end + 1
+// 		}
 
 // 		end = len(result) - 1
 // 		n := len(result)
