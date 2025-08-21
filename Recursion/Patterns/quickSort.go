5d19087e9ae94a3bb230c950f401ba6aecc0e802
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 12:30:55 +0530

    commit

--- Recursion/Patterns/quickSort.go
+++ Recursion/Patterns/quickSort.go
@@ -8,14 +8,14 @@ func main() {
 	fmt.Println(arr)
 }
 
-func sort(arr []int, low int, high int) {
+func sort(arr []int, low, high int) {
 	if low >= high {
 		return
 	}
 
 	start := low
 	end := high
-	mid := start + (end-start)/2
+	mid := start + (end-start)/10
 	pivot := arr[mid]
 
 	for start <= end {
@@ -37,3 +37,33 @@ func sort(arr []int, low int, high int) {
 	sort(arr, low, end)
 	sort(arr, start, high)
 }
+
+// func sort(arr []int, low int, high int) {
+// 	if low >= high {
+// 		return
+// 	}
+
+// 	start := low
+// 	end := high
+// 	mid := start + (end-start)/2
+// 	pivot := arr[mid]
+
+// 	for start <= end {
+// 		for arr[start] < pivot {
+// 			start++
+// 		}
+// 		for arr[end] > pivot {
+// 			end--
+// 		}
+
+// 		if start <= end {
+// 			temp := arr[start]
+// 			arr[start] = arr[end]
+// 			arr[end] = temp
+// 			start++
+// 			end--
+// 		}
+// 	}
+// 	sort(arr, low, end)
+// 	sort(arr, start, high)
+// }
