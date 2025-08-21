2ef6c6e5195699deb1fab6b9d094046424e48621
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 13:07:08 +0530

    comit

---
+++ Recursion/String/skip.go
@@ -0,0 +1,27 @@
+// skip a substirng
+
+package main
+
+import "fmt"
+
+func main() {
+	str := "bananas"
+	result := skipChar(str, 'a')
+	fmt.Println(result)
+}
+
+func skipChar(s string, skip rune) string {
+	// base case
+	if len(s) == 0 {
+		return ""
+	}
+
+	ch := rune(s[0])              // current char
+	rest := skipChar(s[1:], skip) // recursion on the rest of the string
+
+	if ch == skip {
+		return rest // skip this character
+	} else {
+		return string(ch) + rest // keep character other than "a" + result of recursion
+	}
+}
