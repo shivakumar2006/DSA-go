b3ee1d72b0cbf9fbe8b62cf2e1ecfb512d59e89a
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-22 05:08:42 +0530

    commit

--- Recursion/Subsets/permutCount.go
+++ Recursion/Subsets/permutCount.go
@@ -5,7 +5,7 @@ package main
 import "fmt"
 
 func main() {
-	count := permutationCount("", "abc")
+	count := permutationCount("", "abcd")
 	fmt.Println("Permutation count is : ", count)
 }
 
@@ -24,3 +24,19 @@ func permutationCount(process, unprocess string) int {
 	}
 	return count
 }
+
+// func permutationCount(process, unprocess string) int {
+// 	if len(unprocess) == 0 {
+// 		return 1
+// 	}
+
+// 	count := 0
+// 	ch := unprocess[0]
+// 	rest := unprocess[1:]
+
+// 	for i := 0; i <= len(process); i++ {
+// 		newProcess := process[:i] + string(ch) + process[i:]
+// 		count += permutationCount(newProcess, rest)
+// 	}
+// 	return count
+// }
