fedd47e580e920513c9e14245735d0e3ea069185
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 19:47:26 +0530

    commit

---
+++ Recursion/Subsets/subset.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3}
+	ans := subsets(arr)
+	fmt.Println(ans)
+}
+
+func subsets(arr []int) [][]int {
+	result := [][]int{{}}
+
+	for _, num := range arr {
+		newSubsets := [][]int{}
+		for _, subset := range result {
+			// create a new subsetby adding num to exisiting subset
+			newSubset := append([]int{}, subset...)
+			newSubset = append(newSubset, num)
+			newSubsets = append(newSubsets, newSubset)
+		}
+		result = append(result, newSubsets...)
+	}
+	return result
+}
