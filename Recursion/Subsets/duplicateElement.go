006e43be2fb73afae07049b5042ddce0e2cccc14
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 22:42:00 +0530

    commit

---
+++ Recursion/Subsets/duplicateElement.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	arr := []int{1, 2, 2}
+	fmt.Println(subsets(arr))
+}
+
+func subsets(arr []int) [][]int {
+	sort.Ints(arr) // handle duplicates
+	result := [][]int{{}}
+	start, end := 0, 0
+
+	for i := 0; i < len(arr); i++ {
+		start = 0
+		// if current element is same as previous, only then starts from end+1
+		if i > 0 && arr[i] == arr[i-1] {
+			start = end + 1
+		}
+		end = len(result) - 1
+		n := len(result)
+		for j := start; j < n; j++ {
+			// coppy existing subsets
+			newSubset := append([]int{}, result[j]...)
+			newSubset = append(newSubset, arr[i])
+			result = append(result, newSubset)
+		}
+	}
+	return result
+}
