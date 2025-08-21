8d2cf5c6baaaa2e25584ea8831bb64616e13f303
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 18:42:27 +0530

    commit

--- Recursion/Subsets/subsequence.go
+++ Recursion/Subsets/subsequence.go
@@ -4,20 +4,36 @@ import "fmt"
 
 func main() {
 	str := "abc"
-	subsequences("", str)
+	subseq("", str)
 }
 
-func subsequences(processed, unprocessed string) {
+func subseq(processed, unprocessed string) {
 	if len(unprocessed) == 0 {
 		fmt.Println(processed)
 		return
 	}
 
-	ch := string(unprocessed[0])
+	ch := string(unprocessed[0]) // first char
+	rest := unprocessed[1:]      // rest of the characters
 
 	// include characters
-	subsequences(processed+ch, unprocessed[1:])
+	subseq(processed+ch, rest)
 
 	// exclude characters
-	subsequences(processed, unprocessed[1:])
+	subseq(processed, rest)
 }
+
+// func subsequences(processed, unprocessed string) {
+// 	if len(unprocessed) == 0 {
+// 		fmt.Println(processed)
+// 		return
+// 	}
+
+// 	ch := string(unprocessed[0])
+
+// 	// include characters
+// 	subsequences(processed+ch, unprocessed[1:])
+
+// 	// exclude characters
+// 	subsequences(processed, unprocessed[1:])
+// }
