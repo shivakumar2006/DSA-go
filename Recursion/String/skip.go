e151601c4b5c49b911be2d08f648467017d76650
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-21 13:10:12 +0530

    commit

--- Recursion/String/skip.go
+++ Recursion/String/skip.go
@@ -11,17 +11,32 @@ func main() {
 }
 
 func skipChar(s string, skip rune) string {
-	// base case
 	if len(s) == 0 {
 		return ""
 	}
 
-	ch := rune(s[0])              // current char
-	rest := skipChar(s[1:], skip) // recursion on the rest of the string
+	ch := rune(s[0])
+	rest := skipChar(s[1:], skip)
 
 	if ch == skip {
-		return rest // skip this character
+		return rest
 	} else {
-		return string(ch) + rest // keep character other than "a" + result of recursion
+		return string(ch) + rest
 	}
 }
+
+// func skipChar(s string, skip rune) string {
+// 	// base case
+// 	if len(s) == 0 {
+// 		return ""
+// 	}
+
+// 	ch := rune(s[0])              // current char
+// 	rest := skipChar(s[1:], skip) // recursion on the rest of the string
+
+// 	if ch == skip {
+// 		return rest // skip this character
+// 	} else {
+// 		return string(ch) + rest // keep character other than "a" + result of recursion
+// 	}
+// }
