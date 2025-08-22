83f1f92c887ef8267df51ca1f5ca10e7654c3883
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-22 16:27:25 +0530

    commit

--- Recursion/Subsets/subset.go
+++ Recursion/Subsets/subset.go
@@ -24,6 +24,29 @@ func subsets(arr []int) [][]int {
 }
 
 // func subsets(arr []int) [][]int {
+// 	result := [][]int{}
+
+// 	for _, num := range arr {
+
+// 	}
+// }
+
+// func subsets(arr []int) [][]int {
+// 	result := [][]int{{}}
+
+// 	for _, num := range arr {
+// 		newSubsets := [][]int{}
+// 		for _, subset := range result {
+// 			newSubset := append([]int{}, subset...)
+// 			newSubset = append(newSubset, num)
+// 			newSubsets = append(newSubsets, newSubset)
+// 		}
+// 		result = append(result, newSubsets...)
+// 	}
+// 	return result
+// }
+
+// func subsets(arr []int) [][]int {
 // 	result := [][]int{{}}
 
 // 	for _, num := range arr {
