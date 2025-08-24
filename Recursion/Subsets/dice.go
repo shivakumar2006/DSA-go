9f684978d7badb86d021f87a410ddca3147fef5d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-24 12:34:04 +0530

    commit

--- Recursion/Subsets/dice.go
+++ Recursion/Subsets/dice.go
@@ -16,3 +16,14 @@ func dice(process string, tar int) {
 		dice(process+fmt.Sprint(i), tar-i)
 	}
 }
+
+// func dice(process string, tar int) {
+// 	if tar == 0 {
+// 		fmt.Println(process)
+// 		return
+// 	}
+
+// 	for i := 1; i <= 6 && i <= tar; i++ {
+// 		dice(process+fmt.Sprint(i), tar-i)
+// 	}
+// }
