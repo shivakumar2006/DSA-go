ec599130b90cdf3c78c9528fd8b38f3be3c19a60
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-24 13:06:40 +0530

    commut

---
+++ Recursion/Backtracking/maze.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	maze := count(3, 3)
+	fmt.Println(maze)
+}
+
+func count(row, col int) int {
+	if row == 1 || col == 1 {
+		return 1
+	}
+
+	left := count(row-1, col)  // here when we go on down means row by row
+	right := count(row, col-1) // here when we go right means col by col
+	return left + right
+}
