07adf25811945467adc174fd0c9f721a2f8fca0f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-24 09:37:13 +0530

    comit

--- Recursion/Subsets/letterCombo2.go
+++ Recursion/Subsets/letterCombo2.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	ans := pad("", "12")
+	fmt.Println(ans)
+}
+
+func pad(process, unprocess string) []string {
+	if len(unprocess) == 0 {
+		return []string{process}
+	}
+
+	digit := int(unprocess[0] - '0')
+	start := (digit - 1) * 3
+	end := digit * 3
+
+	result := []string{}
+
+	for i := start; i < end; i++ {
+		ch := string('a' + rune(i))
+		newResult := pad(process+ch, unprocess[1:])
+		result = append(result, newResult...)
+	}
+	return result
 }
