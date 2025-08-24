f6aeafa0004142eba3aa6054571fa79d1afe4f45
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-24 09:40:55 +0530

    commit

--- Recursion/Subsets/letterCombo2.go
+++ Recursion/Subsets/letterCombo2.go
@@ -16,12 +16,30 @@ func pad(process, unprocess string) []string {
 	start := (digit - 1) * 3
 	end := digit * 3
 
-	result := []string{}
-
+	ans := []string{}
 	for i := start; i < end; i++ {
 		ch := string('a' + rune(i))
-		newResult := pad(process+ch, unprocess[1:])
-		result = append(result, newResult...)
+		newReuslt := pad(process+ch, unprocess[1:])
+		ans = append(ans, newReuslt...)
 	}
-	return result
+	return ans
 }
+
+// func pad(process, unprocess string) []string {
+// 	if len(unprocess) == 0 {
+// 		return []string{process}
+// 	}
+
+// 	digit := int(unprocess[0] - '0')
+// 	start := (digit - 1) * 3
+// 	end := digit * 3
+
+// 	result := []string{}
+
+// 	for i := start; i < end; i++ {
+// 		ch := string('a' + rune(i))
+// 		newResult := pad(process+ch, unprocess[1:])
+// 		result = append(result, newResult...)
+// 	}
+// 	return result
+// }
