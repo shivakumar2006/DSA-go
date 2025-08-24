391712c2feafb8a31e7425215c131f99aa59d330
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-24 12:31:19 +0530

    commit

---
+++ Recursion/Subsets/dice.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	dice("", 4)
+}
+
+func dice(process string, tar int) {
+	if tar == 0 {
+		fmt.Println(process)
+		return
+	}
+
+	for i := 1; i <= 6 && i <= tar; i++ {
+		dice(process+fmt.Sprint(i), tar-i)
+	}
+}
