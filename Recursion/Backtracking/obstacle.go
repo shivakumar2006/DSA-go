70aa2612ef78c2a56430bd08e4bb61be6922b9e9
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-25 14:12:48 +0530

    commit

---
+++ Recursion/Backtracking/obstacle.go
@@ -0,0 +1,32 @@
+// Maze woth obstacle
+
+package main
+
+import "fmt"
+
+func main() {
+	maze := [][]bool{
+		{true, true, true},
+		{true, false, true},
+		{true, true, true},
+	}
+	path("", maze, 0, 0)
+}
+
+func path(process string, maze [][]bool, row, col int) {
+	if row == len(maze)-1 && col == len(maze[0])-1 {
+		fmt.Println(process)
+		return
+	}
+
+	if !maze[row][col] {
+		return
+	}
+
+	if row < len(maze)-1 {
+		path(process+"D", maze, row+1, col)
+	}
+	if col < len(maze[0])-1 {
+		path(process+"R", maze, row, col+1)
+	}
+}
