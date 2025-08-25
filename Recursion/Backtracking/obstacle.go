ab8c2b7f56e306c112594d32163dbbef6fcfecd1
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-26 04:02:53 +0530

    commit

--- Recursion/Backtracking/obstacle.go
+++ Recursion/Backtracking/obstacle.go
@@ -26,10 +26,29 @@ func path(process string, maze [][]bool, row, col int) {
 	if row < len(maze)-1 {
 		path(process+"D", maze, row+1, col)
 	}
+
 	if col < len(maze[0])-1 {
 		path(process+"R", maze, row, col+1)
 	}
 }
+
+// func path(process string, maze [][]bool, row, col int) {
+// 	if row == len(maze)-1 && col == len(maze[0])-1 {
+// 		fmt.Println(process)
+// 		return
+// 	}
+
+// 	if !maze[row][col] {
+// 		return
+// 	}
+
+// 	if row < len(maze)-1 {
+// 		path(process+"D", maze, row+1, col)
+// 	}
+// 	if col < len(maze[0])-1 {
+// 		path(process+"R", maze, row, col+1)
+// 	}
+// }
 
 // func path(process string, maze [][]bool, row, col int) {
 // 	if row == len(maze)-1 && col == len(maze[0])-1 {
