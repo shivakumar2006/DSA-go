7dca2262595709c47a123822f4bcb6f0e73f8840
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-26 03:59:44 +0530

    commit

--- Recursion/Backtracking/mazePath.go
+++ Recursion/Backtracking/mazePath.go
@@ -43,6 +43,21 @@ func path(process string, row, col int) {
 // 	}
 
 // 	if row > 1 {
+// 		path(process+"D", row-1, col)
+// 	}
+
+// 	if col > 1 {
+// 		path(process+"R", row, col-1)
+// 	}
+// }
+
+// func path(process string, row, col int) {
+// 	if row == 1 && col == 1 {
+// 		fmt.Println(process)
+// 		return
+// 	}
+
+// 	if row > 1 {
 // 		path(process+"D", row-1, col) // go row by row
 // 	}
 
