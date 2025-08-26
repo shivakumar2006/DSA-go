57796c326eea108fe51931bc741b01154898c4c2
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-26 20:48:12 +0530

    commit

--- Recursion/Backtracking/allPath.go
+++ Recursion/Backtracking/allPath.go
@@ -21,25 +21,53 @@ func path(process string, maze [][]bool, row, col int) {
 		return
 	}
 
-	// i am considering this block in my path
 	maze[row][col] = false
 
 	if row < len(maze)-1 {
 		path(process+"D", maze, row+1, col)
 	}
-
 	if col < len(maze[0])-1 {
 		path(process+"R", maze, row, col+1)
 	}
-
 	if row > 0 {
 		path(process+"U", maze, row-1, col)
 	}
-
 	if col > 0 {
 		path(process+"L", maze, row, col-1)
 	}
 
-	// Backtrack unmark this cell
 	maze[row][col] = true
 }
+
+// func path(process string, maze [][]bool, row, col int) {
+// 	if row == len(maze)-1 && col == len(maze[0])-1 {
+// 		fmt.Println(process)
+// 		return
+// 	}
+
+// 	if !maze[row][col] {
+// 		return
+// 	}
+
+// 	// i am considering this block in my path
+// 	maze[row][col] = false
+
+// 	if row < len(maze)-1 {
+// 		path(process+"D", maze, row+1, col)
+// 	}
+
+// 	if col < len(maze[0])-1 {
+// 		path(process+"R", maze, row, col+1)
+// 	}
+
+// 	if row > 0 {
+// 		path(process+"U", maze, row-1, col)
+// 	}
+
+// 	if col > 0 {
+// 		path(process+"L", maze, row, col-1)
+// 	}
+
+// 	// Backtrack unmark this cell
+// 	maze[row][col] = true
+// }
