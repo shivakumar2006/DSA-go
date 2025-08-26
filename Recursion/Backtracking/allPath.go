a06dab9600b42ba8df4df2467db3761bc96b8849
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-26 20:40:37 +0530

    commit

---
+++ Recursion/Backtracking/allPath.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	maze := [][]bool{
+		{true, true, true},
+		{true, true, true},
+		{true, true, true},
+	}
+	path("", maze, 0, 0)
+}
+
+func path(process string, maze [][]bool, row, col int) {
+	if row == len(maze)-1 && col == len(maze[0])-1 {
+		fmt.Println(process)
+		return
+	}
+
+	if !maze[row][col] {
+		return
+	}
+
+	// i am considering this block in my path
+	maze[row][col] = false
+
+	if row < len(maze)-1 {
+		path(process+"D", maze, row+1, col)
+	}
+
+	if col < len(maze[0])-1 {
+		path(process+"R", maze, row, col+1)
+	}
+
+	if row > 0 {
+		path(process+"U", maze, row-1, col)
+	}
+
+	if col > 0 {
+		path(process+"L", maze, row, col-1)
+	}
+
+	// Backtrack unmark this cell
+	maze[row][col] = true
+}
