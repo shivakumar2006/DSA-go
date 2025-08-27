20ebb9b0366072ac9314b59365400363e6ab5015
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-27 12:09:09 +0530

    commit

---
+++ Arrays/linearSearch/random.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 100, 500, 320}
+	tar := 500
+	ans := find(arr, tar)
+	fmt.Println("the index of the targer element is : ", ans)
+}
+
+func find(arr []int, tar int) int {
+	for i := 0; i <= len(arr); i++ {
+		if arr[i] == tar {
+			return i
+		}
+	}
+	return -1
+}
