0e5e1b8edb5bccb5055adf46ae1bba0ef6e32376
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-27 10:38:44 +0530

    commit

--- Recursion/Backtracking/allPath2.go
+++ Recursion/Backtracking/allPath2.go
@@ -117,6 +117,60 @@ func allPath(process string, maze [][]bool, row, col int, steps [][]int, step in
 // 		{true, true, true},
 // 	}
 
+// 	steps := make([][]int, len(maze))
+// 	for i := range steps {
+// 		steps[i] = make([]int, len(maze[0]))
+// 	}
+
+// 	allPath("", maze, 0, 0, steps, 1)
+// }
+
+// func allPath(process string, maze [][]bool, row, col int, steps [][]int, step int) {
+// 	if row == len(maze)-1 && col == len(maze[0])-1 {
+// 		steps[row][col] = step
+// 		for _, arr := range steps {
+// 			fmt.Println(arr)
+// 		}
+// 		fmt.Println(process)
+// 		fmt.Println()
+// 		return
+// 	}
+
+// 	if !maze[row][col] {
+// 		return
+// 	}
+
+// 	maze[row][col] = false
+// 	steps[row][col] = step
+
+// 	if row < len(maze)-1 {
+// 		allPath(process+"D", maze, row+1, col, steps, step+1)
+// 	}
+// 	if col < len(maze[0])-1 {
+// 		allPath(process+"R", maze, row, col+1, steps, step+1)
+// 	}
+// 	if row > 0 {
+// 		allPath(process+"U", maze, row-1, col, steps, step+1)
+// 	}
+// 	if col > 0 {
+// 		allPath(process+"L", maze, row, col-1, steps, step+1)
+// 	}
+
+// 	maze[row][col] = true
+// 	steps[row][col] = step
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	maze := [][]bool{
+// 		{true, true, true},
+// 		{true, true, true},
+// 		{true, true, true},
+// 	}
+
 // 	// create steps board
 // 	steps := make([][]int, len(maze))
 // 	for i := range steps {
