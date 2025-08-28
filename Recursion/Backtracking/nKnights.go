fa796b99bb9dd8da52426ab763f48e6a15daa119
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-28 12:23:02 +0530

    commit

--- Recursion/Backtracking/nKnights.go
+++ Recursion/Backtracking/nKnights.go
@@ -16,33 +16,48 @@ func knight(board [][]bool, row, col int, knights int) {
 	if knights == 0 {
 		display(board)
 		fmt.Println()
-		return 
+		return
 	}
 
 	if row == len(board) {
-		return 
+		return
 	}
 
 	if col == len(board) {
 		knight(board, row+1, 0, knights)
-		return 
+		return
 	}
 
 	if isSafe(board, row, col) {
-		board[row][col] = true 
-		knight(board, row, col+1, knights-1) 
-		board[row][col] = false 
+		board[row][col] = true
+		knight(board, row, col+1, knights-1)
+		board[row][col] = false
 	}
 	knight(board, row, col+1, knights)
 }
 
 func isSafe(board [][]bool, row, col int) bool {
 	if isValid(board, row-2, col-1) && board[row-2][col-1] {
-		return false 
+		return false
 	}
-	if isValid(board, row-1, col-1, )
+	if isValid(board, row-2, col+1) && board[row-2][col+1] {
+		return false
+	}
+	if isValid(board, row-1, col-2) && board[row-1][col-2] {
+		return false
+	}
+	if isValid(board, row-1, col+2) && board[row-1][col+2] {
+		return false
+	}
 
-	return true 
+	return true
+}
+
+func isValid(board [][]bool, row, col int) bool {
+	if row >= 0 && row < len(board) && col >= 0 && col < len(board) {
+		return true
+	}
+	return false
 }
 
 func display(board [][]bool) {
