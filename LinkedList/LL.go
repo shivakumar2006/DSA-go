c17554405f8d7bd1bc934ba8ae114f9e2f7c8e62
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-29 12:45:46 +0530

    commit

--- LinkedList/LL.go
+++ LinkedList/LL.go
@@ -1,6 +1,6 @@
-package main
+// Insert nodes in a singly linkedlist and also insertLast node in this list
 
-import "fmt"
+package main
 
 type Node struct {
 	data int
@@ -12,45 +12,104 @@ type LinkedList struct {
 	tail *Node
 }
 
-// insert
-func (l *LinkedList) Insert(value int) {
+func (l *LinkedList) Inset(value int) {
 	newNode := &Node{data: value}
 	if l.head == nil {
 		l.head = newNode
+		l.tail = newNode // because, first node is also a tail node
 		return
 	}
 
-	temp := l.head
-	for temp.next != nil {
-		temp = temp.next
-	}
-	temp.next = newNode
 }
 
-// insert last
-func (l *LinkedList) InsertLast(value int) {
-	newNode := &Node{data: value}
-	if l.tail == nil {
-		l.tail = newNode
-		return
-	}
+// package main
 
-	temp := l.tail
-	for temp.next != nil {
-		temp = temp.next
-	}
-	temp.next = newNode
-}
+// import "fmt"
 
-// display
-func (l *LinkedList) Display() {
-	temp := l.head
-	for temp != nil {
-		fmt.Print(temp.data, " -> ")
-		temp = temp.next
-	}
-	fmt.Println("nil")
-}
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// 	tail *Node
+// }
+
+// // insert head
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		l.tail = newNode // because, first node is both head and tail
+// 	} else {
+// 		newNode.next = l.head
+// 		l.head = newNode
+// 	}
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("End")
+// }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// 	tail *Node
+// }
+
+// // insert
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// // insert last
+// func (l *LinkedList) InsertLast(value int) {
+// 	newNode := &Node{data: value}
+// 	if l.tail == nil {
+// 		l.tail = newNode
+// 		return
+// 	}
+
+// 	temp := l.tail
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// // display
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("nil")
+// }
 
 // // Node represents a single node
 // type Node struct {
