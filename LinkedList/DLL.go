ce0dd47eb33f02515f485f893fe5731d587f425e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-30 13:35:50 +0530

    commit

--- LinkedList/DLL.go
+++ LinkedList/DLL.go
@@ -20,16 +20,25 @@ func (l *DLL) Insert(value int) {
 	newNode.next = l.head
 	newNode.prev = nil
 	if l.head != nil {
-		l.head.prev = l.head
+		l.head.prev = newNode
 	}
 	l.head = newNode
 }
 
 func (l *DLL) Display() {
 	temp := l.head
+	var last *Node
 	for temp != nil {
 		fmt.Print(temp.data, " <-> ")
+		last = temp
 		temp = temp.next
 	}
 	fmt.Println("end")
+
+	fmt.Println("print in reverse")
+	for last != nil {
+		fmt.Print(last.data, " <-> ")
+		last = last.prev
+	}
+	fmt.Println("start")
 }
