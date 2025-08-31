a5e85b2cbfef3401e495399ed293767bb20499f8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-31 09:33:44 +0530

    commit

--- LinkedList/Interview/insertList.go
+++ LinkedList/Interview/insertList.go
@@ -2,6 +2,8 @@
 
 package main
 
+import "fmt"
+
 type Node struct {
 	data int
 	next *Node
@@ -9,5 +11,48 @@ type Node struct {
 
 type LinkedList struct {
 	head *Node
-	tail *Node
+}
+
+func (l *LinkedList) InsertAt(value int, index int) {
+	l.head = insertRec(l.head, value, index)
+}
+
+func insertRec(temp *Node, value int, index int) *Node {
+	if index == 0 {
+		// create new node and link
+		newNode := &Node{data: value}
+		newNode.next = temp
+		return newNode
+	}
+
+	if temp == nil {
+		fmt.Println("index out of bound")
+		return nil
+	}
+
+	// move to the next node with index-1
+	temp.next = insertRec(temp.next, value, index-1)
+	return temp
+}
+
+// function to print list
+func (l *LinkedList) Print() {
+	temp := l.head
+	for temp != nil {
+		fmt.Print(temp.data, " -> ")
+		temp = temp.next
+	}
+	fmt.Println("end")
+}
+
+func main() {
+	list := &LinkedList{}
+
+	list.InsertAt(50, 0)
+	list.InsertAt(40, 1)
+	list.InsertAt(10, 2)
+	list.InsertAt(60, 3)
+	list.InsertAt(5, 2) // insert at the
+
+	list.Print()
 }
