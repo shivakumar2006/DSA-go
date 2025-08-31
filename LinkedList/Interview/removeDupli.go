5fb7f21939502e85f9c00c81de8d4f1550957c79
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-31 12:26:34 +0530

    commit

--- LinkedList/Interview/removeDupli.go
+++ LinkedList/Interview/removeDupli.go
@@ -1,4 +1,4 @@
-// Remove duplicafes from sorted linkedlist
+// // Remove duplicafes from sorted linkedlist
 
 package main
 
@@ -26,21 +26,6 @@ func (l *LinkedList) Insert(value int) {
 	l.tail = newNode
 }
 
-func (l *LinkedList) Duplicates() {
-	temp := l.head
-
-	for temp.next != nil {
-		if temp.data == temp.next.data {
-			temp.next = temp.next.next
-		} else {
-			temp = temp.next
-		}
-	}
-
-	l.tail = temp
-	l.tail.next = nil
-}
-
 func (l *LinkedList) Display() {
 	temp := l.head
 	for temp != nil {
@@ -57,9 +42,72 @@ func main() {
 	list.Insert(1)
 	list.Insert(1)
 	list.Insert(2)
+	list.Insert(3)
 	list.Insert(4)
 	list.Insert(4)
-	list.Display()
-	list.Duplicates()
 	list.Display()
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// 	tail *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		l.tail = newNode
+// 		return
+// 	}
+
+// 	l.tail.next = newNode
+// 	l.tail = newNode
+// }
+
+// func (l *LinkedList) Duplicates() {
+// 	temp := l.head
+
+// 	for temp.next != nil {
+// 		if temp.data == temp.next.data {
+// 			temp.next = temp.next.next
+// 		} else {
+// 			temp = temp.next
+// 		}
+// 	}
+
+// 	l.tail = temp
+// 	l.tail.next = nil
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("End")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(1)
+// 	list.Insert(1)
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(4)
+// 	list.Insert(4)
+// 	list.Display()
+// 	list.Duplicates()
+// 	list.Display()
+// }
