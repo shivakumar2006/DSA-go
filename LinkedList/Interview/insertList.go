11d833bdfdb7a22e50edeb02fbccfc07a0ef4262
shivakumar2006 <official.shivakumar06@gmail.com>
2025-08-31 09:57:28 +0530

    commit

--- LinkedList/Interview/insertList.go
+++ LinkedList/Interview/insertList.go
@@ -17,26 +17,24 @@ func (l *LinkedList) InsertAt(value int, index int) {
 	l.head = insertRec(l.head, value, index)
 }
 
-func insertRec(temp *Node, value int, index int) *Node {
+func insertRec(temp *Node, value, index int) *Node {
 	if index == 0 {
-		// create new node and link
 		newNode := &Node{data: value}
 		newNode.next = temp
 		return newNode
 	}
 
 	if temp == nil {
-		fmt.Println("index out of bound")
+		fmt.Println("index is out of bound")
 		return nil
 	}
 
-	// move to the next node with index-1
+	// now move to the next node with index-1
 	temp.next = insertRec(temp.next, value, index-1)
 	return temp
 }
 
-// function to print list
-func (l *LinkedList) Print() {
+func (l *LinkedList) Display() {
 	temp := l.head
 	for temp != nil {
 		fmt.Print(temp.data, " -> ")
@@ -54,5 +52,46 @@ func main() {
 	list.InsertAt(60, 3)
 	list.InsertAt(5, 2) // insert at the
 
-	list.Print()
+	list.Display()
 }
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) InsertAt(value int, index int) {
+// 	l.head = insertRec(l.head, value, index)
+// }
+
+// func insertRec(temp *Node, value int, index int) *Node {
+// 	if index == 0 {
+// 		// create new node and link
+// 		newNode := &Node{data: value}
+// 		newNode.next = temp
+// 		return newNode
+// 	}
+
+// 	if temp == nil {
+// 		fmt.Println("index out of bound")
+// 		return nil
+// 	}
+
+// 	// move to the next node with index-1
+// 	temp.next = insertRec(temp.next, value, index-1)
+// 	return temp
+// }
+
+// // function to print list
+// func (l *LinkedList) Print() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("end")
+// }
