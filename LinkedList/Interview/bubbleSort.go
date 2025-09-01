2ba4260fdced142a083aa380043812d563eb8785
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-01 22:06:11 +0530

    comit

--- LinkedList/Interview/bubbleSort.go
+++ LinkedList/Interview/bubbleSort.go
@@ -1,1 +1,50 @@
 package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (l *LinkedList) Insert(value int) {
+	newNode := &Node{data: value}
+
+	if l.head == nil {
+		l.head = newNode
+		return
+	}
+
+	temp := l.head
+	for temp.next != nil {
+		temp = temp.next
+	}
+
+	temp.next = newNode
+}
+
+func (l *LinkedList) Display() {
+	temp := l.head
+	for temp != nil {
+		fmt.Print(temp.data, " -> ")
+		temp = temp.next
+	}
+	fmt.Println("End")
+}
+
+func main() {
+	list := &LinkedList{}
+
+	list.Insert(4)
+	list.Insert(5)
+	list.Insert(2)
+	list.Insert(1)
+	list.Insert(3)
+
+	fmt.Print("Original list : ")
+	list.Display()
+}
