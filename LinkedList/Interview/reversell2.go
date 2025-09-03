f7a38d67f0bcac8e618bdcbda6beaa94f5374f7e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-03 17:42:51 +0530

    commit

---
+++ LinkedList/Interview/reversell2.go
@@ -0,0 +1,51 @@
+// Reverse linked list 2
+// Google, ,microsoft, facebook question
+
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (l *LinkedList) Insert(value int) {
+	newNode := &Node{data: value}
+
+	if l.head == nil {
+		l.head = newNode
+		return
+	}
+
+	temp := l.head
+	for temp.next != nil {
+		temp = temp.next
+	}
+	temp.next = newNode
+}
+
+func (l *LinkedList) Display() {
+	temp := l.head
+	for temp != nil {
+		fmt.Print(temp.data, " -> ")
+		temp = temp.next
+	}
+	fmt.Println("End")
+}
+
+func main() {
+	list := &LinkedList{}
+
+	list.Insert(1)
+	list.Insert(2)
+	list.Insert(3)
+	list.Insert(4)
+	list.Insert(5)
+
+	list.Display()
+}
