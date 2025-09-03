40a1a6a9d0758dc3b51fdea7015cc1df470a6f94
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-03 17:23:36 +0530

    commit

--- LinkedList/Interview/reversell.go
+++ LinkedList/Interview/reversell.go
@@ -25,7 +25,6 @@ func (l *LinkedList) Insert(value int) {
 	for temp.next != nil {
 		temp = temp.next
 	}
-
 	temp.next = newNode
 }
 
@@ -57,17 +56,89 @@ func (l *LinkedList) Display() {
 func main() {
 	list := &LinkedList{}
 
-	list.Insert(5)
-	list.Insert(3)
 	list.Insert(1)
 	list.Insert(2)
+	list.Insert(3)
 	list.Insert(4)
+	list.Insert(5)
 
-	fmt.Print("Original list : ")
+	fmt.Print("original list : ")
 	list.Display()
 
 	list.reverseIterative()
 
-	fmt.Print("Rotated list : ")
+	fmt.Print("reverse list : ")
 	list.Display()
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+
+// 	temp.next = newNode
+// }
+
+// func (l *LinkedList) reverseIterative() {
+// 	var prev *Node = nil
+// 	var current *Node = l.head
+// 	var next *Node = current.next
+
+// 	for current != nil {
+// 		current.next = prev
+// 		prev = current
+// 		current = next
+// 		if next != nil {
+// 			next = next.next
+// 		}
+// 	}
+// 	l.head = prev
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("End")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(5)
+// 	list.Insert(3)
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(4)
+
+// 	fmt.Print("Original list : ")
+// 	list.Display()
+
+// 	list.reverseIterative()
+
+// 	fmt.Print("Rotated list : ")
+// 	list.Display()
+// }
