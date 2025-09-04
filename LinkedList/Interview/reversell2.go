fa1fed53b00863cad73284ba3cb021a892a7f789
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-04 16:13:53 +0530

    commit

--- LinkedList/Interview/reversell2.go
+++ LinkedList/Interview/reversell2.go
@@ -29,6 +29,43 @@ func (l *LinkedList) Insert(value int) {
 	temp.next = newNode
 }
 
+func (l *LinkedList) reverseBetween(head *Node, left, right int) *Node {
+	if left == right {
+		return head
+	}
+
+	// iterate till left
+	current := head
+	var prev *Node = nil
+	for i := 0; i < left-1; i++ {
+		prev = current
+		current = current.next
+	}
+
+	last := prev
+	newEnd := current
+
+	// now rotate between the left and right part of the list
+	var next *Node = current.next
+	for i := 0; current != nil && i < right-left+1; i++ {
+		current.next = prev
+		prev = current
+		current = next
+		if next != nil {
+			next = next.next
+		}
+	}
+
+	if last != nil {
+		last.next = prev
+	} else {
+		head = prev
+	}
+
+	newEnd.next = current
+	return head
+}
+
 func (l *LinkedList) Display() {
 	temp := l.head
 	for temp != nil {
@@ -48,6 +85,11 @@ func main() {
 	list.Insert(5)
 
 	fmt.Print("original list : ")
+	list.Display()
+
+	list.head = list.reverseBetween(list.head, 2, 4)
+
+	fmt.Print("reverse between 2 and 4 : ")
 	list.Display()
 }
 
