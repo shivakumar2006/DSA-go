e653f24b63f45b46f1934701c8ee16f9be17c613
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-04 16:23:52 +0530

    commit

--- LinkedList/Interview/reversell2.go
+++ LinkedList/Interview/reversell2.go
@@ -26,6 +26,7 @@ func (l *LinkedList) Insert(value int) {
 	for temp.next != nil {
 		temp = temp.next
 	}
+
 	temp.next = newNode
 }
 
@@ -34,9 +35,10 @@ func (l *LinkedList) reverseBetween(head *Node, left, right int) *Node {
 		return head
 	}
 
-	// iterate till left
 	current := head
 	var prev *Node = nil
+
+	// now iterate till left-1 node
 	for i := 0; i < left-1; i++ {
 		prev = current
 		current = current.next
@@ -45,7 +47,7 @@ func (l *LinkedList) reverseBetween(head *Node, left, right int) *Node {
 	last := prev
 	newEnd := current
 
-	// now rotate between the left and right part of the list
+	// now reverse the list between 2 and 4
 	var next *Node = current.next
 	for i := 0; current != nil && i < right-left+1; i++ {
 		current.next = prev
@@ -56,6 +58,7 @@ func (l *LinkedList) reverseBetween(head *Node, left, right int) *Node {
 		}
 	}
 
+	// now reconnect the reverse list on both side
 	if last != nil {
 		last.next = prev
 	} else {
@@ -84,14 +87,106 @@ func main() {
 	list.Insert(4)
 	list.Insert(5)
 
-	fmt.Print("original list : ")
+	fmt.Print("Original list : ")
 	list.Display()
 
 	list.head = list.reverseBetween(list.head, 2, 4)
 
-	fmt.Print("reverse between 2 and 4 : ")
+	fmt.Print("reverse list between 2 and 4 ")
 	list.Display()
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// func (l *LinkedList) reverseBetween(head *Node, left, right int) *Node {
+// 	if left == right {
+// 		return head
+// 	}
+
+// 	// iterate till left
+// 	current := head
+// 	var prev *Node = nil
+// 	for i := 0; i < left-1; i++ {
+// 		prev = current
+// 		current = current.next
+// 	}
+
+// 	last := prev
+// 	newEnd := current
+
+// 	// now rotate between the left and right part of the list
+// 	var next *Node = current.next
+// 	for i := 0; current != nil && i < right-left+1; i++ {
+// 		current.next = prev
+// 		prev = current
+// 		current = next
+// 		if next != nil {
+// 			next = next.next
+// 		}
+// 	}
+
+// 	if last != nil {
+// 		last.next = prev
+// 	} else {
+// 		head = prev
+// 	}
+
+// 	newEnd.next = current
+// 	return head
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("End")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(3)
+// 	list.Insert(4)
+// 	list.Insert(5)
+
+// 	fmt.Print("original list : ")
+// 	list.Display()
+
+// 	list.head = list.reverseBetween(list.head, 2, 4)
+
+// 	fmt.Print("reverse between 2 and 4 : ")
+// 	list.Display()
+// }
 
 // package main
 
