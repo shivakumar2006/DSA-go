cb1ec7b6e266fff24f20eb2ea8e4409164b2c4d2
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-06 19:23:34 +0530

    commit

--- LinkedList/Interview/reorderlist.go
+++ LinkedList/Interview/reorderlist.go
@@ -1,5 +1,5 @@
-// Reorder list
-// Google, Facebook question
+// // Reorder list
+// // Google, Facebook question
 
 package main
 
@@ -60,7 +60,7 @@ func reorderList(head *Node) {
 
 	mid := middleNode(head)
 	headSecond := reverseList(mid.next)
-	mid.next = nil // break first half
+	mid.next = nil // break into half
 
 	headFirst := head
 
@@ -94,28 +94,78 @@ func main() {
 	list.Insert(4)
 	list.Insert(5)
 
-	fmt.Print("original list : ")
+	fmt.Print("Original list : ")
 	list.Display()
 
 	reorderList(list.head)
 
-	fmt.Print("reorder list : ")
+	fmt.Print("reordered list : ")
 	list.Display()
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// func middleNode(head *Node) *Node {
+// 	slow := head
+// 	fast := head
+
+// 	for fast != nil && fast.next != nil {
+// 		slow = slow.next
+// 		fast = fast.next.next
+// 	}
+// 	return slow
+// }
+
+// func reverseList(head *Node) *Node {
+// 	var prev *Node
+// 	current := head
+
+// 	for current != nil {
+// 		next := current.next
+// 		current.next = prev
+// 		prev = current
+// 		current = next
+// 	}
+// 	return prev
+// }
 
 // func reorderList(head *Node) {
 // 	if head == nil || head.next == nil {
 // 		return
 // 	}
 
-// 	// 1. Find middle
 // 	mid := middleNode(head)
 // 	headSecond := reverseList(mid.next)
 // 	mid.next = nil // break first half
 
 // 	headFirst := head
 
-// 	// 2. Merge two halves
 // 	for headSecond != nil {
 // 		temp1 := headFirst.next
 // 		temp2 := headSecond.next
@@ -127,3 +177,55 @@ func main() {
 // 		headSecond = temp2
 // 	}
 // }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("END")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(3)
+// 	list.Insert(4)
+// 	list.Insert(5)
+
+// 	fmt.Print("original list : ")
+// 	list.Display()
+
+// 	reorderList(list.head)
+
+// 	fmt.Print("reorder list : ")
+// 	list.Display()
+// }
+
+// // func reorderList(head *Node) {
+// // 	if head == nil || head.next == nil {
+// // 		return
+// // 	}
+
+// // 	// 1. Find middle
+// // 	mid := middleNode(head)
+// // 	headSecond := reverseList(mid.next)
+// // 	mid.next = nil // break first half
+
+// // 	headFirst := head
+
+// // 	// 2. Merge two halves
+// // 	for headSecond != nil {
+// // 		temp1 := headFirst.next
+// // 		temp2 := headSecond.next
+
+// // 		headFirst.next = headSecond
+// // 		headSecond.next = temp1
+
+// // 		headFirst = temp1
+// // 		headSecond = temp2
+// // 	}
+// // }
