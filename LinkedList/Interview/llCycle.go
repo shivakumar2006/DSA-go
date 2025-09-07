b5d8312c7118566601a8ef0a03d1e758be5fe09d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-07 19:24:57 +0530

    commit

--- LinkedList/Interview/llCycle.go
+++ LinkedList/Interview/llCycle.go
@@ -26,17 +26,16 @@ func (l *LinkedList) Insert(value int) {
 	for temp.next != nil {
 		temp = temp.next
 	}
-
 	temp.next = newNode
 }
 
 func (l *LinkedList) hasCycle() bool {
-	fast := l.head
 	slow := l.head
+	fast := l.head
 
 	for fast != nil && fast.next != nil {
-		fast = fast.next.next
 		slow = slow.next
+		fast = fast.next.next
 		if fast == slow {
 			return true
 		}
@@ -44,34 +43,16 @@ func (l *LinkedList) hasCycle() bool {
 	return false
 }
 
-func (l *LinkedList) cycleLength() int {
-	fast := l.head
-	slow := l.head
-
-	if fast != nil && fast.next != nil {
-		fast = fast.next.next
-		slow = slow.next
-		if fast == slow {
-			// calculate the length
-			temp := slow.next
-			length := 1
-			for temp != slow {
-				temp = temp.next
-				length++
-			}
-			return length
-		}
-	}
-	return 0
-}
-
-func (l *LinkedList) Display() {
+func (l *LinkedList) Display(limit int) {
 	temp := l.head
-	for temp != nil {
+	count := 0
+
+	for temp != nil && count < limit {
 		fmt.Print(temp.data, " -> ")
 		temp = temp.next
+		count++
 	}
-	fmt.Println("End")
+	fmt.Println("...")
 }
 
 func main() {
@@ -84,7 +65,6 @@ func main() {
 	list.Insert(5)
 	list.Insert(6)
 
-	// create a cycle for testing
 	temp := list.head
 	var thirdNode *Node
 	for temp.next != nil {
@@ -93,16 +73,119 @@ func main() {
 		}
 		temp = temp.next
 	}
-	temp.next = thirdNode // create the cycle
+	temp.next = thirdNode
 
 	if list.hasCycle() {
-		fmt.Println("cycle is present in the list")
-		length := list.cycleLength()
-		fmt.Println("Cycle length : ", length)
+		fmt.Println("Cycle is present in the list...")
 	} else {
-		fmt.Println("cycle is not present in the list")
+		fmt.Println("Cycle is not present in the list.")
 	}
+
+	list.Display(10)
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+
+// 	temp.next = newNode
+// }
+
+// func (l *LinkedList) hasCycle() bool {
+// 	fast := l.head
+// 	slow := l.head
+
+// 	for fast != nil && fast.next != nil {
+// 		fast = fast.next.next
+// 		slow = slow.next
+// 		if fast == slow {
+// 			return true
+// 		}
+// 	}
+// 	return false
+// }
+
+// func (l *LinkedList) cycleLength() int {
+// 	fast := l.head
+// 	slow := l.head
+
+// 	for fast != nil && fast.next != nil {
+// 		fast = fast.next.next
+// 		slow = slow.next
+// 		if fast == slow {
+// 			// calculate the length
+// 			length := 1
+// 			temp := slow.next
+// 			for temp != slow {
+// 				temp = temp.next
+// 				length++
+// 			}
+// 			return length
+// 		}
+// 	}
+// 	return 0
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("End")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(3)
+// 	list.Insert(4)
+// 	list.Insert(5)
+// 	list.Insert(6)
+
+// 	// create a cycle for testing
+// 	temp := list.head
+// 	var thirdNode *Node
+// 	for temp.next != nil {
+// 		if temp.data == 3 {
+// 			thirdNode = temp
+// 		}
+// 		temp = temp.next
+// 	}
+// 	temp.next = thirdNode // create the cycle
+
+// 	if list.hasCycle() {
+// 		fmt.Println("cycle is present in the list")
+// 		length := list.cycleLength()
+// 		fmt.Println("Cycle length : ", length)
+// 	} else {
+// 		fmt.Println("cycle is not present in the list")
+// 	}
+// }
 
 // func (l *LinkedList) hasCycle() bool {
 // 	fast := l.head
