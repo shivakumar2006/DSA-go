441a2a34163e3ac114550ee88c9705c1afbf4bd4
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-07 08:46:52 +0530

    commit

--- LinkedList/Interview/reverseKNode.go
+++ LinkedList/Interview/reverseKNode.go
@@ -1,4 +1,4 @@
-// Reverse K Nodes
+// // Reverse K Nodes
 
 package main
 
@@ -93,6 +93,103 @@ func main() {
 
 	list.head = reverseKGroup(list.head, 3)
 
-	fmt.Print("Reversed in K Groups: ")
+	fmt.Print("reverse k node list : ")
 	list.Display()
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// func reverseKGroup(head *Node, k int) *Node {
+// 	if k <= 1 || head == nil {
+// 		return head
+// 	}
+
+// 	var prev *Node = nil
+// 	current := head
+
+// 	for {
+// 		last := prev
+// 		newEnd := current
+
+// 		// reverse between left and right
+// 		var next *Node = current.next
+// 		for i := 0; current != nil && i < k; i++ {
+// 			current.next = prev
+// 			prev = current
+// 			current = next
+// 			if next != nil {
+// 				next = next.next
+// 			}
+// 		}
+
+// 		if last != nil {
+// 			last.next = prev
+// 		} else {
+// 			head = prev
+// 		}
+
+// 		newEnd.next = current
+
+// 		if current == nil {
+// 			break
+// 		}
+
+// 		prev = newEnd
+// 	}
+// 	return head
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("END")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(3)
+// 	list.Insert(4)
+// 	list.Insert(5)
+// 	list.Insert(6)
+// 	list.Insert(7)
+
+// 	fmt.Print("Original list : ")
+// 	list.Display()
+
+// 	list.head = reverseKGroup(list.head, 3)
+
+// 	fmt.Print("Reversed in K Groups: ")
+// 	list.Display()
+// }
