30c87e5b080dece9299a361064e7f799063e7b67
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-08 11:00:24 +0530

    commit

--- LinkedList/Interview/rotatedll.go
+++ LinkedList/Interview/rotatedll.go
@@ -28,32 +28,6 @@ func (l *LinkedList) Insert(value int) {
 	temp.next = newNode
 }
 
-func rotateRight(head *Node, k int) *Node {
-	if k <= 0 || head == nil || head.next == nil {
-		return head
-	}
-
-	last := head
-	length := 1
-	for last.next != nil {
-		last = last.next
-		length++
-	}
-	last.next = head
-
-	rotations := k % length
-	skip := length - rotations
-	newLast := head
-	for i := 0; i < skip-1; i++ {
-		newLast = newLast.next
-	}
-
-	head = newLast.next
-	newLast.next = nil
-
-	return head
-}
-
 func (l *LinkedList) Display() {
 	temp := l.head
 	for temp != nil {
@@ -74,13 +48,89 @@ func main() {
 
 	fmt.Print("Original list : ")
 	list.Display()
-
-	list.head = rotateRight(list.head, 2)
-
-	fmt.Print("rotated linked list : ")
-	list.Display()
-
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type LinkedList struct {
+// 	head *Node
+// }
+
+// func (l *LinkedList) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// func rotateRight(head *Node, k int) *Node {
+// 	if k <= 0 || head == nil || head.next == nil {
+// 		return head
+// 	}
+
+// 	last := head
+// 	length := 1
+// 	for last.next != nil {
+// 		last = last.next
+// 		length++
+// 	}
+// 	last.next = head
+
+// 	rotations := k % length
+// 	skip := length - rotations
+// 	newLast := head
+// 	for i := 0; i < skip-1; i++ {
+// 		newLast = newLast.next
+// 	}
+
+// 	head = newLast.next
+// 	newLast.next = nil
+
+// 	return head
+// }
+
+// func (l *LinkedList) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("END")
+// }
+
+// func main() {
+// 	list := &LinkedList{}
+
+// 	list.Insert(1)
+// 	list.Insert(2)
+// 	list.Insert(3)
+// 	list.Insert(4)
+// 	list.Insert(5)
+
+// 	fmt.Print("Original list : ")
+// 	list.Display()
+
+// 	list.head = rotateRight(list.head, 2)
+
+// 	fmt.Print("rotated linked list : ")
+// 	list.Display()
+
+// }
 
 // package main
 
