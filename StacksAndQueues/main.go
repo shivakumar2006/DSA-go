6289551d4ad171876f4ea678819a2486d472f4ca
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-08 11:21:20 +0530

    commit

---
+++ StacksAndQueues/main.go
@@ -0,0 +1,23 @@
+// Stacks & Queues
+
+package main
+
+import "fmt"
+
+func main() {
+	stack := []int{}
+
+	stack = append(stack, 10)
+	stack = append(stack, 20)
+	stack = append(stack, 30)
+	stack = append(stack, 40)
+	stack = append(stack, 50)
+
+	fmt.Println("Stacks : ", stack)
+
+	// pop
+	top := stack[len(stack)-1]
+	stack = stack[:len(stack)-1]
+	fmt.Println("Popped : ", top)
+	fmt.Println("Stack now : ", stack)
+}
