968e23cdecb53d3ef13ea4a277fe78b3fafe2c89
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-09 16:03:16 +0530

    commut

--- StacksAndQueues/Interview/queueUsingStack.go
+++ StacksAndQueues/Interview/queueUsingStack.go
@@ -20,23 +20,19 @@ func NewQueueUsingStack() *queueUsingStack {
 	}
 }
 
-// first add or Enqueue all the elements in the stack 1
 func (q *queueUsingStack) Enqueue(value int) {
 	q.stack1 = append(q.stack1, value)
 }
 
-// check if its empty
 func (q *queueUsingStack) isEmpty() bool {
 	return len(q.stack1) == 0 && len(q.stack2) == 0
 }
 
-// now add all the elements in stack 2 from stack 1
 func (q *queueUsingStack) Dequeue() (int, error) {
 	if q.isEmpty() {
-		return 0, errors.New("Queue is empty ! ")
+		return 0, errors.New("queue is empty!")
 	}
 
-	// now shift
 	if len(q.stack2) == 0 {
 		for len(q.stack1) > 0 {
 			n := len(q.stack1)
@@ -46,17 +42,16 @@ func (q *queueUsingStack) Dequeue() (int, error) {
 		}
 	}
 
-	// now pop the first element from stack 2 which is 10
+	// pop the top element from stack 2 which is 10
 	n := len(q.stack2)
-	fornt := q.stack2[n-1]
+	top := q.stack2[n-1]
 	q.stack2 = q.stack2[:n-1]
-	return fornt, nil
+	return top, nil
 }
 
-// now return the front or top element of the stack 2 which is 20
 func (q *queueUsingStack) Front() (int, error) {
 	if q.isEmpty() {
-		return 0, errors.New("Queue is empty!")
+		return 0, errors.New("Empty!")
 	}
 
 	if len(q.stack2) == 0 {
@@ -67,20 +62,18 @@ func (q *queueUsingStack) Front() (int, error) {
 			q.stack2 = append(q.stack2, top)
 		}
 	}
-
 	return q.stack2[len(q.stack2)-1], nil
 }
 
-// now display
 func (q *queueUsingStack) Display() {
 	if q.isEmpty() {
 		fmt.Println("Empty!")
 		return
 	}
 
-	// print stack 2
+	// print stack2
 	for i := len(q.stack2) - 1; i >= 0; i-- {
-		fmt.Printf("%d -> ", q.stack2[i])
+		fmt.Printf("%d <- ", q.stack2[i])
 	}
 
 	// print stack 1
@@ -108,6 +101,114 @@ func main() {
 	front, _ := q.Front()
 	fmt.Println("Front : ", front)
 }
+
+// package main
+
+// import (
+// 	"errors"
+// 	"fmt"
+// )
+
+// type queueUsingStack struct {
+// 	stack1 []int
+// 	stack2 []int
+// }
+
+// func NewQueueUsingStack() *queueUsingStack {
+// 	return &queueUsingStack{
+// 		stack1: []int{},
+// 		stack2: []int{},
+// 	}
+// }
+
+// // first add or Enqueue all the elements in the stack 1
+// func (q *queueUsingStack) Enqueue(value int) {
+// 	q.stack1 = append(q.stack1, value)
+// }
+
+// // check if its empty
+// func (q *queueUsingStack) isEmpty() bool {
+// 	return len(q.stack1) == 0 && len(q.stack2) == 0
+// }
+
+// // now add all the elements in stack 2 from stack 1
+// func (q *queueUsingStack) Dequeue() (int, error) {
+// 	if q.isEmpty() {
+// 		return 0, errors.New("Queue is empty ! ")
+// 	}
+
+// 	// now shift
+// 	if len(q.stack2) == 0 {
+// 		for len(q.stack1) > 0 {
+// 			n := len(q.stack1)
+// 			top := q.stack1[n-1]
+// 			q.stack1 = q.stack1[:n-1]
+// 			q.stack2 = append(q.stack2, top)
+// 		}
+// 	}
+
+// 	// now pop the first element from stack 2 which is 10
+// 	n := len(q.stack2)
+// 	fornt := q.stack2[n-1]
+// 	q.stack2 = q.stack2[:n-1]
+// 	return fornt, nil
+// }
+
+// // now return the front or top element of the stack 2 which is 20
+// func (q *queueUsingStack) Front() (int, error) {
+// 	if q.isEmpty() {
+// 		return 0, errors.New("Queue is empty!")
+// 	}
+
+// 	if len(q.stack2) == 0 {
+// 		for len(q.stack1) > 0 {
+// 			n := len(q.stack1)
+// 			top := q.stack1[n-1]
+// 			q.stack1 = q.stack1[:n-1]
+// 			q.stack2 = append(q.stack2, top)
+// 		}
+// 	}
+
+// 	return q.stack2[len(q.stack2)-1], nil
+// }
+
+// // now display
+// func (q *queueUsingStack) Display() {
+// 	if q.isEmpty() {
+// 		fmt.Println("Empty!")
+// 		return
+// 	}
+
+// 	// print stack 2
+// 	for i := len(q.stack2) - 1; i >= 0; i-- {
+// 		fmt.Printf("%d -> ", q.stack2[i])
+// 	}
+
+// 	// print stack 1
+// 	for i := 0; i < len(q.stack1); i++ {
+// 		fmt.Printf("%d -> ", q.stack1[i])
+// 	}
+
+// 	fmt.Println("END")
+// }
+
+// func main() {
+// 	q := NewQueueUsingStack()
+
+// 	q.Enqueue(10)
+// 	q.Enqueue(20)
+// 	q.Enqueue(30)
+
+// 	q.Display()
+
+// 	value, _ := q.Dequeue()
+// 	fmt.Println("Dequed : ", value)
+
+// 	q.Display()
+
+// 	front, _ := q.Front()
+// 	fmt.Println("Front : ", front)
+// }
 
 // package main
 
