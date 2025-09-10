9bc17d6112e308326002255347cb9cc0c5dd2499
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-10 12:34:17 +0530

    commit

--- StacksAndQueues/Interview/histogram.go
+++ StacksAndQueues/Interview/histogram.go
@@ -1,3 +1,44 @@
 // Largest Rectangle in histogram...
-// Google question. 
+// Google, Amazon Facebook question.
 
+package main
+
+import "fmt"
+
+func main() {
+	heights := []int{2, 1, 5, 6, 2, 3}
+	fmt.Println("Largest Rectangle area : ", largestRectangeArea(heights)) // 10
+}
+
+func largestRectangeArea(heights []int) int {
+	stack := []int{} // store indices
+	maxArea := 0
+	n := len(heights)
+
+	for i := 0; i <= n; i++ {
+		// treat i == n as height == 0
+		h := 0
+		if i < n {
+			h = heights[i]
+		}
+
+		// maintain increasing stack
+		for len(stack) > 0 && h < heights[stack[len(stack)-1]] {
+			// pop the height
+			topIndex := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			height := heights[topIndex]
+			width := i
+			if len(stack) > 0 {
+				width = i - stack[len(stack)-1] - 1
+			}
+			area := height * width
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+		stack = append(stack, i)
+	}
+	return maxArea
+}
