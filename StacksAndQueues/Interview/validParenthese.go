600dd7edb33df81b8c07e62490d840458d6e5b8e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-10 21:27:58 +0530

    commit

--- StacksAndQueues/Interview/validParenthese.go
+++ StacksAndQueues/Interview/validParenthese.go
@@ -1,3 +1,46 @@
-// Valid paranthesis...
-// Google and Meta question 
+// Valid parantheses...
+// Google and Meta question
 
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	stack := []rune{} // use rune slice to store characters
+
+	for _, ch := range s {
+		if ch == '(' || ch == '{' || ch == '[' {
+			// push opening bracket
+			stack = append(stack, ch)
+		} else {
+			// for closing brackets, check stack
+			if len(stack) == 0 {
+				return false
+			}
+
+			// pop top of stack
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if ch == ')' && top != '(' {
+				return false
+			}
+			if ch == '}' && top != '{' {
+				return false
+			}
+			if ch == ']' && top != '[' {
+				return false
+			}
+		}
+	}
+	// at the end, stack must be empty
+	return len(stack) == 0
+}
+
+func main() {
+	fmt.Println(isValid("()"))     // true
+	fmt.Println(isValid("()[]{}")) // true
+	fmt.Println(isValid("(]"))     // false
+	fmt.Println(isValid("([)]"))   // false
+	fmt.Println(isValid("{[]}"))   // true
+}
