04bfbd8822f60e26dba4960516aa59010bfa6ae4
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-10 19:18:57 +0530

    commit

--- StacksAndQueues/Interview/histogram.go
+++ StacksAndQueues/Interview/histogram.go
@@ -7,10 +7,10 @@ import "fmt"
 
 func main() {
 	heights := []int{2, 1, 5, 6, 2, 3}
-	fmt.Println("Largest Rectangle area : ", largestRectangeArea(heights)) // 10
+	fmt.Println("Largest Rectangle area : ", largestRectangleArea(heights)) // 10
 }
 
-func largestRectangeArea(heights []int) int {
+func largestRectangleArea(heights []int) int {
 	stack := []int{}
 	maxArea := 0
 	n := len(heights)
@@ -22,7 +22,6 @@ func largestRectangeArea(heights []int) int {
 		}
 
 		for len(stack) > 0 && h < heights[stack[len(stack)-1]] {
-			// pop the height
 			topIndex := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
 
@@ -40,6 +39,37 @@ func largestRectangeArea(heights []int) int {
 	}
 	return maxArea
 }
+
+// func largestRectangeArea(heights []int) int {
+// 	stack := []int{}
+// 	maxArea := 0
+// 	n := len(heights)
+
+// 	for i := 0; i <= n; i++ {
+// 		h := 0
+// 		if i < n {
+// 			h = heights[i]
+// 		}
+
+// 		for len(stack) > 0 && h < heights[stack[len(stack)-1]] {
+// 			// pop the height
+// 			topIndex := stack[len(stack)-1]
+// 			stack = stack[:len(stack)-1]
+
+// 			height := heights[topIndex]
+// 			width := i
+// 			if len(stack) > 0 {
+// 				width = i - stack[len(stack)-1] - 1
+// 			}
+// 			area := height * width
+// 			if area > maxArea {
+// 				maxArea = area
+// 			}
+// 		}
+// 		stack = append(stack, i)
+// 	}
+// 	return maxArea
+// }
 
 // func largestRectangeArea(heights []int) int {
 // 	stack := []int{} // store indices
