4a25289dad2a7f581317a05dce58547652aa8a31
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-11 13:47:04 +0530

    commit

--- StacksAndQueues/Interview/validParenthese.go
+++ StacksAndQueues/Interview/validParenthese.go
@@ -1,27 +1,23 @@
-// Valid parantheses...
-// Google and Meta question
+// // Valid parantheses...
+// // Google and Meta question
 
 package main
 
 import "fmt"
 
 func isValid(s string) bool {
-	stack := []rune{} // use rune slice to store characters
+	stack := []rune{}
 
 	for _, ch := range s {
 		if ch == '(' || ch == '{' || ch == '[' {
-			// push opening bracket
 			stack = append(stack, ch)
 		} else {
-			// for closing brackets, check stack
 			if len(stack) == 0 {
 				return false
 			}
 
-			// pop top of stack
 			top := stack[len(stack)-1]
 			stack = stack[:len(stack)-1]
-
 			if ch == ')' && top != '(' {
 				return false
 			}
@@ -33,14 +29,53 @@ func isValid(s string) bool {
 			}
 		}
 	}
-	// at the end, stack must be empty
 	return len(stack) == 0
 }
 
 func main() {
-	fmt.Println(isValid("()"))     // true
-	fmt.Println(isValid("()[]{}")) // true
-	fmt.Println(isValid("(]"))     // false
-	fmt.Println(isValid("([)]"))   // false
-	fmt.Println(isValid("{[]}"))   // true
+	fmt.Println(isValid("([{}])"))
 }
+
+// package main
+
+// import "fmt"
+
+// func isValid(s string) bool {
+// 	stack := []rune{} // use rune slice to store characters
+
+// 	for _, ch := range s {
+// 		if ch == '(' || ch == '{' || ch == '[' {
+// 			// push opening bracket
+// 			stack = append(stack, ch)
+// 		} else {
+// 			// for closing brackets, check stack
+// 			if len(stack) == 0 {
+// 				return false
+// 			}
+
+// 			// pop top of stack
+// 			top := stack[len(stack)-1]
+// 			stack = stack[:len(stack)-1]
+
+// 			if ch == ')' && top != '(' {
+// 				return false
+// 			}
+// 			if ch == '}' && top != '{' {
+// 				return false
+// 			}
+// 			if ch == ']' && top != '[' {
+// 				return false
+// 			}
+// 		}
+// 	}
+// 	// at the end, stack must be empty
+// 	return len(stack) == 0
+// }
+
+// func main() {
+// 	fmt.Println(isValid("()"))     // true
+// 	fmt.Println(isValid("()[]{}")) // true
+// 	fmt.Println(isValid("(]"))     // false
+// 	fmt.Println(isValid("([)]"))   // false
+// 	fmt.Println(isValid("{[]}"))   // true
+// }
