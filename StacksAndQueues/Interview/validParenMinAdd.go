7e41b78e0fb4aff6c9bb79bb5f2c33095c4d1de8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-11 14:22:44 +0530

    commit

---
+++ StacksAndQueues/Interview/validParenMinAdd.go
@@ -0,0 +1,32 @@
+// Minimum add to make valid parentheses...
+
+package main
+
+import "fmt"
+
+func minAddToMakeValid(s string) int {
+	stack := []rune{}
+
+	for _, ch := range s {
+		if ch == ')' {
+			// if stack top is '(', pop it.
+			if len(stack) > 0 && stack[len(stack)-1] == '(' {
+				stack = stack[:len(stack)-1]
+			} else {
+				// otherwise push ')'
+				stack = append(stack, ch)
+			}
+		} else {
+			// push '('
+			stack = append(stack, ch)
+		}
+	}
+	return len(stack)
+}
+
+func main() {
+	fmt.Println(minAddToMakeValid("())"))    // 1
+	fmt.Println(minAddToMakeValid("((("))    // 3
+	fmt.Println(minAddToMakeValid("()"))     // 0
+	fmt.Println(minAddToMakeValid("()))((")) // 4
+}
