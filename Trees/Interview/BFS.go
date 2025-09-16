4098e217690bc61f0deb11facf6759adf99cd59f
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-16 23:01:24 +0530

    commit

--- Trees/Interview/BFS.go
+++ Trees/Interview/BFS.go
@@ -26,7 +26,6 @@ func BFS(root *Node) {
 		if current.left != nil {
 			queue = append(queue, current.left)
 		}
-
 		if current.right != nil {
 			queue = append(queue, current.right)
 		}
@@ -38,17 +37,18 @@ func Display(node *Node, level int) {
 		return
 	}
 
-	Display(node.left, level+1)
+	Display(node.right, level+1)
 
 	if level != 0 {
 		for i := 0; i < level-1; i++ {
 			fmt.Print("|\t")
 		}
-		fmt.Println("|----->", node.data)
+		fmt.Println("|---->", node.data)
 	} else {
 		fmt.Println(node.data)
 	}
-	Display(node.right, level+1)
+
+	Display(node.left, level+1)
 }
 
 func main() {
@@ -60,13 +60,81 @@ func main() {
 	root.right.left = &Node{data: 6}
 	root.right.right = &Node{data: 7}
 
-	fmt.Println("BFS traversal")
+	fmt.Println("BFS traversal : ")
 	BFS(root)
 	fmt.Println()
 
-	fmt.Println("\ndiaplay tree : ")
+	fmt.Println("\nDisplay tree")
 	Display(root, 0)
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// func BFS(root *Node) {
+// 	if root == nil {
+// 		return
+// 	}
+
+// 	queue := []*Node{root}
+
+// 	for len(queue) > 0 {
+// 		current := queue[0]
+// 		queue = queue[1:]
+
+// 		fmt.Print(current.data, " ")
+
+// 		if current.left != nil {
+// 			queue = append(queue, current.left)
+// 		}
+
+// 		if current.right != nil {
+// 			queue = append(queue, current.right)
+// 		}
+// 	}
+// }
+
+// func Display(node *Node, level int) {
+// 	if node == nil {
+// 		return
+// 	}
+
+// 	Display(node.left, level+1)
+
+// 	if level != 0 {
+// 		for i := 0; i < level-1; i++ {
+// 			fmt.Print("|\t")
+// 		}
+// 		fmt.Println("|----->", node.data)
+// 	} else {
+// 		fmt.Println(node.data)
+// 	}
+// 	Display(node.right, level+1)
+// }
+
+// func main() {
+// 	root := &Node{data: 1}
+// 	root.left = &Node{data: 2}
+// 	root.right = &Node{data: 3}
+// 	root.left.left = &Node{data: 4}
+// 	root.left.right = &Node{data: 5}
+// 	root.right.left = &Node{data: 6}
+// 	root.right.right = &Node{data: 7}
+
+// 	fmt.Println("BFS traversal")
+// 	BFS(root)
+// 	fmt.Println()
+
+// 	fmt.Println("\ndiaplay tree : ")
+// 	Display(root, 0)
+// }
 
 // package main
 
