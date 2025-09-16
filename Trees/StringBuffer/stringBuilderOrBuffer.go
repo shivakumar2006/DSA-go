9b16717160acb833e424f80e6bf29c620ea55572
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-16 18:08:26 +0530

    commit

--- Trees/StringBuffer/stringBuilderOrBuffer.go
+++ Trees/StringBuffer/stringBuilderOrBuffer.go
@@ -1,43 +1,61 @@
-// strings builder, Strings.Builder
+// now mixing byte and string -> usinf bytes.Buffer
 
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"strings"
 )
 
 func main() {
-	// JSON like output
-	var sb strings.Builder
-	sb.WriteString("{")
-	sb.WriteString(`"name":"Shiva",`)
-	sb.WriteString(`"age":"19"`)
-	sb.WriteString("}")
-	fmt.Println(sb.String())
+	var b bytes.Buffer
 
-	// // Pre-Allocating buffer size
-	// var sb strings.Builder
-	// sb.Grow(100) // reserve 100 Bytes
-
-	// sb.WriteString("Hello ")
-	// sb.WriteString("World!")
-	// fmt.Println(sb.String())
-
-	// var sb strings.Builder
-
-	// for i := 0; i <= 5; i++ {
-	// 	sb.WriteString(fmt.Sprintf("Item %d\n", i))
-	// }
-
-	// result := sb.String()
-	// fmt.Println(result)
-
-	// var builder strings.Builder
-	// builder.WriteString("Hello")
-	// builder.WriteString(" Kumar")
-	// fmt.Println(builder.String())
+	b.Write([]byte("Hello "))
+	b.WriteByte('G')
+	b.WriteByte('o')
+	fmt.Println(b.String())
 }
+
+// // strings builder, Strings.Builder
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main() {
+// 	// JSON like output
+// 	var sb strings.Builder
+// 	sb.WriteString("{")
+// 	sb.WriteString(`"name":"Shiva",`)
+// 	sb.WriteString(`"age":"19"`)
+// 	sb.WriteString("}")
+// 	fmt.Println(sb.String())
+
+// 	// // Pre-Allocating buffer size
+// 	// var sb strings.Builder
+// 	// sb.Grow(100) // reserve 100 Bytes
+
+// 	// sb.WriteString("Hello ")
+// 	// sb.WriteString("World!")
+// 	// fmt.Println(sb.String())
+
+// 	// var sb strings.Builder
+
+// 	// for i := 0; i <= 5; i++ {
+// 	// 	sb.WriteString(fmt.Sprintf("Item %d\n", i))
+// 	// }
+
+// 	// result := sb.String()
+// 	// fmt.Println(result)
+
+// 	// var builder strings.Builder
+// 	// builder.WriteString("Hello")
+// 	// builder.WriteString(" Kumar")
+// 	// fmt.Println(builder.String())
+// }
 
 // // string buffer bytes.Buffer
 
