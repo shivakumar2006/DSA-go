44565312e017e27432f9a6fb011f88c09f640de6
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-16 17:53:15 +0530

    commit

--- Trees/StringBuffer/stringBuilderOrBuffer.go
+++ Trees/StringBuffer/stringBuilderOrBuffer.go
@@ -1,15 +1,31 @@
-// string buffer bytes.Buffer
+// strings builder, Strings.Builder
 
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	var buf bytes.Buffer
-	buf.WriteString("Hello")
-	buf.WriteString(" World")
-	fmt.Println(buf.String()) // "Hello World"
+	var builder strings.Builder
+	builder.WriteString("Hello")
+	builder.WriteString(" Kumar")
+	fmt.Println(builder.String())
 }
+
+// // string buffer bytes.Buffer
+
+// package main
+
+// import (
+// 	"bytes"
+// 	"fmt"
+// )
+
+// func main() {
+// 	var buf bytes.Buffer
+// 	buf.WriteString("Hello")
+// 	buf.WriteString(" World")
+// 	fmt.Println(buf.String()) // "Hello World"
+// }
