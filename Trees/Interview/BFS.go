6e2e68f9b3bac097421e5869d1050110283cfe0d
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-16 22:21:16 +0530

    commit

--- Trees/Interview/BFS.go
+++ Trees/Interview/BFS.go
@@ -1,4 +1,4 @@
-// Breadth First Search (BFS)
+// // Breadth First Search (BFS)
 
 package main
 
@@ -10,41 +10,35 @@ type Node struct {
 	right *Node
 }
 
-// BFS traversal
 func BFS(root *Node) {
 	if root == nil {
 		return
 	}
 
-	// create queue slice to store nodes level by level
 	queue := []*Node{root}
 
 	for len(queue) > 0 {
-		// Dequeue the first element
 		current := queue[0]
 		queue = queue[1:]
 
-		// process the current node
 		fmt.Print(current.data, " ")
 
-		// Enqueue left child if exist
 		if current.left != nil {
 			queue = append(queue, current.left)
 		}
 
-		// Enqueue right child if exists
 		if current.right != nil {
 			queue = append(queue, current.right)
 		}
 	}
 }
 
-func prettyDisplay(node *Node, level int) {
+func Display(node *Node, level int) {
 	if node == nil {
 		return
 	}
 
-	prettyDisplay(node.right, level+1)
+	Display(node.left, level+1)
 
 	if level != 0 {
 		for i := 0; i < level-1; i++ {
@@ -54,7 +48,7 @@ func prettyDisplay(node *Node, level int) {
 	} else {
 		fmt.Println(node.data)
 	}
-	prettyDisplay(node.left, level+1)
+	Display(node.right, level+1)
 }
 
 func main() {
@@ -66,10 +60,84 @@ func main() {
 	root.right.left = &Node{data: 6}
 	root.right.right = &Node{data: 7}
 
-	fmt.Print("BFS Traversal : ")
+	fmt.Println("BFS traversal")
 	BFS(root)
 	fmt.Println()
 
-	fmt.Println("\nTree Structure:")
-	prettyDisplay(root, 0)
+	fmt.Println("\ndiaplay tree : ")
+	Display(root, 0)
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// // BFS traversal
+// func BFS(root *Node) {
+// 	if root == nil {
+// 		return
+// 	}
+
+// 	// create queue slice to store nodes level by level
+// 	queue := []*Node{root}
+
+// 	for len(queue) > 0 {
+// 		// Dequeue the first element
+// 		current := queue[0]
+// 		queue = queue[1:]
+
+// 		// process the current node
+// 		fmt.Print(current.data, " ")
+
+// 		// Enqueue left child if exist
+// 		if current.left != nil {
+// 			queue = append(queue, current.left)
+// 		}
+
+// 		// Enqueue right child if exists
+// 		if current.right != nil {
+// 			queue = append(queue, current.right)
+// 		}
+// 	}
+// }
+
+// func prettyDisplay(node *Node, level int) {
+// 	if node == nil {
+// 		return
+// 	}
+
+// 	prettyDisplay(node.right, level+1)
+
+// 	if level != 0 {
+// 		for i := 0; i < level-1; i++ {
+// 			fmt.Print("|\t")
+// 		}
+// 		fmt.Println("|----->", node.data)
+// 	} else {
+// 		fmt.Println(node.data)
+// 	}
+// 	prettyDisplay(node.left, level+1)
+// }
+
+// func main() {
+// 	root := &Node{data: 1}
+// 	root.left = &Node{data: 2}
+// 	root.right = &Node{data: 3}
+// 	root.left.left = &Node{data: 4}
+// 	root.left.right = &Node{data: 5}
+// 	root.right.left = &Node{data: 6}
+// 	root.right.right = &Node{data: 7}
+
+// 	fmt.Print("BFS Traversal : ")
+// 	BFS(root)
+// 	fmt.Println()
+
+// 	fmt.Println("\nTree Structure:")
+// 	prettyDisplay(root, 0)
+// }
