6e5fb85e5178e11ef9ad1ae423be448a3dcce35e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-18 13:03:28 +0530

    commit

--- Trees/Interview/BFS.go
+++ Trees/Interview/BFS.go
@@ -43,7 +43,7 @@ func Display(node *Node, level int) {
 		for i := 0; i < level-1; i++ {
 			fmt.Print("|\t")
 		}
-		fmt.Println("|----->", node.data)
+		fmt.Println("|---->", node.data)
 	} else {
 		fmt.Println(node.data)
 	}
@@ -59,13 +59,80 @@ func main() {
 	root.right.left = &Node{data: 6}
 	root.right.right = &Node{data: 7}
 
-	fmt.Println("BFS Traversal : ")
+	fmt.Println("BFS traversal : ")
 	BFS(root)
 	fmt.Println()
 
 	fmt.Println("\nDisplay tree : ")
 	Display(root, 0)
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// func BFS(root *Node) {
+// 	if root == nil {
+// 		return
+// 	}
+
+// 	queue := []*Node{root}
+
+// 	for len(queue) > 0 {
+// 		current := queue[0]
+// 		queue = queue[1:]
+
+// 		fmt.Print(current.data, " ")
+
+// 		if current.left != nil {
+// 			queue = append(queue, current.left)
+// 		}
+// 		if current.right != nil {
+// 			queue = append(queue, current.right)
+// 		}
+// 	}
+// }
+
+// func Display(node *Node, level int) {
+// 	if node == nil {
+// 		return
+// 	}
+
+// 	Display(node.right, level+1)
+
+// 	if level != 0 {
+// 		for i := 0; i < level-1; i++ {
+// 			fmt.Print("|\t")
+// 		}
+// 		fmt.Println("|----->", node.data)
+// 	} else {
+// 		fmt.Println(node.data)
+// 	}
+// 	Display(node.left, level+1)
+// }
+
+// func main() {
+// 	root := &Node{data: 1}
+// 	root.left = &Node{data: 2}
+// 	root.right = &Node{data: 3}
+// 	root.left.left = &Node{data: 4}
+// 	root.left.right = &Node{data: 5}
+// 	root.right.left = &Node{data: 6}
+// 	root.right.right = &Node{data: 7}
+
+// 	fmt.Println("BFS Traversal : ")
+// 	BFS(root)
+// 	fmt.Println()
+
+// 	fmt.Println("\nDisplay tree : ")
+// 	Display(root, 0)
+// }
 
 // package main
 
