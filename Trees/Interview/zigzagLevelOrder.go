04513269ab35ffc706dcf8a822af3571c6a1b3b7
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-18 12:19:04 +0530

    commit

---
+++ Trees/Interview/zigzagLevelOrder.go
@@ -0,0 +1,11 @@
+// Binary tree Zigzag level order traversal
+// Google, Facebook and Amazon question...
+package main
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+func ZigzagLevelOrder(root *Node)
