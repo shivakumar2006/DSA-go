648edad6568982d0e8332f128ffaeb89dadac2ae
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-19 14:15:31 +0530

    commit

---
+++ Trees/Interview/cousins.go
@@ -0,0 +1,75 @@
+// Find cousins in Binary tree
+
+package main
+
+import "fmt"
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+// main function check if x and y are cousins
+func isCousins(root *Node, x, y int) bool {
+	xx := findNode(root, x)
+	yy := findNode(root, y)
+
+	if xx == nil && yy == nil {
+		return false
+	}
+
+	return (level(root, xx, 0) == level(root, yy, 0) && !isSiblings(root, xx, yy))
+}
+
+func findNode(node *Node, x int) *Node {
+	if node == nil {
+		return nil
+	}
+
+	if node.data == x {
+		return node
+	}
+	if n := findNode(node.left, x); n != nil {
+		return n
+	}
+	return findNode(node.right, x)
+}
+
+func isSiblings(node *Node, x, y *Node) bool {
+	if node == nil {
+		return false
+	}
+
+	if (node.left == x && node.right == y) || (node.left == y && node.right == x) {
+		return true
+	}
+	return isSiblings(node.left, x, y) || isSiblings(node.right, x, y)
+}
+
+func level(node *Node, x *Node, lev int) int {
+	if node == nil {
+		return 0 // not found
+	}
+
+	if node == x {
+		return lev
+	}
+
+	l := level(node.left, x, lev+1)
+	if l != 0 {
+		return l
+	}
+	return level(node.right, x, lev+1)
+}
+
+func main() {
+	root := &Node{data: 1}
+	root.left = &Node{data: 2}
+	root.right = &Node{data: 3}
+	root.left.left = &Node{data: 4}
+	root.right.right = &Node{data: 5}
+
+	fmt.Println(isCousins(root, 4, 5)) // true (4 and 5 are cousins)
+	fmt.Println(isCousins(root, 4, 3)) // false
+}
