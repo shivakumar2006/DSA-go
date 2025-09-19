a9186cc01ed156a6524012ab2aa56d853c8d5068
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-19 16:07:03 +0530

    commit

--- Trees/Interview/symmetric.go
+++ Trees/Interview/symmetric.go
@@ -1,4 +1,4 @@
-// Symmetric tree
+// // Symmetric tree
 
 package main
 
@@ -25,12 +25,15 @@ func Mirror(root *Node) bool {
 		if left == nil && right == nil {
 			continue
 		}
+
 		if left == nil || right == nil {
 			return false
 		}
+
 		if left.data != right.data {
 			return false
 		}
+
 		queue = append(queue, left.left)
 		queue = append(queue, right.right)
 		queue = append(queue, left.right)
@@ -53,6 +56,62 @@ func main() {
 			right: &Node{data: 3},
 		},
 	}
-
 	fmt.Println(Mirror(root))
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// func Mirror(root *Node) bool {
+// 	if root == nil {
+// 		return true
+// 	}
+
+// 	queue := []*Node{root.left, root.right}
+
+// 	for len(queue) > 0 {
+// 		left := queue[0]
+// 		right := queue[1]
+// 		queue = queue[2:]
+
+// 		if left == nil && right == nil {
+// 			continue
+// 		}
+// 		if left == nil || right == nil {
+// 			return false
+// 		}
+// 		if left.data != right.data {
+// 			return false
+// 		}
+// 		queue = append(queue, left.left)
+// 		queue = append(queue, right.right)
+// 		queue = append(queue, left.right)
+// 		queue = append(queue, right.left)
+// 	}
+// 	return true
+// }
+
+// func main() {
+// 	root := &Node{
+// 		data: 1,
+// 		left: &Node{
+// 			data:  2,
+// 			left:  &Node{data: 3},
+// 			right: &Node{data: 4},
+// 		},
+// 		right: &Node{
+// 			data:  2,
+// 			left:  &Node{data: 4},
+// 			right: &Node{data: 3},
+// 		},
+// 	}
+
+// 	fmt.Println(Mirror(root))
+// }
