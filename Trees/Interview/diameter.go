4e3b2b088aeb913b4ac488b494660cc8bd724438
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-19 22:48:02 +0530

    commit

--- Trees/Interview/diameter.go
+++ Trees/Interview/diameter.go
@@ -3,7 +3,7 @@
 
 package main
 
-import "math"
+import "fmt"
 
 type Node struct {
 	data  int
@@ -18,16 +18,33 @@ func diameterOfBinaryTree(root *Node) int {
 	return diameter
 }
 
-func height(root *Node) {
+func height(root *Node) int {
 	if root == nil {
-		return
+		return 0
 	}
 
 	leftHeight := height(root.left)
 	rightHeight := height(root.right)
 
-	dia := leftHeight - rightHeight + 1
-	diameter = math.Max(diameter, dia)
+	dia := leftHeight + rightHeight
 
-	return math.Max(leftHeight, rightHeight) + 1
+	if dia > diameter {
+		diameter = dia
+	}
+
+	// return the height as in your last line but with int max
+	if leftHeight > rightHeight {
+		return leftHeight + 1
+	}
+	return rightHeight + 1
+}
+
+func main() {
+	root := &Node{data: 1}
+	root.left = &Node{data: 2}
+	root.right = &Node{data: 3}
+	root.left.left = &Node{data: 4}
+	root.left.right = &Node{data: 5}
+
+	fmt.Println("Diameter:", diameterOfBinaryTree(root)) // should print 3
 }
