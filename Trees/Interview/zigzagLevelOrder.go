e50676cae7195f6147a61f1f45ea56e298372250
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-19 12:20:47 +0530

    commit

--- Trees/Interview/zigzagLevelOrder.go
+++ Trees/Interview/zigzagLevelOrder.go
@@ -78,6 +78,52 @@ func main() {
 
 // 	queue := []*Node{root}
 // 	var result [][]int
+// 	leftToRight := true
+
+// 	for len(queue) > 0 {
+// 		levelSize := len(queue)
+// 		level := make([]int, levelSize)
+
+// 		for i := 0; i < levelSize; i++ {
+// 			current := queue[0]
+// 			queue = queue[1:]
+
+// 			if leftToRight {
+// 				level[i] = current.data
+// 			} else {
+// 				level[levelSize-1-i] = current.data
+// 			}
+
+// 			if current.left != nil {
+// 				queue = append(queue, current.left)
+// 			}
+// 			if current.right != nil {
+// 				queue = append(queue, current.right)
+// 			}
+// 		}
+// 		result = append(result, level)
+// 		leftToRight = !leftToRight
+// 	}
+// 	return result
+// }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// func ZigzagLevelOrder(root *Node) [][]int {
+// 	if root == nil {
+// 		return nil
+// 	}
+
+// 	queue := []*Node{root}
+// 	var result [][]int
 // 	leftToRight := true // toggle direction
 
 // 	for len(queue) > 0 {
