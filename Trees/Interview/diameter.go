0b0fc58586c4e41372eaf5664bcd79ae81f77e40
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-19 22:41:49 +0530

    commit

--- Trees/Interview/diameter.go
+++ Trees/Interview/diameter.go
@@ -1,3 +1,33 @@
 // Diameter in BinarySearch
-// Google, Facebook and Amazon question 
+// Google, Facebook and Amazon question
 
+package main
+
+import "math"
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+var diameter int = 0
+
+func diameterOfBinaryTree(root *Node) int {
+	height(root)
+	return diameter
+}
+
+func height(root *Node) {
+	if root == nil {
+		return
+	}
+
+	leftHeight := height(root.left)
+	rightHeight := height(root.right)
+
+	dia := leftHeight - rightHeight + 1
+	diameter = math.Max(diameter, dia)
+
+	return math.Max(leftHeight, rightHeight) + 1
+}
