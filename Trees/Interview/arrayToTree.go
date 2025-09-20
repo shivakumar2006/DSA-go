69eb69dc4918df41a8ed9c74aca64bc5109200f3
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-20 22:10:25 +0530

    commit

---
+++ Trees/Interview/arrayToTree.go
@@ -0,0 +1,136 @@
+// convert sorted array to binary search tree
+
+package main
+
+import "fmt"
+
+type Node struct {
+	data   int
+	left   *Node
+	right  *Node
+	height int
+}
+
+type BinarySearchTree struct {
+	root *Node
+}
+
+func NewBST() *BinarySearchTree {
+	return &BinarySearchTree{root: nil}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func (bst *BinarySearchTree) height(node *Node) int {
+	if node == nil {
+		return -1
+	}
+
+	return node.height
+}
+
+func (bst *BinarySearchTree) Insert(value int) {
+	bst.root = bst.insert(bst.root, value)
+}
+
+func (bst *BinarySearchTree) insert(node *Node, value int) *Node {
+	if node == nil {
+		return &Node{data: value}
+	}
+
+	if value < node.data {
+		node.left = bst.insert(node.left, value)
+	} else if value > node.data {
+		node.right = bst.insert(node.right, value)
+	}
+	node.height = max(bst.height(node.left), bst.height(node.right)) + 1
+	return node
+}
+
+func (bst *BinarySearchTree) Balanced() bool {
+	return bst.balanced(bst.root)
+}
+
+func (bst *BinarySearchTree) balanced(node *Node) bool {
+	if node == nil {
+		return true
+	}
+
+	leftHeight := bst.height(node.left)
+	rightHeight := bst.height(node.right)
+
+	return abs(leftHeight-rightHeight) <= 1 && bst.balanced(node.left) && bst.balanced(node.right)
+}
+
+func (bst *BinarySearchTree) PopulatedSorted(nums []int) {
+	bst.populatedSorted(nums, 0, len(nums))
+}
+
+func (bst *BinarySearchTree) populatedSorted(nums []int, start, end int) {
+	if start >= end {
+		return
+	}
+
+	mid := (start + end) / 2
+	bst.Insert(nums[mid])
+	bst.populatedSorted(nums, start, mid)
+	bst.populatedSorted(nums, mid+1, end)
+}
+
+func (bst *BinarySearchTree) Display() {
+	bst.display(bst.root, "Root node : ")
+}
+
+func (bst *BinarySearchTree) display(node *Node, details string) {
+	if node == nil {
+		return
+	}
+	fmt.Println(details, node.data)
+	bst.display(node.left, fmt.Sprintf("the left child of %d : ", node.data))
+	bst.display(node.right, fmt.Sprintf("the right chils of the %d: ", node.data))
+}
+
+func (bst *BinarySearchTree) DisplayTree(node *Node, level int) {
+	if node == nil {
+		return
+	}
+
+	bst.DisplayTree(node.right, level+1)
+
+	if level != 0 {
+		for i := 0; i < level-1; i++ {
+			fmt.Print("|\t")
+		}
+		fmt.Println("|----->", node.data)
+	} else {
+		fmt.Println(node.data)
+	}
+	bst.DisplayTree(node.left, level+1)
+}
+
+func main() {
+	tree := NewBST()
+	value := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	tree.PopulatedSorted(value)
+
+	tree.Display()
+
+	fmt.Println("Balanced : ", tree.Balanced())
+	fmt.Println()
+
+	fmt.Println("binary Search tree : ")
+	tree.DisplayTree(tree.root, 0)
+}
