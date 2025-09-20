a5b27f18256465a4c0b71b8ac27dc2f4bc30db7a
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-20 21:12:18 +0530

    commit

--- Trees/Interview/invertTree.go
+++ Trees/Interview/invertTree.go
@@ -1,4 +1,4 @@
-// Invert Binary Tree
+// // Invert Binary Tree
 
 package main
 
@@ -15,69 +15,31 @@ func InvertTree(root *Node) *Node {
 		return nil
 	}
 
-	Left := InvertTree(root.left)
-	Right := InvertTree(root.right)
+	left := InvertTree(root.left)
+	right := InvertTree(root.right)
 
-	root.left = Right
-	root.right = Left
+	root.left = right
+	root.right = left
 
 	return root
 }
 
-func Display(root *Node) {
-	if root == nil {
-		return
-	}
-	Display(root.left)
-
-	fmt.Print(root.data, "")
-
-	Display(root.right)
-}
-
-func DisplayLevelOrderTree(root *Node) {
+func Display(root *Node, level int) {
 	if root == nil {
 		return
 	}
 
-	queue := []*Node{root}
-
-	for len(queue) > 0 {
-		levelSize := len(queue)
-		for i := 0; i < levelSize; i++ {
-			current := queue[0]
-			queue = queue[1:]
-
-			fmt.Println(current.data, " ")
-
-			if current.left != nil {
-				queue = append(queue, current.left)
-			}
-			if current.right != nil {
-				queue = append(queue, current.right)
-			}
-		}
-		fmt.Println()
-	}
-}
-
-func DisplayAll(root *Node, level int) {
-	if root == nil {
-		return
-	}
-
-	DisplayAll(root.right, level+1)
+	Display(root.right, level+1)
 
 	if level != 0 {
 		for i := 0; i < level-1; i++ {
 			fmt.Print("|\t")
 		}
-		fmt.Println("|---->", root.data)
+		fmt.Println("|----->", root.data)
 	} else {
 		fmt.Println(root.data)
 	}
-
-	DisplayAll(root.left, level+1)
+	Display(root.left, level+1)
 }
 
 func main() {
@@ -89,21 +51,121 @@ func main() {
 	root.right.left = &Node{data: 6}
 	root.right.right = &Node{data: 7}
 
-	fmt.Println("Original tree: ")
-	Display(root)
+	fmt.Println("Display Inverted Tree Before : ")
+	Display(root, 0)
 	fmt.Println()
 
 	root = InvertTree(root)
 
-	fmt.Println("Inverted tree : ")
-	Display(root)
-	fmt.Println()
-
-	fmt.Print("Inverted tree : (level-order)")
-	DisplayLevelOrderTree(root)
-	fmt.Println()
-
-	fmt.Println("Display Inverted Tree: ")
-	DisplayAll(root, 0)
+	fmt.Println("Display Inverted Tree After : ")
+	Display(root, 0)
 	fmt.Println()
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// func InvertTree(root *Node) *Node {
+// 	if root == nil {
+// 		return nil
+// 	}
+
+// 	Left := InvertTree(root.left)
+// 	Right := InvertTree(root.right)
+
+// 	root.left = Right
+// 	root.right = Left
+
+// 	return root
+// }
+
+// func Display(root *Node) {
+// 	if root == nil {
+// 		return
+// 	}
+// 	Display(root.left)
+
+// 	fmt.Print(root.data, "")
+
+// 	Display(root.right)
+// }
+
+// func DisplayLevelOrderTree(root *Node) {
+// 	if root == nil {
+// 		return
+// 	}
+
+// 	queue := []*Node{root}
+
+// 	for len(queue) > 0 {
+// 		levelSize := len(queue)
+// 		for i := 0; i < levelSize; i++ {
+// 			current := queue[0]
+// 			queue = queue[1:]
+
+// 			fmt.Println(current.data, " ")
+
+// 			if current.left != nil {
+// 				queue = append(queue, current.left)
+// 			}
+// 			if current.right != nil {
+// 				queue = append(queue, current.right)
+// 			}
+// 		}
+// 		fmt.Println()
+// 	}
+// }
+
+// func DisplayAll(root *Node, level int) {
+// 	if root == nil {
+// 		return
+// 	}
+
+// 	DisplayAll(root.right, level+1)
+
+// 	if level != 0 {
+// 		for i := 0; i < level-1; i++ {
+// 			fmt.Print("|\t")
+// 		}
+// 		fmt.Println("|---->", root.data)
+// 	} else {
+// 		fmt.Println(root.data)
+// 	}
+
+// 	DisplayAll(root.left, level+1)
+// }
+
+// func main() {
+// 	root := &Node{data: 1}
+// 	root.left = &Node{data: 2}
+// 	root.right = &Node{data: 3}
+// 	root.left.left = &Node{data: 4}
+// 	root.left.right = &Node{data: 5}
+// 	root.right.left = &Node{data: 6}
+// 	root.right.right = &Node{data: 7}
+
+// 	fmt.Println("Original tree: ")
+// 	Display(root)
+// 	fmt.Println()
+
+// 	root = InvertTree(root)
+
+// 	fmt.Println("Inverted tree : ")
+// 	Display(root)
+// 	fmt.Println()
+
+// 	fmt.Print("Inverted tree : (level-order)")
+// 	DisplayLevelOrderTree(root)
+// 	fmt.Println()
+
+// 	fmt.Println("Display Inverted Tree: ")
+// 	DisplayAll(root, 0)
+// 	fmt.Println()
+// }
