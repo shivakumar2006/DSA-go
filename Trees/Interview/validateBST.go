fb498d1971f9e76597e2cac26cfa334356f4feba
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-20 23:10:50 +0530

    commit

--- Trees/Interview/validateBST.go
+++ Trees/Interview/validateBST.go
@@ -2,8 +2,41 @@
 
 package main
 
+import "fmt"
+
 type Node struct {
 	data  int
 	left  *Node
 	right *Node
+}
+
+func isValidBST(root *Node) bool {
+	return helper(root, nil, nil)
+}
+
+func helper(root *Node, low *int, high *int) bool {
+	if root == nil {
+		return true
+	}
+
+	if low != nil && root.data <= *low {
+		return false
+	}
+
+	if high != nil && root.data >= *high {
+		return false
+	}
+
+	leftTree := helper(root.left, low, &root.data)
+	rightTree := helper(root.right, &root.data, high)
+
+	return leftTree && rightTree
+}
+
+func main() {
+	root := &Node{data: 2}
+	root.left = &Node{data: 1}
+	root.right = &Node{data: 3}
+
+	fmt.Println(isValidBST(root))
 }
