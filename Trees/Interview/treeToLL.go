07f9f4f823f9ebc1003fcd7b49e4714f2d435cf5
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-20 22:37:36 +0530

    commit

--- Trees/Interview/treeToLL.go
+++ Trees/Interview/treeToLL.go
@@ -1,2 +1,54 @@
-// Flatten Binary Tree to linked list   
+// Flatten Binary Tree to linked list
+// Facebook question
 
+package main
+
+import "fmt"
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+func flatten(root *Node) {
+	if root == nil {
+		return
+	}
+
+	current := root
+
+	for current != nil {
+		if current.left != nil {
+			temp := current.left
+			if temp.right != nil {
+				temp = temp.right
+			}
+
+			temp.right = current.right
+			current.right = current.left
+			current.left = nil
+		}
+		current = current.right
+	}
+}
+
+func DisplayFlatten(root *Node) {
+	for root != nil {
+		fmt.Print(root.data, " -> ")
+		root = root.right
+	}
+	fmt.Println("END")
+}
+
+func main() {
+	root := &Node{data: 1}
+	root.left = &Node{data: 2}
+	root.right = &Node{data: 5}
+	root.left.left = &Node{data: 3}
+	root.left.right = &Node{data: 4}
+	root.right.right = &Node{data: 6}
+
+	flatten(root)
+	DisplayFlatten(root)
+}
