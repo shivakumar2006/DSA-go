04c64a1d8600b7cf47e7f1217097aeeac9bb1fd2
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-21 13:10:59 +0530

    commit

---
+++ Trees/Interview/smallestElement.go
@@ -0,0 +1,48 @@
+// Kth smallest element in BST
+// Facebook Amazon and Google question
+
+package main
+
+import "fmt"
+
+type Node struct {
+	data  int
+	left  *Node
+	right *Node
+}
+
+var count int = 0
+
+func smallest(root *Node, k int) int {
+	return helper(root, k).data
+}
+
+func helper(root *Node, k int) *Node {
+	if root == nil {
+		return nil
+	}
+
+	left := helper(root.left, k)
+
+	if left != nil {
+		return left
+	}
+
+	count++
+
+	if count == k {
+		return root
+	}
+
+	return helper(root.right, k)
+}
+
+func main() {
+	root := &Node{data: 3}
+	root.left = &Node{data: 1}
+	root.right = &Node{data: 4}
+	root.left.right = &Node{data: 2}
+
+	k := 3
+	fmt.Println("smallest kth element is : ", smallest(root, k))
+}
