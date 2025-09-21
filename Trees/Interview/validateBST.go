c6f4fa9e017f922d6958a2285cc622fd18f2ce8c
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-21 12:07:38 +0530

    commit

--- Trees/Interview/validateBST.go
+++ Trees/Interview/validateBST.go
@@ -1,4 +1,4 @@
-// Validate Binary search tree
+// // Validate Binary search tree
 
 package main
 
@@ -10,7 +10,7 @@ type Node struct {
 	right *Node
 }
 
-func isValidBST(root *Node) bool {
+func isValidate(root *Node) bool {
 	return helper(root, nil, nil)
 }
 
@@ -38,5 +38,46 @@ func main() {
 	root.left = &Node{data: 1}
 	root.right = &Node{data: 3}
 
-	fmt.Println(isValidBST(root))
+	fmt.Println(isValidate(root))
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// func isValidBST(root *Node) bool {
+// 	return helper(root, nil, nil)
+// }
+
+// func helper(root *Node, low *int, high *int) bool {
+// 	if root == nil {
+// 		return true
+// 	}
+
+// 	if low != nil && root.data <= *low {
+// 		return false
+// 	}
+
+// 	if high != nil && root.data >= *high {
+// 		return false
+// 	}
+
+// 	leftTree := helper(root.left, low, &root.data)
+// 	rightTree := helper(root.right, &root.data, high)
+
+// 	return leftTree && rightTree
+// }
+
+// func main() {
+// 	root := &Node{data: 2}
+// 	root.left = &Node{data: 1}
+// 	root.right = &Node{data: 3}
+
+// 	fmt.Println(isValidBST(root))
+// }
