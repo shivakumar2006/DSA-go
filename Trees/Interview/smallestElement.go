a6b61049d035ebbff95fe6b6efbe629aefcf1860
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-21 15:21:58 +0530

    commit

--- Trees/Interview/smallestElement.go
+++ Trees/Interview/smallestElement.go
@@ -1,47 +1,6 @@
 // Kth smallest element in BST
 // Facebook Amazon and Google question
 
-package main
-
-import "fmt"
-
-type Node struct {
-	data  int
-	left  *Node
-	right *Node
-}
-
-var count int
-
-func smallest(root *Node, k int) int {
-	count = 0
-	node := helper(root, k)
-	if node == nil {
-		return -1
-	}
-	return node.data
-}
-
-func helper(root *Node, k int) *Node {
-	if root == nil {
-		return nil
-	}
-
-	left := helper(root.left, k)
-
-	if left != nil {
-		return left
-	}
-
-	count++
-
-	if count == k {
-		return root
-	}
-
-	return helper(root.right, k)
-}
-
 func main() {
 	root := &Node{data: 2}
 	root.left = &Node{data: 1}
@@ -51,3 +10,85 @@ func main() {
 	k := 3
 	fmt.Println("smallest kth element is : ", smallest(root, k))
 }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// var count int
+
+// func smallest(root *Node, k int) int {
+// 	count = 0
+// 	node := helper(root, k)
+// 	if node == nil {
+// 		return -1
+// 	}
+// 	return node.data
+// }
+
+// func helper(root *Node, k int) *Node {
+// 	if root == nil {
+// 		return nil
+// 	}
+
+// 	left := helper(root.left, k)
+
+// 	if left != nil {
+// 		return left
+// 	}
+
+// 	count++
+
+// 	if count == k {
+// 		return root
+// 	}
+
+// 	return helper(root.right, k)
+// }
+
+// package main
+
+// import "fmt"
+
+// type Node struct {
+// 	data  int
+// 	left  *Node
+// 	right *Node
+// }
+
+// var count int
+
+// func smallest(root *Node, k int) int {
+// 	count = 0
+// 	node := helper(root, k)
+// 	if node == nil {
+// 		return -1
+// 	}
+// 	return node.data
+// }
+
+// func helper(root *Node, k int) *Node {
+// 	if root == nil {
+// 		return nil
+// 	}
+
+// 	left := helper(root.left, k)
+
+// 	if left != nil {
+// 		return left
+// 	}
+
+// 	count++
+
+// 	if count == k {
+// 		return root
+// 	}
+
+// 	return helper(root.right, k)
+// }
