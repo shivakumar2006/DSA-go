618cbda350e36e2796b5289b6e44611b8a9d15de
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-23 21:50:20 +0530

    commit

---
+++ Heap/heap.go
@@ -0,0 +1,117 @@
+// Heap
+
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"golang.org/x/exp/constraints"
+)
+
+// heap is generic min-heap
+type Heap[T constraints.Ordered] struct {
+	list []T
+}
+
+// creates an empty heap
+func NewHeap[T constraints.Ordered]() *Heap[T] {
+	return &Heap[T]{list: []T{}}
+}
+
+func (h *Heap[T]) swap(i, j int) {
+	h.list[i], h.list[j] = h.list[j], h.list[i]
+}
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+func leftChild(i int) int {
+	return (2 * i) + 1
+}
+
+func rightChild(i int) int {
+	return (2 * i) + 2
+}
+
+// insert pushes a value into a heap
+func (h *Heap[T]) Insert(value T) {
+	h.list = append(h.list, value)
+	h.upHeap(len(h.list) - 1)
+}
+
+func (h *Heap[T]) upHeap(i int) {
+	if i == 0 {
+		return
+	}
+	p := parent(i)
+	if h.list[i] < h.list[p] {
+		h.swap(i, p)
+		h.upHeap(p)
+	}
+}
+
+// remove pops the smallest element
+func (h *Heap[T]) Remove() (T, error) {
+	var zero T
+	if len(h.list) == 0 {
+		return zero, errors.New("removing from an empty heap!")
+	}
+
+	temp := h.list[0]
+	last := h.list[len(h.list)-1]
+	h.list = h.list[:len(h.list)-1]
+
+	if len(h.list) > 0 {
+		h.list[0] = last
+		h.downHeap(0)
+	}
+	return temp, nil
+}
+
+func (h *Heap[T]) downHeap(i int) {
+	min := i
+	l := leftChild(i)
+	r := rightChild(i)
+
+	if l < len(h.list) && h.list[min] > h.list[l] {
+		min = l
+	}
+	if r < len(h.list) && h.list[min] > h.list[r] {
+		min = r
+	}
+	if min != i {
+		h.swap(min, i)
+		h.downHeap(min)
+	}
+}
+
+// heapsort remove all items and reutrns them in order.
+func (h *Heap[T]) HeapSort() ([]T, error) {
+	var data []T
+	for len(h.list) > 0 {
+		item, err := h.Remove()
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, item)
+	}
+	return data, nil
+}
+
+func main() {
+	h := NewHeap[int]()
+	h.Insert(5)
+	h.Insert(3)
+	h.Insert(10)
+	h.Insert(1)
+
+	fmt.Println("Heap array : ", h.list)
+
+	val, _ := h.Remove()
+	fmt.Println("Removed: ", val)
+
+	sorted, _ := h.HeapSort()
+	fmt.Println("Sorted : ", sorted)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module DSA-go
 
-go 1.23.4
+go 1.24.0
+
+toolchain go1.24.7
+
+require golang.org/x/exp v0.0.0-20250911091902-df9299821621
