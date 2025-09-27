42651fe3f02c1f1695ded82394573c33d3163108
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-28 00:55:55 +0530

    commmit

---
+++ Assignments/smallestletter.go
@@ -0,0 +1,27 @@
+// Find smallest letter greater than target
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
+
+package main
+
+import "fmt"
+
+func main() {
+	input := []byte{'c', 'j', 'w'}
+	var target byte = 'a'
+	fmt.Println("the smalles character greater than a is : ", string(nextGreatestLetter(input, target)))
+}
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	start := 0
+	end := len(letters) - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+		if letters[mid] <= target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	return letters[start%len(letters)]
+}
