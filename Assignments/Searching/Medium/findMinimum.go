5f44e245c88517d9d0faad5aecb40833f459f6f4
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-30 15:48:39 +0530

    commit

---
+++ Assignments/Searching/Medium/findMinimum.go
@@ -0,0 +1,24 @@
+// Find minimum in rotated sorted array
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{3, 4, 5, 1, 2}
+	fmt.Println("Minimm element : ", findMin(arr))
+}
+
+func findMin(arr []int) int {
+	start, end := 0, len(arr)-1
+	for start < end {
+		mid := start + (end-start)/2
+		if arr[mid] > arr[end] {
+			start = mid + 1
+		} else {
+			end = mid
+		}
+	}
+	return arr[start]
+}
