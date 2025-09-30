4fe2a1efdbdce00f59b164714584a1af33f4ac72
shivakumar2006 <official.shivakumar06@gmail.com>
2025-09-30 11:24:52 +0530

    commit

--- Assignments/intersect2Arrays.go
+++ Assignments/intersect2Arrays.go
@@ -5,24 +5,23 @@ package main
 
 import "fmt"
 
-func intersection(nums1 []int, nums2 []int) []int {
+func intersection(nums1, nums2 []int) []int {
 	set := make(map[int]struct{})
-	for _, v := range nums1 {
-		set[v] = struct{}{}
+	for _, val := range nums1 {
+		set[val] = struct{}{}
 	}
 
 	resSet := make(map[int]struct{})
-	for _, v := range nums2 {
-		if _, ok := set[v]; ok {
-			resSet[v] = struct{}{}
+	for _, val := range nums2 {
+		if _, ok := set[val]; ok {
+			resSet[val] = struct{}{}
 		}
 	}
 
 	res := make([]int, 0, len(resSet))
-	for v := range resSet {
-		res = append(res, v)
+	for val := range resSet {
+		res = append(res, val)
 	}
-
 	return res
 }
 
@@ -32,3 +31,24 @@ func main() {
 
 	fmt.Println("Intersection of 2 arrays is : ", intersection(nums1, nums2))
 }
+
+// func intersection(nums1 []int, nums2 []int) []int {
+// 	set := make(map[int]struct{})
+// 	for _, v := range nums1 {
+// 		set[v] = struct{}{}
+// 	}
+
+// 	resSet := make(map[int]struct{})
+// 	for _, v := range nums2 {
+// 		if _, ok := set[v]; ok {
+// 			resSet[v] = struct{}{}
+// 		}
+// 	}
+
+// 	res := make([]int, 0, len(resSet))
+// 	for v := range resSet {
+// 		res = append(res, v)
+// 	}
+
+// 	return res
+// }
---
+++ Assignments/intersect2Arrays2.go
@@ -0,0 +1,13 @@
+// intersection of two arrays
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+package main
+
+func main() {
+	nums1 := []int{1, 2, 2, 1}
+	nums2 := []int{2, 2}
+}
+
+func intersect(nums1, nums2 []int) []int {
+
+}
