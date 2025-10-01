56cab2a89d173b9d8fae0e8417c5cf5a8a0b3500
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-01 15:32:42 +0530

    commit

--- Assignments/Searching/Medium/findRightIntervals.go
+++ Assignments/Searching/Medium/findRightIntervals.go
@@ -29,7 +29,6 @@ func findRightInterval(arr [][]int) []int {
 		indices[i] = i
 	}
 
-	// sort start
 	sort.Slice(indices, func(i, j int) bool {
 		return start[indices[i]] < start[indices[j]]
 	})
@@ -53,3 +52,44 @@ func findRightInterval(arr [][]int) []int {
 	}
 	return result
 }
+
+// func findRightInterval(arr [][]int) []int {
+// 	n := len(arr)
+// 	if n == 1 {
+// 		if arr[0][0] >= arr[0][1] {
+// 			return []int{0}
+// 		}
+// 		return []int{-1}
+// 	}
+
+// 	start := make([]int, n)
+// 	indices := make([]int, n)
+// 	for i := 0; i < n; i++ {
+// 		start[i] = arr[i][0]
+// 		indices[i] = i
+// 	}
+
+// 	// sort start
+// 	sort.Slice(indices, func(i, j int) bool {
+// 		return start[indices[i]] < start[indices[j]]
+// 	})
+
+// 	result := make([]int, n)
+// 	for i := 0; i < n; i++ {
+// 		end := arr[i][1]
+
+// 		l, r := 0, n-1
+// 		ans := -1
+// 		for l <= r {
+// 			mid := l + (r-l)/2
+// 			if start[indices[mid]] >= end {
+// 				ans = indices[mid]
+// 				r = mid - 1
+// 			} else {
+// 				l = mid + 1
+// 			}
+// 		}
+// 		result[i] = ans
+// 	}
+// 	return result
+// }
