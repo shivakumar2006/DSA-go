494e16a19b59f909312c274a3db07ef5c160b11c
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-01 22:54:17 +0530

    commit

--- Assignments/Searching/Medium/kokoEatsBanana.go
+++ Assignments/Searching/Medium/kokoEatsBanana.go
@@ -41,3 +41,31 @@ func canEatAll(piles []int, h int, mid int) bool {
 	}
 	return hours <= h
 }
+
+// func minEatingSpeed(piles []int, h int) int {
+// 	start, end := 1, 0
+
+// 	for _, pile := range piles {
+// 		if pile > end {
+// 			end = pile
+// 		}
+// 	}
+
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if canEatAll(piles, h, mid) {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return start
+// }
+
+// func canEatAll(piles []int, h int, mid int) bool {
+// 	hours := 0
+// 	for _, pile := range piles {
+// 		hours += int(math.Ceil(float64(pile) / float64(mid)))
+// 	}
+// 	return hours <= h
+// }
