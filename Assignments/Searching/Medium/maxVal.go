6b4a2642e5fed344feaafbb1811d44361535b293
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-01 22:10:15 +0530

    commit

--- Assignments/Searching/Medium/maxVal.go
+++ Assignments/Searching/Medium/maxVal.go
@@ -13,43 +13,80 @@ func main() {
 	fmt.Println(ans)
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
+func maxValue(n, index, maxSum int) int {
+	start, end := 0, maxSum
+	ans := 1
 
-func maxValue(n int, index int, maxSum int) int {
-	maxVal := 1
-
-	for val := 1; ; val++ {
-		arr := make([]int, n)
-		arr[index] = val
-
-		// fill the left side
-		for i := index - 1; i >= 0; i-- {
-			arr[i] = max(1, arr[i+1]-1)
+	for start <= end {
+		mid := start + (end-start)/2
+		if isPossible(mid, n, index, maxSum) {
+			ans = mid
+			end = mid - 1
+		} else {
+			start = mid + 1
 		}
-
-		// fill right side
-		for i := index + 1; i < n; i++ {
-			arr[i] = max(1, arr[i-1]-1)
-		}
-
-		// calculate sum
-		sum := 0
-		for i := 0; i < n; i++ {
-			sum += arr[i] // sum = sum + arr[i]
-		}
-		if sum > maxSum {
-			break
-		}
-		maxVal = val
 	}
 
-	return maxVal
+	return ans
 }
+
+func isPossible(mid, n, index, maxSum int) bool {
+	leftLen := index
+	rightLen := n - index - 1
+
+	left := calcSum(mid, leftLen)
+	right := calcSum(mid, rightLen)
+
+	total := left + right + maxSum
+	return total <= maxSum
+}
+
+func calcSum(mid int, length int) int {
+	if mid > length {
+		last := mid - length
+		return (mid - 1 + last) * length / 2
+	} else {
+		return (mid-1)*mid/2 + (length - (mid - 1))
+	}
+}
+
+// func max(a, b int) int {
+// 	if a > b {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// func maxValue(n int, index int, maxSum int) int {
+// 	maxVal := 1
+
+// 	for val := 1; ; val++ {
+// 		arr := make([]int, n)
+// 		arr[index] = val
+
+// 		// fill the left side
+// 		for i := index - 1; i >= 0; i-- {
+// 			arr[i] = max(1, arr[i+1]-1)
+// 		}
+
+// 		// fill right side
+// 		for i := index + 1; i < n; i++ {
+// 			arr[i] = max(1, arr[i-1]-1)
+// 		}
+
+// 		// calculate sum
+// 		sum := 0
+// 		for i := 0; i < n; i++ {
+// 			sum += arr[i] // sum = sum + arr[i]
+// 		}
+// 		if sum > maxSum {
+// 			break
+// 		}
+// 		maxVal = val
+// 	}
+
+// 	return maxVal
+// }
 
 // func maxValue(n int, index int, maxSum int) int {
 // 	start, end := 1, maxSum
