c68e0f3fd7aae653f08d93091389a30915dd65f8
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-01 20:17:20 +0530

    commit

--- Assignments/Searching/Medium/reachNumber.go
+++ Assignments/Searching/Medium/reachNumber.go
@@ -3,24 +3,46 @@
 
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func reachNumber(target int) int {
-	if target < 0 {
-		target = -target
+func main() {
+	tar := 2
+	fmt.Println(reachNumber(tar))
+}
+
+func reachNumber(tar int) int {
+	if tar < 0 {
+		tar = -tar
 	}
 
 	sum, steps := 0, 0
-	for sum < target || (sum-target)%2 != 0 {
+	for sum < tar || (sum-tar)%2 != 0 {
 		steps++
 		sum += steps
 	}
 	return steps
 }
 
-func main() {
-	target := 3
-	fmt.Println(reachNumber(target))
-}
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// func reachNumber(target int) int {
+// 	if target < 0 {
+// 		target = -target
+// 	}
+
+// 	sum, steps := 0, 0
+// 	for sum < target || (sum-target)%2 != 0 {
+// 		steps++
+// 		sum += steps
+// 	}
+// 	return steps
+// }
+
+// func main() {
+// 	target := 3
+// 	fmt.Println(reachNumber(target))
+// }
