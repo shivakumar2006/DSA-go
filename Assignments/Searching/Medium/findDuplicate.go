a04e045759cfe6d799c0a5eeb81de5fa46221620
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-03 18:11:39 +0530

    commit

--- Assignments/Searching/Medium/findDuplicate.go
+++ Assignments/Searching/Medium/findDuplicate.go
@@ -18,7 +18,6 @@ func findDuplicate(nums []int) int {
 				count++
 			}
 		}
-
 		if count > mid {
 			high = mid
 		} else {
@@ -32,3 +31,26 @@ func main() {
 	nums := []int{1, 3, 4, 2, 2}
 	fmt.Println(findDuplicate(nums))
 }
+
+// func findDuplicate(nums []int) int {
+// 	n := len(nums) - 1
+// 	low := 1
+// 	high := n
+
+// 	for low < high {
+// 		mid := low + (high-low)/2
+// 		count := 0
+// 		for _, num := range nums {
+// 			if num <= mid {
+// 				count++
+// 			}
+// 		}
+
+// 		if count > mid {
+// 			high = mid
+// 		} else {
+// 			low = mid + 1
+// 		}
+// 	}
+// 	return low
+// }
