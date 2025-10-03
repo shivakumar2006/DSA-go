8f42b17ae7417a7701d4ab28ec2b63a3623b7b8e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-04 03:57:19 +0530

    commit

--- Assignments/Searching/Easy/firstBadNum.go
+++ Assignments/Searching/Easy/firstBadNum.go
@@ -15,9 +15,7 @@ func isBadVersion(version int) bool {
 }
 
 func firstBadVersion(n int) int {
-	start := 1
-	end := n
-
+	start, end := 1, n
 	for start < end {
 		mid := start + (end-start)/2
 		res := isBadVersion(mid)
@@ -34,3 +32,26 @@ func main() {
 	n := 5
 	fmt.Println("THe bad number is : ", firstBadVersion(n))
 }
+
+// func isBadVersion(version int) bool {
+// 	if version == bad {
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func firstBadVersion(n int) int {
+// 	start := 1
+// 	end := n
+
+// 	for start < end {
+// 		mid := start + (end-start)/2
+// 		res := isBadVersion(mid)
+// 		if res == true {
+// 			end = mid
+// 		} else if res == false {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return start
+// }
