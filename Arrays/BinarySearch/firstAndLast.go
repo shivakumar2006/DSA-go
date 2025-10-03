c18f13060836a5d57ed7cb43dc8d9d470433246e
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-04 02:32:04 +0530

    commit

--- Arrays/BinarySearch/firstAndLast.go
+++ Arrays/BinarySearch/firstAndLast.go
@@ -35,13 +35,13 @@ func findFirst(arr []int, tar int) int {
 	res := -1
 	for start <= end {
 		mid := start + (end-start)/2
-		if tar == arr[mid] {
+		if arr[mid] == tar {
 			res = mid
 			end = mid - 1
-		} else if tar < arr[mid] {
-			end = mid - 1
-		} else {
+		} else if arr[mid] < tar {
 			start = mid + 1
+		} else {
+			end = mid - 1
 		}
 	}
 	return res
@@ -52,17 +52,62 @@ func findLast(arr []int, tar int) int {
 	res := -1
 	for start <= end {
 		mid := start + (end-start)/2
-		if tar == arr[mid] {
+		if arr[mid] == tar {
 			res = mid
 			start = mid + 1
-		} else if tar < arr[mid] {
-			end = mid - 1
-		} else {
+		} else if arr[mid] < tar {
 			start = mid + 1
+		} else {
+			end = mid - 1
 		}
 	}
 	return res
 }
+
+// func search(arr []int, tar int) []int {
+// 	first := findFirst(arr, tar)
+// 	last := findLast(arr, tar)
+
+// 	if first == -1 && last == -1 {
+// 		return []int{-1, -1}
+// 	}
+
+// 	return []int{first, last}
+// }
+
+// func findFirst(arr []int, tar int) int {
+// 	start, end := 0, len(arr)-1
+// 	res := -1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			res = mid
+// 			end = mid - 1
+// 		} else if tar < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return res
+// }
+
+// func findLast(arr []int, tar int) int {
+// 	start, end := 0, len(arr)-1
+// 	res := -1
+// 	for start <= end {
+// 		mid := start + (end-start)/2
+// 		if tar == arr[mid] {
+// 			res = mid
+// 			start = mid + 1
+// 		} else if tar < arr[mid] {
+// 			end = mid - 1
+// 		} else {
+// 			start = mid + 1
+// 		}
+// 	}
+// 	return res
+// }
 
 // func search(arr []int, tar int) []int {
 // 	first := findFirst(arr, tar)
