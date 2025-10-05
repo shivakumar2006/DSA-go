8964bf965c9111de8cca4e51404d25a72ad595cd
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-05 19:01:20 +0530

    commit

---
+++ Assignments/Sorting/Easy/intersection2.go
@@ -0,0 +1,28 @@
+// intersection of 2 arrays 2
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+
+package main
+
+import "fmt"
+
+func intersect(nums1, nums2 []int) []int {
+	freq := make(map[int]int)
+	for _, val := range nums1 {
+		freq[val]++
+	}
+
+	res := []int{}
+	for _, val := range nums2 {
+		if freq[val] > 0 {
+			res = append(res, val)
+			freq[val]--
+		}
+	}
+	return res
+}
+
+func main() {
+	nums1 := []int{1, 2, 2, 1}
+	nums2 := []int{2, 2}
+	fmt.Println(intersect(nums1, nums2))
+}
