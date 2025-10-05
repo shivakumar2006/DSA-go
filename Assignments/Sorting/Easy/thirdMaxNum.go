b6a89c5431565e16f7bd6010503ab65593033145
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-05 18:51:16 +0530

    commit

--- Assignments/Sorting/Easy/thirdMaxNum.go
+++ Assignments/Sorting/Easy/thirdMaxNum.go
@@ -1,5 +1,5 @@
-// Third MaxNum
-// https://leetcode.com/problems/third-maximum-number/description/
+// // Third MaxNum
+// // https://leetcode.com/problems/third-maximum-number/description/
 
 package main
 
@@ -11,13 +11,11 @@ func thirdMax(nums []int) int {
 		set[num] = true
 	}
 
-	// convert map to slice
 	distinctNum := []int{}
 	for num := range set {
 		distinctNum = append(distinctNum, num)
 	}
 
-	// sort elements in descending order
 	for i := 1; i < len(distinctNum); i++ {
 		j := i
 		for j > 0 && distinctNum[j] > distinctNum[j-1] {
@@ -26,7 +24,6 @@ func thirdMax(nums []int) int {
 		}
 	}
 
-	// return third max if it's exist otherwise max
 	if len(distinctNum) >= 3 {
 		return distinctNum[2]
 	}
@@ -41,3 +38,44 @@ func main() {
 	nums := []int{2, 2, 3, 1}
 	fmt.Println(thirdMax(nums))
 }
+
+// package main
+
+// import "fmt"
+
+// func thirdMax(nums []int) int {
+// 	set := make(map[int]bool)
+// 	for _, num := range nums {
+// 		set[num] = true
+// 	}
+
+// 	// convert map to slice
+// 	distinctNum := []int{}
+// 	for num := range set {
+// 		distinctNum = append(distinctNum, num)
+// 	}
+
+// 	// sort elements in descending order
+// 	for i := 1; i < len(distinctNum); i++ {
+// 		j := i
+// 		for j > 0 && distinctNum[j] > distinctNum[j-1] {
+// 			swap(distinctNum, j, j-1)
+// 			j--
+// 		}
+// 	}
+
+// 	// return third max if it's exist otherwise max
+// 	if len(distinctNum) >= 3 {
+// 		return distinctNum[2]
+// 	}
+// 	return distinctNum[0]
+// }
+
+// func swap(nums []int, i, j int) {
+// 	nums[i], nums[j] = nums[j], nums[i]
+// }
+
+// func main() {
+// 	nums := []int{2, 2, 3, 1}
+// 	fmt.Println(thirdMax(nums))
+// }
