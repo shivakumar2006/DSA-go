7b933a1112fbd533eb5a2653a61946f4c856bc26
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-11 05:22:52 +0530

    commit

--- Assignments/Sorting/Medium/3Sum.go
+++ Assignments/Sorting/Medium/3Sum.go
@@ -14,22 +14,23 @@ func threeSum(arr []int) [][]int {
 	res := [][]int{}
 
 	for i := 0; i < n-2; i++ {
-		if i > 0 && arr[i] == arr[i-1] { // skip duplicates
+		if i > 0 && arr[i] == arr[i-1] {
 			continue
 		}
 
 		left, right := i+1, n-1
-
 		for left < right {
 			sum := arr[i] + arr[left] + arr[right]
 			if sum == 0 {
 				res = append(res, []int{arr[i], arr[left], arr[right]})
+
 				for left < right && arr[left] == arr[left+1] {
 					left++
 				}
 				for left < right && arr[right] == arr[right-1] {
 					right--
 				}
+
 				left++
 				right--
 			} else if sum < 0 {
@@ -46,3 +47,37 @@ func main() {
 	arr := []int{-1, 0, 1, 2, -1, -4}
 	fmt.Println(threeSum(arr))
 }
+
+// func threeSum(arr []int) [][]int {
+// 	sort.Ints(arr)
+// 	n := len(arr)
+// 	res := [][]int{}
+
+// 	for i := 0; i < n-2; i++ {
+// 		if i > 0 && arr[i] == arr[i-1] { // skip duplicates
+// 			continue
+// 		}
+
+// 		left, right := i+1, n-1
+
+// 		for left < right {
+// 			sum := arr[i] + arr[left] + arr[right]
+// 			if sum == 0 {
+// 				res = append(res, []int{arr[i], arr[left], arr[right]})
+// 				for left < right && arr[left] == arr[left+1] {
+// 					left++
+// 				}
+// 				for left < right && arr[right] == arr[right-1] {
+// 					right--
+// 				}
+// 				left++
+// 				right--
+// 			} else if sum < 0 {
+// 				left++
+// 			} else {
+// 				right--
+// 			}
+// 		}
+// 	}
+// 	return res
+// }
