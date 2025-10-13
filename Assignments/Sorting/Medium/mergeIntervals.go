0f49be6f249b6acf6be9152fcc11330984695e00
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-13 14:15:13 +0530

    commit

--- Assignments/Sorting/Medium/mergeIntervals.go
+++ Assignments/Sorting/Medium/mergeIntervals.go
@@ -45,6 +45,33 @@ func main() {
 // 		return [][]int{}
 // 	}
 
+// 	sort.Slice(arr, func(i, j int) bool {
+// 		return arr[i][0] < arr[j][0]
+// 	})
+
+// 	merged := [][]int{arr[0]}
+
+// 	for i := 0; i < len(arr); i++ {
+// 		last := merged[len(merged)-1]
+// 		current := arr[i]
+
+// 		if current[0] <= last[1] {
+// 			if current[1] > last[1] {
+// 				last[1] = current[1]
+// 			}
+// 			merged[len(merged)-1] = last
+// 		} else {
+// 			merged = append(merged, current)
+// 		}
+// 	}
+// 	return merged
+// }
+
+// func merge(arr [][]int) [][]int {
+// 	if len(arr) == 0 {
+// 		return [][]int{}
+// 	}
+
 // 	// sort the start line
 // 	sort.Slice(arr, func(i, j int) bool {
 // 		return arr[i][0] < arr[j][0]
