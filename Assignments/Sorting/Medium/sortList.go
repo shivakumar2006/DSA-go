774ac41599c5c8f3d1bdc69317b0fe5d9b24cd1c
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-13 15:56:40 +0530

    commit

--- Assignments/Sorting/Medium/sortList.go
+++ Assignments/Sorting/Medium/sortList.go
@@ -26,6 +26,7 @@ func (l *SL) Insert(value int) {
 	for temp.next != nil {
 		temp = temp.next
 	}
+
 	temp.next = newNode
 }
 
@@ -52,6 +53,7 @@ func getMid(head *Node) *Node {
 		slow = slow.next
 		fast = fast.next.next
 	}
+
 	return slow
 }
 
@@ -105,3 +107,87 @@ func main() {
 	fmt.Println("List after sorting : ")
 	list.Display()
 }
+
+// type Node struct {
+// 	data int
+// 	next *Node
+// }
+
+// type SL struct {
+// 	head *Node
+// }
+
+// func (l *SL) Insert(value int) {
+// 	newNode := &Node{data: value}
+
+// 	if l.head == nil {
+// 		l.head = newNode
+// 		return
+// 	}
+
+// 	temp := l.head
+// 	for temp.next != nil {
+// 		temp = temp.next
+// 	}
+// 	temp.next = newNode
+// }
+
+// func sortList(head *Node) *Node {
+// 	if head == nil || head.next == nil {
+// 		return head
+// 	}
+
+// 	mid := getMid(head)
+// 	right := mid.next
+// 	mid.next = nil
+
+// 	left := sortList(head)
+// 	right = sortList(right)
+
+// 	return merge(left, right)
+// }
+
+// func getMid(head *Node) *Node {
+// 	slow := head
+// 	fast := head.next
+
+// 	for fast != nil && fast.next != nil {
+// 		slow = slow.next
+// 		fast = fast.next.next
+// 	}
+// 	return slow
+// }
+
+// func merge(l1, l2 *Node) *Node {
+// 	dummy := &Node{}
+// 	tail := dummy
+
+// 	for l1 != nil && l2 != nil {
+// 		if l1.data < l2.data {
+// 			tail.next = l1
+// 			l1 = l1.next
+// 		} else {
+// 			tail.next = l2
+// 			l2 = l2.next
+// 		}
+// 		tail = tail.next
+// 	}
+
+// 	if l1 != nil {
+// 		tail.next = l1
+// 	}
+// 	if l2 != nil {
+// 		tail.next = l2
+// 	}
+
+// 	return dummy.next
+// }
+
+// func (l *SL) Display() {
+// 	temp := l.head
+// 	for temp != nil {
+// 		fmt.Print(temp.data, " -> ")
+// 		temp = temp.next
+// 	}
+// 	fmt.Println("END")
+// }
