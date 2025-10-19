1c20a11bc4b70ff7996ab041eaa27d30d0b55cf0
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-19 20:47:28 +0530

    commit

---
+++ Assignments/Recursion/Medium/symbolInGrammar.go
@@ -0,0 +1,25 @@
+// K-th Symbol in Grammar
+// http://leetcode.com/problems/k-th-symbol-in-grammar/description/
+
+package main
+
+import "fmt"
+
+func kthGrammar(n int, k int) int {
+	if n == 1 {
+		return 0
+	}
+
+	mid := 1 << (n - 2) // middle position of current row
+	if k < mid {
+		return kthGrammar(n-1, k) // left half: same as previous row
+	} else {
+		return 1 - kthGrammar(n-1, k-mid) // right half: flip previous row value
+	}
+}
+
+func main() {
+	n := 2
+	k := 1
+	fmt.Println(kthGrammar(n, k))
+}
