c49b0967430c57e2a1db91ad33808e78abe60aa4
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-20 13:46:46 +0530

    commit

--- Assignments/Recursion/Medium/symbolInGrammar.go
+++ Assignments/Recursion/Medium/symbolInGrammar.go
@@ -9,9 +9,9 @@ func kthGrammar(n int, k int) int {
 	if n == 1 {
 		return 0
 	}
-
+	// 2^n-2
 	mid := 1 << (n - 2) // middle position of current row
-	if k < mid {
+	if k <= mid {
 		return kthGrammar(n-1, k) // left half: same as previous row
 	} else {
 		return 1 - kthGrammar(n-1, k-mid) // right half: flip previous row value
