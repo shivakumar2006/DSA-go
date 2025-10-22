e2ba700efba1db4c7b30660af1ae09782d124446
shivakumar2006 <official.shivakumar06@gmail.com>
2025-10-22 17:52:51 +0530

    commit

---
+++ Assignments/Recursion/Medium/minNonZeroProd.go
@@ -0,0 +1,32 @@
+// Minimum non-zero product of the array element
+// https://leetcode.com/problems/minimum-non-zero-product-of-the-array-elements/description/
+
+package main
+
+import "fmt"
+
+const mod int64 = 1e9 + 7
+
+func modPow(secondMax, exp, mod int64) int64 {
+	result := int64(1)
+	for exp > 0 {
+		if exp%2 == 1 {
+			result = (result * secondMax) % mod
+		}
+		secondMax = (secondMax * secondMax) % mod
+		exp /= 2
+	}
+	return result
+}
+
+func minNonZeroProduct(p int) int {
+	maxNum := int64((1 << p) - 1)
+	secondMax := maxNum - 1
+	powPart := modPow(secondMax, maxNum-1, mod)
+	return int((powPart * maxNum) % mod)
+}
+
+func main() {
+	p := 1
+	fmt.Println(minNonZeroProduct(p))
+}
