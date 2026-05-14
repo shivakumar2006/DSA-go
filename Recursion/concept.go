bf4f6eb181796742d512c37b56a9a0e31a7297e3
shivakumar2006 <official.shivakumar06@gmail.com>
2026-05-14 14:22:41 +0530

    commit

--- Recursion/concept.go
+++ Recursion/concept.go
@@ -11,9 +11,18 @@ func fun(n int) {
 		return
 	}
 
-	fmt.Println(n)
 	fun(n - 1)
+	fmt.Println(n)
 }
+
+// func fun(n int) {
+// 	if n == 0 {
+// 		return
+// 	}
+
+// 	fmt.Println(n)
+// 	fun(n - 1)
+// }
 
 // func fun(n int) {
 // 	if n == 0 {
